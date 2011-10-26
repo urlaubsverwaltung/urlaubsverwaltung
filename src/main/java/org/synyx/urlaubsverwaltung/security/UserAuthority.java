7913caa760b20191ada8353ebe438531510e049e
Murygina <murygina@synyx.de>
2011-10-26 11:21:53 +0200

    added security package with UserAuthority and UserSaltSource

---
+++ src/main/java/org/synyx/urlaubsverwaltung/security/UserAuthority.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.synyx.urlaubsverwaltung.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+
+/**
+ * @author  aljona
+ */
+public class UserAuthority implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    public String getAuthority() {
+
+        return "ROLE_USER";
+    }
+}
