2adcc5654a39d88b2770368324ca3e226827189a
Murygina <murygina@synyx.de>
2011-10-26 10:54:21 +0200

    modified AntragDAO and AntragServiceImpl, implemented methods: getAllRequestsByState(State state); getAllRequestsForPerson(Person person);

--- src/main/java/org/synyx/urlaubsverwaltung/dao/AntragDAO.java
+++ src/main/java/org/synyx/urlaubsverwaltung/dao/AntragDAO.java
@@ -1,12 +1,25 @@
 package org.synyx.urlaubsverwaltung.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import org.synyx.urlaubsverwaltung.domain.Antrag;
+import org.synyx.urlaubsverwaltung.domain.Person;
+import org.synyx.urlaubsverwaltung.domain.State;
+
+import java.util.List;
+
 
 /**
- * @author johannes
+ * @author  johannes
  */
 public interface AntragDAO extends JpaRepository<Antrag, Integer> {
 
-	// empty - for now...
+    // get List<Antrag> by certain state (e.g. 'wartend')
+    @Query("select x from antrag x where x.state = ?")
+    List<Antrag> getAllRequestsByState(State state);
+
+
+    @Query("select x from antrag x where x.person = ?")
+    List<Antrag> getAllRequestsForPerson(Person person);
 }
--- src/main/java/org/synyx/urlaubsverwaltung/service/AntragServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/service/AntragServiceImpl.java
@@ -1,67 +1,84 @@
 package org.synyx.urlaubsverwaltung.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.transaction.annotation.Transactional;
+
 import org.synyx.urlaubsverwaltung.dao.AntragDAO;
 import org.synyx.urlaubsverwaltung.domain.Antrag;
 import org.synyx.urlaubsverwaltung.domain.Person;
 import org.synyx.urlaubsverwaltung.domain.State;
 
+import java.util.List;
+
+
 /**
  * implementation of the requestdata-access-service.
- * 
- * @author johannes
- * 
+ *
+ * @author  johannes
  */
 @Transactional
 public class AntragServiceImpl implements AntragService {
 
-	private AntragDAO antragDAO;
+    private AntragDAO antragDAO;
 
-	@Autowired
-	public AntragServiceImpl(AntragDAO antragDAO) {
-		this.antragDAO = antragDAO;
-	}
+    @Autowired
+    public AntragServiceImpl(AntragDAO antragDAO) {
 
-	@Override
-	public void save(Antrag antrag) {
-		antragDAO.save(antrag);
-	}
+        this.antragDAO = antragDAO;
+    }
 
-	@Override
-	public void approve(Antrag antrag) {
-		antrag.setState(State.GENEHMIGT);
-		antragDAO.save(antrag);
-	}
+    @Override
+    public void save(Antrag antrag) {
 
-	@Override
-	public void decline(Antrag antrag) {
-		antrag.setState(State.ABGELEHNT);
-		antragDAO.save(antrag);
-	}
+        antragDAO.save(antrag);
+    }
 
-	@Override
-	public Antrag getRequestById(Integer id) {
-		return antragDAO.findOne(id);
-	}
 
-	@Override
-	public List<Antrag> getAllRequestsForPerson(Person person) {
-		// TODO implement
-		return null;
-	}
+    @Override
+    public void approve(Antrag antrag) {
 
-	@Override
-	public List<Antrag> getAllRequests() {
-		return antragDAO.findAll();
-	}
+        antrag.setState(State.GENEHMIGT);
+        antragDAO.save(antrag);
+    }
 
-	@Override
-	public List<Antrag> getAllRequestsByState(State state) {
-		// TODO implement
-		return null;
-	}
 
+    @Override
+    public void decline(Antrag antrag) {
+
+        antrag.setState(State.ABGELEHNT);
+        antragDAO.save(antrag);
+    }
+
+
+    @Override
+    public Antrag getRequestById(Integer id) {
+
+        return antragDAO.findOne(id);
+    }
+
+
+    @Override
+    public List<Antrag> getAllRequestsForPerson(Person person) {
+
+        antragDAO.getAllRequestsForPerson(person);
+
+        return null;
+    }
+
+
+    @Override
+    public List<Antrag> getAllRequests() {
+
+        return antragDAO.findAll();
+    }
+
+
+    @Override
+    public List<Antrag> getAllRequestsByState(State state) {
+
+        antragDAO.getAllRequestsByState(state);
+
+        return null;
+    }
 }
