d2d76293fd166963d8c75264d1a0b25e006e5d3d
Murygina <murygina@synyx.de>
2011-10-26 11:13:41 +0200

    added DateMidnightPropertyEditor and CharsetFilter

---
+++ src/main/java/org/synyx/urlaubsverwaltung/editor/DateMidnightPropertyEditor.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.synyx.urlaubsverwaltung.editor;
+
+import org.joda.time.DateTime;
+import org.joda.time.ReadableInstant;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import org.springframework.util.StringUtils;
+
+import java.beans.PropertyEditorSupport;
+
+import java.util.Locale;
+
+
+/**
+ * @author  aljona
+ */
+public class DateMidnightPropertyEditor extends PropertyEditorSupport {
+
+    private DateTimeFormatter formatter = null;
+
+    public DateMidnightPropertyEditor(Locale locale) {
+
+        this.formatter = DateTimeFormat.mediumDate().withLocale(locale);
+    }
+
+    // aus Datum String machen
+    @Override
+    public String getAsText() {
+
+        if (this.getValue() == null) {
+            return "";
+        }
+
+        return formatter.print((ReadableInstant) this.getValue());
+    }
+
+
+    // aus String Datum erzeugen
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+
+        if (!StringUtils.hasText(text)) {
+            this.setValue(null);
+        } else {
+            DateTime dateTime = formatter.parseDateTime(text);
+
+            this.setValue(dateTime.toDateMidnight());
+        }
+    }
+}
