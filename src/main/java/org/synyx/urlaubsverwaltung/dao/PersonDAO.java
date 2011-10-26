9ed17c2daa35739b1ef535038129e68eb834ea90
Murygina <murygina@synyx.de>
2011-10-26 10:40:27 +0200

    removed userDAO because its methods actually have to be used by personDAO

--- src/main/java/org/synyx/urlaubsverwaltung/dao/PersonDAO.java
+++ src/main/java/org/synyx/urlaubsverwaltung/dao/PersonDAO.java
@@ -1,11 +1,16 @@
 package org.synyx.urlaubsverwaltung.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import org.synyx.urlaubsverwaltung.domain.Person;
 
+
 /**
- * @author johannes
+ * @author  johannes
  */
 public interface PersonDAO extends JpaRepository<Person, Integer> {
 
+    @Query("select x from holiday x where x.person = ?")
+    Integer readVacationDays(Person person);
 }
