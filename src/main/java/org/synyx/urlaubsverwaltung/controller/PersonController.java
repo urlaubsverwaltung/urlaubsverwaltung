911e1c3d35f4d1a356c0ee573625ad16b6e086d0
Murygina <murygina@synyx.de>
2011-10-31 16:54:24 +0100

    Added personform.jsp and methods in PersonController for editing or creating a person. And therefore modified properties file.

--- src/main/java/org/synyx/urlaubsverwaltung/controller/PersonController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/controller/PersonController.java
@@ -3,6 +3,7 @@ package org.synyx.urlaubsverwaltung.controller;
 
 import org.springframework.ui.Model;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +33,13 @@ public class PersonController {
         this.dateService = dateService;
     }
 
+    /**
+     * Listenansicht aller Mitarbeiter und ihrer Urlaubstage
+     *
+     * @param  model
+     *
+     * @return
+     */
     @RequestMapping(value = "/mitarbeiter/list", method = RequestMethod.GET)
     public String showMitarbeiterList(Model model) {
 
@@ -39,10 +47,17 @@ public class PersonController {
 
         model.addAttribute("mitarbeiter", mitarbeiter);
 
-        return "personen/mitarbeiter/list";
+        return "personen/mitarbeiterliste";
     }
 
 
+    /**
+     * Detailansicht aller Mitarbeiter und ihrer Urlaubstage
+     *
+     * @param  model
+     *
+     * @return
+     */
     @RequestMapping(value = "/mitarbeiter/detail", method = RequestMethod.GET)
     public String showMitarbeiterDetail(Model model) {
 
@@ -50,10 +65,18 @@ public class PersonController {
 
         model.addAttribute("mitarbeiter", mitarbeiter);
 
-        return "personen/mitarbeiter/detail";
+        return "personen/mitarbeiterdetails";
     }
 
 
+    /**
+     * Uebersicht fuer User: Infos zu einzelnem Mitarbeiter
+     *
+     * @param  mitarbeiterId
+     * @param  model
+     *
+     * @return
+     */
     @RequestMapping(value = "/mitarbeiter/{mitarbeiterId}/overview", method = RequestMethod.GET)
     public String showOverview(@PathVariable("mitarbeiterId") Integer mitarbeiterId, Model model) {
 
@@ -66,5 +89,85 @@ public class PersonController {
         model.addAttribute("person", person);
 
         return "personen/overview";
+    }
+
+
+    /**
+     * Liefert Formular, um einen Mitarbeiter zu editieren
+     *
+     * @param  mitarbeiterId
+     * @param  model
+     *
+     * @return
+     */
+    @RequestMapping(value = "/mitarbeiter/{mitarbeiterId}/edit", method = RequestMethod.GET)
+    public String editPersonForm(@PathVariable("mitarbeiterId") Integer mitarbeiterId, Model model) {
+
+        Person person = personService.getPersonByID(mitarbeiterId);
+
+        model.addAttribute("person", person);
+
+        return "personen/personform";
+    }
+
+
+    /**
+     * Speichert Datenaenderungen eines Mitarbeiters ab.
+     *
+     * @param  person
+     * @param  mitarbeiterId
+     *
+     * @return
+     */
+    @RequestMapping(value = "/mitarbeiter/{mitarbeiterId}/edit", method = RequestMethod.PUT)
+    public String editPerson(@ModelAttribute("person") Person person,
+        @PathVariable("mitarbeiterId") Integer mitarbeiterId) {
+
+        Person personToUpdate = personService.getPersonByID(mitarbeiterId);
+
+        personToUpdate.setLastName(person.getLastName());
+        personToUpdate.setFirstName(person.getFirstName());
+        personToUpdate.setEmail(person.getEmail());
+        personToUpdate.setVacationDays(person.getVacationDays());
+
+        personService.save(personToUpdate);
+
+        // braucht noch richtigen Verweis
+        return "redirect:irgendwohin";
+    }
+
+
+    /**
+     * Liefert Formular, um eine neue Person anzulegen.
+     *
+     * @param  model
+     *
+     * @return
+     */
+    @RequestMapping(value = "/mitarbeiter/new", method = RequestMethod.GET)
+    public String newPersonForm(Model model) {
+
+        Person person = new Person();
+
+        model.addAttribute("person", person);
+
+        return "personen/personform";
+    }
+
+
+    /**
+     * Speichert eine neu angelegte Person ab.
+     *
+     * @param  person
+     *
+     * @return
+     */
+    @RequestMapping(value = "/mitarbeiter/new", method = RequestMethod.POST)
+    public String newPerson(@ModelAttribute("person") Person person) {
+
+        personService.save(person);
+
+        // braucht noch richtigen Verweis
+        return "redirect:irgendwohin";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/personen/personform.jsp
@@ -0,0 +1,76 @@
+<%-- 
+    Document   : personform
+    Created on : 31.10.2011, 10:00:10
+    Author     : aljona
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@taglib prefix="joda" uri="http://www.joda.org/joda/time/tags" %>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><spring:message code="title" /></title>
+    </head>
+    
+    <body>
+        
+        <spring:url var="formUrlPrefix" value="/web/urlaubsverwaltung" />
+        
+        <c:choose>
+            <c:when test="${person.id == null}">
+                <c:set var="method" value="post" />
+                <c:set var="url" value="${formUrlPrefix}/mitarbeiter/new" />
+            </c:when>
+            <c:otherwise>
+                <c:set var="method" value="put" />
+                <c:set var="url" value="${formUrlPrefix}/mitarbeiter/${person.id}/edit" />
+            </c:otherwise>
+        </c:choose>
+        
+        <form:form method="${method}" action="${url}" modelAttribute="person" enctype="multipart/form-data"> 
+            <form:hidden path="id" />
+                <table>
+                    <tr>
+                        <td><label for="nachname"><spring:message code="lastname" />:</label></td>
+                        <td><form:input id="nachname" path="lastName" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="vorname"><spring:message code="firstname" />:</label></td>
+                        <td><form:input id="vorname" path="firstName" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="email"><spring:message code="email" />:</label></td>
+                        <td><form:input id="email" path="email" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="urlaubsanspruch"><spring:message code="anspruch" />:</label></td>
+                        <td><form:input id="urlaubsanspruch" path="vacationDays" /></td>
+                    </tr>
+                    <c:if test="${person.id == null}">
+                    <tr>
+                        <td><label for="unterschrift"><spring:message code="lastname" />:</label></td>
+                        <td><input type="file" name="sign" id="unterschrift" size="17%" /></td>
+                    </tr>
+                    </c:if>
+                    <tr>
+                        <td>
+                            <input type="submit" name="<spring:message code="save" />" value="<spring:message code="save" />" />                        
+                        </td>
+                        <td>
+                            <!--  weiss noch nicht, wo der Link hin zeigen soll -->
+                            <a href=""><spring:message code="cancel" /></a>
+                        </td>
+                    </tr>
+                </table>
+        </form:form>
+        
+    </body>
+    
+</html>
