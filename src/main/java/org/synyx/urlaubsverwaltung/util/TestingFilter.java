7a2f71785763ab317b53e0fd8f9056d4aaa389cc
Murygina <murygina@synyx.de>
2011-11-14 10:28:50 +0100

    Configurating LDAP and logger (log4j). By Johannes

---
+++ src/main/java/org/synyx/urlaubsverwaltung/util/TestingFilter.java
@@ -0,0 +1,92 @@
+package org.synyx.urlaubsverwaltung.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+
+public final class TestingFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+        ServletException {
+
+        ByteResponseWrapper brw = new ByteResponseWrapper((HttpServletResponse) response);
+        chain.doFilter(request, brw);
+
+        String out = new String(brw.getBytes());
+
+        out = out.replace("<body>",
+                "<body><p><a href=\"http://www.thevadersong.com/\"><img src=\"http://farm3.static.flickr.com/2248/2282734669_a7f431e660_o.jpg\"></a></p>");
+
+        response.getWriter().print(out);
+    }
+
+
+    public void destroy() {
+    }
+
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    static class ByteResponseWrapper extends HttpServletResponseWrapper {
+
+        private PrintWriter writer;
+        private ByteOutputStream output;
+
+        public ByteResponseWrapper(HttpServletResponse response) {
+
+            super(response);
+            output = new ByteOutputStream();
+            writer = new PrintWriter(output);
+        }
+
+        public byte[] getBytes() {
+
+            writer.flush();
+
+            return output.getBytes();
+        }
+
+
+        @Override
+        public PrintWriter getWriter() {
+
+            return writer;
+        }
+
+
+        @Override
+        public ServletOutputStream getOutputStream() throws IOException {
+
+            return output;
+        }
+    }
+
+    static class ByteOutputStream extends ServletOutputStream {
+
+        private ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        @Override
+        public void write(int b) throws IOException {
+
+            bos.write(b);
+        }
+
+
+        public byte[] getBytes() {
+
+            return bos.toByteArray();
+        }
+    }
+}
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -13,36 +13,30 @@
 
 <!DOCTYPE html>
 <html>
-    
+
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><spring:message code="title" /></title>
     </head>
-    
+
     <body>
-        
-        <form method="post" action="j_security_check">
-            
-            <table>
-                <tr>
-                   <td><label for="username"><spring:message code='username' />:</label></td> 
-                   <td><input id="username" type="text" name="j_username" /></td>    
-                <tr/>
-                <tr>
-                   <td><label for="password"><spring:message code='pw' />:</label></td>
-                   <td><input id="password" type="password" name="j_password" /></td>
-                </tr>    
-                <tr>
-                   <td colspan="2">&nbsp;</td>
-                </tr>
-                <tr>
-                   <td>&nbsp;</td>
-                   <td><input type="submit" name="<spring:message code='login' />" value="<spring:message code='login' />" /></td>
-                </tr>
-             </table>
-                
-        </form>   
-        
+        <%
+            if (request.getParameter("login_error") != null) {
+                out.println("Der Benutzer ist fehlerhaft.");
+            }
+
+        %>
+        <form method="post" action="j_spring_security_check">
+            <label for="j_username">Username</label>
+            <input type="text" name="j_username" id="j_username"/>
+            <br/>
+            <label for="j_password">Password</label>
+            <input type="password" name="j_password" id="j_password"/>
+            <br/>
+            <input type='checkbox' name='_spring_security_remember_me'/> Remember me on this computer.
+            <br/>
+            <input type="submit" value="Login"/>
+        </form>
     </body>
-    
+
 </html>
