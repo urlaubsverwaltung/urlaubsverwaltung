e39d748bea4e3c59b8268944b5644b008170d0d4
Murygina <murygina@synyx.de>
2011-11-30 15:30:41 +0100

    Modified/implemented test for getVacationDays. refs #8621

--- src/test/java/org/synyx/urlaubsverwaltung/calendar/OwnCalendarServiceTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/calendar/OwnCalendarServiceTest.java
@@ -16,6 +16,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import org.synyx.urlaubsverwaltung.domain.Application;
+import org.synyx.urlaubsverwaltung.domain.DayLength;
+
+import java.math.BigDecimal;
+
 
 /**
  * @author  Aljona Murygina
@@ -23,6 +28,7 @@ import org.junit.Test;
 public class OwnCalendarServiceTest {
 
     private OwnCalendarService instance;
+    private Application application;
 
     public OwnCalendarServiceTest() {
     }
@@ -41,6 +47,8 @@ public class OwnCalendarServiceTest {
     public void setUp() {
 
         instance = new OwnCalendarService();
+
+        application = new Application();
     }
 
 
@@ -62,34 +70,38 @@ public class OwnCalendarServiceTest {
         assertEquals(9.0, returnValue, 0.0);
     }
 
+
     /** Test of getVacationDays method, of class OwnCalendarService. */
-// @Test
-// public void testGetVacationDays() {
-//
-// // Testen fuer 2010: 17.12. ist Freitag, 31.12. ist Freitag
-// // dazwischen liegen insgesamt 2 Feiertage (25., 26.) plus zwei Mal halbe Tage (24., 31.)
-// // Brutto sind es 14 Tage
-// // Netto sind es unter Beruecksichtigung von Feiertagen und Wochenenden 10 Tage
-//
-// DateMidnight start = new DateMidnight(2010, 12, 17);
-// DateMidnight end = new DateMidnight(2010, 12, 31);
-//
-// Double returnValue = instance.getVacationDays(start, end);
-//
-// assertNotNull(returnValue);
-// assertEquals(10.0, returnValue, 0.0);
-//
-// // Testen fuer 2009: 17.12. ist Donnerstag, 31.12. ist Donnerstag
-// // dazwischen liegen insgesamt 2 Feiertage (25., 26.) plus zwei Mal halbe Tage (24., 31.)
-// // Brutto sind es 14 Tage
-// // Netto sind es unter Beruecksichtigung von Feiertagen und Wochenenden 9 Tage
-//
-// start = new DateMidnight(2009, 12, 17);
-// end = new DateMidnight(2009, 12, 31);
-//
-// returnValue = instance.getVacationDays(start, end);
-//
-// assertNotNull(returnValue);
-// assertEquals(9.0, returnValue, 0.0);
-// }
+    @Test
+    public void testGetVacationDays() {
+
+        // testing for full days
+        application.setHowLong(DayLength.FULL);
+
+        // Testing for 2010: 17.12. is Friday, 31.12. is Friday
+        // between these dates: 2 public holidays (25., 26.) plus 2*0.5 public holidays (24., 31.)
+        // total days: 14
+        // netto days: 10 (considering public holidays and weekends)
+
+        DateMidnight start = new DateMidnight(2010, 12, 17);
+        DateMidnight end = new DateMidnight(2010, 12, 31);
+
+        BigDecimal returnValue = instance.getVacationDays(application, start, end);
+
+        assertNotNull(returnValue);
+        assertEquals(BigDecimal.valueOf(10.0).setScale(2), returnValue);
+
+        // Testing for 2009: 17.12. is Thursday, 31.12. ist Thursday
+        // between these dates: 2 public holidays (25., 26.) plus 2*0.5 public holidays (24., 31.)
+        // total days: 14
+        // netto days: 9 (considering public holidays and weekends)
+
+        start = new DateMidnight(2009, 12, 17);
+        end = new DateMidnight(2009, 12, 31);
+
+        returnValue = instance.getVacationDays(application, start, end);
+
+        assertNotNull(returnValue);
+        assertEquals(BigDecimal.valueOf(9.0).setScale(2), returnValue);
+    }
 }
