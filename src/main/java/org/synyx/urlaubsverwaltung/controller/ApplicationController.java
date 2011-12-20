95c3c1f0f1ecaf0b9fb1162f152c50c855659f81
Murygina <murygina@synyx.de>
2011-12-20 14:45:54 +0100

    Implemented methods for showing application with details to chef and for allow or reject application by chef. refs #8748

--- src/main/java/org/synyx/urlaubsverwaltung/controller/ApplicationController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/controller/ApplicationController.java
@@ -43,8 +43,8 @@ public class ApplicationController {
 
     // jsps
     private static final String SHOW_APP_JSP = "application/show";
-    private static final String SHOW_APP_CHEF_JSP = "application/show_chef";
-    private static final String SHOW_APP_OFFICE_JSP = "application/show_office";
+    private static final String SHOW_APP_CHEF_JSP = "application/app_chef";
+    private static final String SHOW_APP_OFFICE_JSP = "application/app_office";
     private static final String PRINT_VIEW_APP_JSP = "application/print";
     private static final String APP_LIST_JSP = "application/list";
     private static final String APP_FORM_JSP = "application/app_form";
@@ -63,14 +63,29 @@ public class ApplicationController {
 
     // links
     private static final String WEB = "web/";
+
+    // list of applications by state
     private static final String WAITING_APPS = "/application/waiting";
     private static final String ALLOWED_APPS = "/application/allowed";
     private static final String CANCELLED_APPS = "/application/cancelled";
     private static final String REJECTED_APPS = "/application/rejected";
+
+    // list of applications by person
     private static final String APPS_BY_PERSON = "/application/{" + PERSON_ID + "}";
+
+    // form to apply vacation
     private static final String NEW_APP = "/application/{" + PERSON_ID + "}/new";
+
+    // for user: if application has state waiting, user may change various attributes
+    // if application has state allowed, user is able to cancel application, but not to edit it
     private static final String EDIT_APP = "/application/{" + APPLICATION_ID + "}/edit";
+
+    // detailed view of application for chef
     private static final String SHOW_APP = "/application/{" + APPLICATION_ID + "}";
+
+    // allow or reject application
+    private static final String ALLOW_APP = "/application/{" + APPLICATION_ID + "}/allow";
+    private static final String REJECT_APP = "/application/{" + APPLICATION_ID + "}/reject";
 
     // logger
     private static final Logger LOG = Logger.getLogger(PersonController.class);
@@ -238,48 +253,115 @@ public class ApplicationController {
         Person person = personService.getPersonByID(personId);
 
         application.setPerson(person);
-        application.setHowLong(DayLength.FULL);
+
+        DateMidnight date = DateMidnight.now(GregorianChronology.getInstance());
+        application.setApplicationDate(date);
 
         applicationService.save(application);
 
+//        cryptoService and mailService must be modified
 //        applicationService.signApplicationByUser(application, person);
 
         LOG.info(application.getApplicationDate() + " ID: " + application.getId() + " Es wurde ein neuer Antrag von "
             + person.getLastName() + " " + person.getFirstName() + " angelegt.");
 
-        return "redirect: web/staff/list";
+        return "redirect:/web/staff/" + personId + "/overview";
     }
 
 
-//    /**
-//     * view for chef who has to decide if he allows or rejects the application
-//     *
-//     * @param  applicationId
-//     * @param  model
-//     *
-//     * @return
-//     */
-//    @RequestMapping(value = "/application/{applicationId}/chef", method = RequestMethod.GET)
-//    public String showapplicationDetailChef(@PathVariable(application_ID) Integer applicationId, Model model) {
-//
-////        Application application = applicationService.getRequestById(applicationId);
-//
-////        model.addAttribute(application_ATTRIBUTE_NAME, application);
-    // setLoggedUser(model);
-//
-//        return "applications/applicationdetailchef";
-//    }
+    /**
+     * view for chef who has to decide if he allows or rejects the application
+     *
+     * @param  applicationId
+     * @param  model
+     *
+     * @return
+     */
+    @RequestMapping(value = SHOW_APP, method = RequestMethod.GET)
+    public String showApplicationDetailChef(@PathVariable(APPLICATION_ID) Integer applicationId, Model model) {
 
-//    /**
-//     * view for office who can print or edit the request
-//     *
-//     * @param  applicationId
-//     * @param  model
-//     *
-//     * @return
-//     */
-//    @RequestMapping(value = "/application/{applicationId}/office", method = RequestMethod.GET)
-//    public String showapplicationDetailOffice(@PathVariable(APPLICATION_ID) Integer applicationId, Model model) {
+        Application application = applicationService.getApplicationById(applicationId);
+
+        model.addAttribute(APPLICATION, application);
+        setLoggedUser(model);
+
+        return SHOW_APP_CHEF_JSP;
+    }
+
+
+    /**
+     * used if you want to allow an existing request (boss only)
+     *
+     * @param  model
+     *
+     * @return
+     */
+    @RequestMapping(value = ALLOW_APP, method = RequestMethod.PUT)
+    public String allowApplication(@PathVariable(APPLICATION_ID) Integer applicationId, Model model) {
+
+        Application application = applicationService.getApplicationById(applicationId);
+
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Person boss = personService.getPersonByLogin(name);
+
+        applicationService.allow(application, boss);
+
+        LOG.info(application.getApplicationDate() + " ID: " + application.getId() + "Der Antrag von "
+            + application.getPerson().getFirstName() + " " + application.getPerson().getLastName()
+            + " wurde am " + DateMidnight.now().toString("dd.MM.yyyy") + " von " + boss.getFirstName() + " "
+            + boss.getLastName() + " genehmigt.");
+
+        return "";
+    }
+
+
+    /**
+     * used if you want to reject a request (boss only)
+     *
+     * @param  applicationId  the id of the to declining request
+     * @param  reason  the reason of the rejection
+     * @param  model
+     *
+     * @return
+     */
+    @RequestMapping(value = REJECT_APP, method = RequestMethod.PUT)
+    public String rejectApplication(@PathVariable(APPLICATION_ID) Integer applicationId,
+        @ModelAttribute("reasonForRejecting") String reasonForRejecting, Model model) {
+
+        Application application = applicationService.getApplicationById(applicationId);
+
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Person boss = personService.getPersonByLogin(name);
+
+        applicationService.reject(application, boss, reasonForRejecting);
+
+        LOG.info(application.getApplicationDate() + " ID: " + application.getId() + "Der Antrag von "
+            + application.getPerson().getFirstName() + " " + application.getPerson().getLastName()
+            + " wurde am " + DateMidnight.now().toString("dd.MM.yyyy") + " von " + boss.getFirstName() + " "
+            + boss.getLastName() + " abgelehnt.");
+
+        return "";
+    }
+
+
+    private void setLoggedUser(Model model) {
+
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        Person loggedUser = personService.getPersonByLogin(user);
+
+        model.addAttribute("loggedUser", loggedUser);
+    }
+
+    // /**
+// * view for office who can print or edit the request
+// *
+// * @param  applicationId
+// * @param  model
+// *
+// * @return
+// */
+// @RequestMapping(value = "/application/{applicationId}/office", method = RequestMethod.GET)
+// public String showapplicationDetailOffice(@PathVariable(APPLICATION_ID) Integer applicationId, Model model) {
 //
 ////        Application application = applicationService.getRequestById(applicationId);
 ////
@@ -309,84 +391,4 @@ public class ApplicationController {
 //    }
 //
 //
-//    /**
-//     * used if you want to allow an existing request (boss only)
-//     *
-//     * @param  model
-//     *
-//     * @return
-//     */
-//    @RequestMapping(value = "/application/{applicationId}/genehmigen", method = RequestMethod.PUT)
-//    public String approveapplication(@PathVariable(APPLICATION_ID) Integer applicationId, Model model) {
-//
-////        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        // irgendwie an LoginNamen kommen via security stuff
-//        String login = "";
-//
-////        Application application = applicationService.getRequestById(applicationId);
-//        Person boss = personService.getPersonByLogin(login);
-//
-//        // über die logik sollten wir nochmal nachdenken...
-//// applicationService.allow(application);
-//
-//// try {
-//// // Application mit Boss Daten signieren
-//////            applicationService.signApplicationByBoss(application, boss);
-////        } catch (NoSuchAlgorithmException ex) {
-////            java.util.logging.Logger.getLogger(ApplicationController.class.getName()).log(Level.SEVERE, null, ex);
-////        } catch (InvalidKeySpecException ex) {
-////            java.util.logging.Logger.getLogger(ApplicationController.class.getName()).log(Level.SEVERE, null, ex);
-////        }
-//
-//        LOG.info("Ein application wurde genehmigt.");
-//
-//        // benachrichtigungs-zeugs
-//
-//        return "";
-//    }
-//
-//
-//    /**
-//     * used if you want to reject a request (boss only)
-//     *
-//     * @param  applicationId  the id of the to declining request
-//     * @param  reason  the reason of the rejection
-//     * @param  model
-//     *
-//     * @return
-//     */
-//    @RequestMapping(value = "/application/{applicationId}/ablehnen", method = RequestMethod.PUT)
-//    public String declineapplication(@PathVariable(APPLICATION_ID) Integer applicationId,
-//        @ModelAttribute("reasonForDeclining") String reasonForDeclining, Model model) {
-//
-//        // ueber security zeugs an login name kommen
-//        String login = "";
-//
-//        Person boss = personService.getPersonByLogin(login);
-////        Application application = applicationService.getRequestById(applicationId);
-////
-////        applicationService.reject(application, boss, reasonForDeclining);
-////
-////        try {
-////            applicationService.signApplicationByBoss(application, boss);
-////        } catch (NoSuchAlgorithmException ex) {
-////            java.util.logging.Logger.getLogger(ApplicationController.class.getName()).log(Level.SEVERE, null, ex);
-////        } catch (InvalidKeySpecException ex) {
-////            java.util.logging.Logger.getLogger(ApplicationController.class.getName()).log(Level.SEVERE, null, ex);
-////        }
-//
-//        LOG.info("Ein application wurde abgelehnt.");
-//
-//        // benachrichtigungs-zeugs
-//
-//        return "";
-//    }
-
-    private void setLoggedUser(Model model) {
-
-        String user = SecurityContextHolder.getContext().getAuthentication().getName();
-        Person loggedUser = personService.getPersonByLogin(user);
-
-        model.addAttribute("loggedUser", loggedUser);
-    }
 }
