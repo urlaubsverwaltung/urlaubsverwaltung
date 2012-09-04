e77898a85883a609533d4f961818d8dfd3416347
Murygina <murygina@synyx.de>
2012-09-04 10:40:25 +0200

    Refactoring calculation: added Logger and log messages to HolidaysAccountServiceImpl. refs #10887

--- src/main/java/org/synyx/urlaubsverwaltung/service/HolidaysAccountServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/service/HolidaysAccountServiceImpl.java
@@ -1,4 +1,3 @@
-
 package org.synyx.urlaubsverwaltung.service;
 
 import java.math.BigDecimal;
@@ -9,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.synyx.urlaubsverwaltung.dao.AccountDAO;
 import org.synyx.urlaubsverwaltung.domain.Account;
 import org.synyx.urlaubsverwaltung.domain.Person;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -17,47 +17,48 @@ import org.synyx.urlaubsverwaltung.domain.Person;
 @Transactional
 public class HolidaysAccountServiceImpl implements HolidaysAccountService {
 
-//    @Autowired
+    private static final Logger LOG = Logger.getLogger("audit");
     private AccountDAO accountDAO;
-    
+
     @Autowired
     public HolidaysAccountServiceImpl(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
     }
-    
+
     @Override
     public Account getHolidaysAccount(int year, Person person) {
         return accountDAO.getHolidaysAccountByYearAndPerson(year, person);
     }
-    
+
     @Override
     public void createHolidaysAccount(Person person, DateMidnight validFrom, DateMidnight validTo, BigDecimal days, BigDecimal remaining, boolean remainingDaysExpire) {
-        
+
         Account account = new Account(person, validFrom.toDate(), validTo.toDate(), days, remaining, remainingDaysExpire);
         account.calculateActualVacationDays();
         accountDAO.save(account);
-        
+
+        LOG.info("Created holidays account for " + person.getLoginName() + " with following values: " + "{validFrom: " + validFrom.toString("dd.MM.yyyy") + ", validTo: " + validTo.toString("dd.MM.yyyy") + ", annualVacationDays: " + days + ", remainingDays: " + remaining + ", remainingDaysExpiring: " + remainingDaysExpire + "}");
+
     }
 
     @Override
     public void editHolidaysAccount(Account account, DateMidnight validFrom, DateMidnight validTo, BigDecimal days, BigDecimal remaining, boolean remainingDaysExpire) {
-        
+
         account.setValidFrom(validFrom);
         account.setValidTo(validTo);
         account.setAnnualVacationDays(days);
         account.setRemainingVacationDays(remaining);
         account.setRemainingVacationDaysExpire(remainingDaysExpire);
         account.calculateActualVacationDays();
-        
-        accountDAO.save(account);
-    }
 
+        accountDAO.save(account);
+        
+        LOG.info("Edited holidays account of " + account.getPerson().getLoginName() + " with following values: " + "{validFrom: " + validFrom.toString("dd.MM.yyyy") + ", validTo: " + validTo.toString("dd.MM.yyyy") + ", annualVacationDays: " + days + ", remainingDays: " + remaining + ", remainingDaysExpiring: " + remainingDaysExpire + "}");
+    }
 
     @Override
     public void updateHolidaysAccounts(List<Person> persons, int year) {
         // TODO write method update account
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
-    
 }
