7e749fb8d9c5ca97aecaed1a78eb74cdb82e6f37
Murygina <murygina@synyx.de>
2013-07-17 14:12:15 +0200

    StartupService: fixed logging message
    refs #13618

--- src/main/java/org/synyx/urlaubsverwaltung/startup/StartupService.java
+++ src/main/java/org/synyx/urlaubsverwaltung/startup/StartupService.java
@@ -1,51 +1,49 @@
 
 package org.synyx.urlaubsverwaltung.startup;
 
-import java.io.IOException;
-import javax.annotation.PostConstruct;
 import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.synyx.urlaubsverwaltung.calendar.GoogleCalendarService;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
 
 /**
- *
- * @author Aljona Murygina - murygina@synyx.de
+ * @author  Aljona Murygina - murygina@synyx.de
  */
 public class StartupService {
-    
+
     private static final Logger LOG = Logger.getLogger(StartupService.class);
-    
+
     @Value("${username}")
-    private String dbName;
-    
+    private String dbUser;
+
     @Value("${url}")
     private String dbUrl;
-    
+
     @Value("${email.boss}")
     private String emailBoss;
-    
+
     @Value("${email.office}")
     private String emailOffice;
-    
+
     @Value("${email.all}")
     private String emailAll;
-    
+
     @Value("${email.manager}")
     private String emailManager;
-    
-    
+
     @PostConstruct
     public void logStartupInfo() throws IOException {
-        
-        LOG.info("Using database with username " + dbName + " and Url " + dbUrl);
-        
+
+        LOG.info("Using database " + dbUrl + " with user " + dbUser);
+
         LOG.info("Using following email addresses for notification:");
         LOG.info("Email boss: " + emailBoss);
         LOG.info("Email office: " + emailOffice);
         LOG.info("Email all: " + emailAll);
         LOG.info("Email manager: " + emailManager);
-        
     }
-    
-    
 }
