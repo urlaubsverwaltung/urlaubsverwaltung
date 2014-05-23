f77257354b847be9f62af33c5969aba078778688
Aljona Murygina <murygina@synyx.de>
2014-05-23 16:22:16 +0200

    Add api for persons in application
    * refs #16363

---
+++ src/main/java/org/synyx/urlaubsverwaltung/restapi/PersonController.java
@@ -0,0 +1,60 @@
+package org.synyx.urlaubsverwaltung.restapi;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.synyx.urlaubsverwaltung.person.Person;
+import org.synyx.urlaubsverwaltung.person.PersonService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author  Aljona Murygina - murygina@synyx.de
+ */
+@Controller
+public class PersonController {
+
+    private static final String ROOT_URL = "/persons";
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping(value = ROOT_URL, method = RequestMethod.GET)
+    @ModelAttribute("response")
+    public PersonListResponse persons(@RequestParam(value = "ldap", required = false) String ldapName) {
+
+        List<Person> persons = new ArrayList<Person>();
+
+        if (ldapName == null) {
+            persons = personService.getAllPersons();
+        } else {
+            Person person = personService.getPersonByLogin(ldapName);
+
+            if (person != null) {
+                persons.add(person);
+            }
+        }
+
+        List<PersonResponse> personResponses = Lists.transform(persons, new Function<Person, PersonResponse>() {
+
+                    @Override
+                    public PersonResponse apply(Person person) {
+
+                        return new PersonResponse(person);
+                    }
+                });
+
+        return new PersonListResponse(personResponses);
+    }
+}
---
+++ src/main/java/org/synyx/urlaubsverwaltung/restapi/PersonResponse.java
@@ -0,0 +1,73 @@
+package org.synyx.urlaubsverwaltung.restapi;
+
+import org.synyx.urlaubsverwaltung.person.Person;
+
+
+/**
+ * @author  Aljona Murygina - murygina@synyx.de
+ */
+public class PersonResponse {
+
+    private String ldapName;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    public PersonResponse(Person person) {
+
+        this.ldapName = person.getLoginName();
+        this.email = person.getEmail();
+        this.firstName = person.getFirstName();
+        this.lastName = person.getLastName();
+    }
+
+    public String getLdapName() {
+
+        return ldapName;
+    }
+
+
+    public void setLdapName(String ldapName) {
+
+        this.ldapName = ldapName;
+    }
+
+
+    public String getEmail() {
+
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+
+        return lastName;
+    }
+
+
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+}
