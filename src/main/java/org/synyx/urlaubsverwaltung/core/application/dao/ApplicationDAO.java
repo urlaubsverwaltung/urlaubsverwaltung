22108caa49f2c659cdcbff6c22d69ca8be2431bc
Aljona Murygina <murygina@synyx.de>
2015-01-20 17:53:55 +0100

    Fix DAO query with deprecated application for leave state
    * State of applications for leave is represented now as String in database
      instead of Integer.
    
    * fixes #19018

--- src/main/java/org/synyx/urlaubsverwaltung/core/application/dao/ApplicationDAO.java
+++ src/main/java/org/synyx/urlaubsverwaltung/core/application/dao/ApplicationDAO.java
@@ -94,7 +94,7 @@ public interface ApplicationDAO extends JpaRepository<Application, Integer> {
 
     @Query(
         "select x from Application x where ((x.startDate between ?1 and ?2) or (x.endDate between ?1 and ?2) or (x.startDate < ?1 and x.endDate > ?2)) "
-        + "and x.person = ?3 and (x.status = 0 or x.status = 1) and x.howLong = ?4 order by x.startDate"
+        + "and x.person = ?3 and (x.status = 'WAITING' or x.status = 'ALLOWED') and x.howLong = ?4 order by x.startDate"
     )
     List<Application> getRelevantActiveApplicationsByPeriodAndDayLength(Date startDate, Date endDate, Person person,
         DayLength length);
@@ -102,7 +102,7 @@ public interface ApplicationDAO extends JpaRepository<Application, Integer> {
 
     @Query(
         "select x from Application x where ((x.startDate between ?1 and ?2) or (x.endDate between ?1 and ?2) or (x.startDate < ?1 and x.endDate > ?2)) "
-        + "and x.person = ?3 and (x.status = 0 or x.status = 1) order by x.startDate"
+        + "and x.person = ?3 and (x.status = 'WAITING' or x.status = 'ALLOWED') order by x.startDate"
     )
     List<Application> getRelevantActiveApplicationsByPeriodForEveryDayLength(Date startDate, Date endDate,
         Person person);
