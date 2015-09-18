9cffa62f7d0c2914583a53b1da6d2acb805a8de3
Vincent <potucek@synyx.de>
2015-09-18 16:45:44 +0200

    refactored PublicHolidayController

--- src/main/java/org/synyx/urlaubsverwaltung/restapi/PublicHolidayController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/restapi/PublicHolidayController.java
@@ -3,25 +3,14 @@ package org.synyx.urlaubsverwaltung.restapi;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
-
-import de.jollyday.Holiday;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
-
-import org.springframework.util.StringUtils;
-
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import org.synyx.urlaubsverwaltung.core.calendar.PublicHolidaysService;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 
@@ -50,33 +39,19 @@ public class PublicHolidayController {
         @RequestParam(value = "month", required = false)
         String month) {
 
-        PublicHolidayListResponse emptyResponse = new PublicHolidayListResponse();
+        int parsedYear, parsedMonth;
 
-        boolean hasYear = StringUtils.hasText(year);
-        boolean hasMonth = StringUtils.hasText(month);
-
-        Set<Holiday> holidays = new HashSet<>();
-
-        if (hasYear && !hasMonth) {
-            try {
-                holidays = publicHolidaysService.getHolidays(Integer.parseInt(year));
-            } catch (NumberFormatException ex) {
-                return emptyResponse;
-            }
+        try {
+            parsedYear = Integer.parseInt(year);
+            parsedMonth = Integer.parseInt(month);
+        } catch (NumberFormatException ex) {
+            return new PublicHolidayListResponse();
         }
 
-        if (hasYear && hasMonth) {
-            try {
-                holidays = publicHolidaysService.getHolidays(Integer.parseInt(year), Integer.parseInt(month));
-            } catch (NumberFormatException ex) {
-                return emptyResponse;
-            }
-        }
-
-        List<PublicHolidayResponse> publicHolidayResponses = holidays.stream().
-                map(holiday -> new PublicHolidayResponse(holiday, publicHolidaysService.getWorkingDurationOfDate(holiday.getDate().toDateMidnight()))).
-                collect(Collectors.toList());
-
-        return new PublicHolidayListResponse(publicHolidayResponses);
+        return new PublicHolidayListResponse(
+                publicHolidaysService.getHolidays(parsedYear, parsedMonth)
+                        .stream()
+                        .map(holiday -> new PublicHolidayResponse(holiday, publicHolidaysService.getWorkingDurationOfDate(holiday.getDate().toDateMidnight()))).
+                        collect(Collectors.toList()));
     }
 }
