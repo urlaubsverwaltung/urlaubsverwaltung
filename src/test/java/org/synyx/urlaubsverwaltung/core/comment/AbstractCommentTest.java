f06636b3bed7d67e3fa5a3d895caa2fe1e767cbf
Aljona Murygina <murygina@synyx.de>
2015-10-13 15:03:50 +0200

    Add missing unit test for abstract comment
    * refs #109

--- src/test/java/org/synyx/urlaubsverwaltung/core/comment/AbstractCommentTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/core/comment/AbstractCommentTest.java
@@ -1,9 +1,30 @@
 package org.synyx.urlaubsverwaltung.core.comment;
 
+import org.joda.time.DateMidnight;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
 /**
- * Don't forget the Java-Doc!
- *
  * @author  Aljona Murygina - murygina@synyx.de
  */
 public class AbstractCommentTest {
+
+    @Test
+    public void ensureHasDateSetAfterInitialization() {
+
+        TestComment comment = new TestComment();
+
+        Assert.assertNotNull("Should not be null", comment.getDate());
+        Assert.assertEquals("Wrong date", DateMidnight.now(), comment.getDate());
+    }
+
+    private class TestComment extends AbstractComment {
+
+        private TestComment() {
+
+            super();
+        }
+    }
 }
