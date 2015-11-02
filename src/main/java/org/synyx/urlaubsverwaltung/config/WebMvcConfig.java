6a53782a099df5aa00328dc3976b9ea02baadfbd
Aljona Murygina <murygina@synyx.de>
2015-11-02 18:53:39 +0100

    Fix performance problem due to JSP compilation
    * refs #88

--- src/main/java/org/synyx/urlaubsverwaltung/config/WebMvcConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/config/WebMvcConfig.java
@@ -1,7 +1,16 @@
 package org.synyx.urlaubsverwaltung.config;
 
+import org.apache.catalina.Container;
+import org.apache.catalina.Context;
+import org.apache.catalina.Wrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
@@ -22,10 +31,45 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private UserInterceptor userInterceptor;
 
+    @Value("${jsp.mode.development}")
+    private String developmentMode;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         InterceptorRegistration interceptorRegistration = registry.addInterceptor(userInterceptor);
         interceptorRegistration.addPathPatterns("/**").excludePathPatterns("/api/**");
+    }
+
+    /**
+     * NOTE: Fix performance problem due to issue: https://github.com/spring-projects/spring-boot/issues/2825
+     */
+    @Bean
+    public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    customizeTomcat((TomcatEmbeddedServletContainerFactory) container);
+                }
+            }
+
+            private void customizeTomcat(TomcatEmbeddedServletContainerFactory tomcatFactory) {
+                tomcatFactory.addContextCustomizers(new TomcatContextCustomizer() {
+
+                    @Override
+                    public void customize(Context context) {
+                        Container jsp = context.findChild("jsp");
+                        if (jsp instanceof Wrapper) {
+                            ((Wrapper) jsp).addInitParameter("development", developmentMode);
+                        }
+
+                    }
+
+                });
+            }
+
+        };
     }
 }
