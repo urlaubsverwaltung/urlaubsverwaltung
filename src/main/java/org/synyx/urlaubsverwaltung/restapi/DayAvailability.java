4e1837c94739e566e8282eab113aeaae292300c3
Timo Eifler <eifler@synyx.de>
2016-10-24 14:32:45 +0200

    enhance availability api to return more precise infos
    * the information how many hours a person is expected to work per day
      is not part of this application and is therefore not part of the api
      anymore - instead the expected ratio is returned
    * the api now also returns the ratio for each absence span seperately
    * for each absence span the relevant part of the Day is noted
      (morning/noon)

--- src/main/java/org/synyx/urlaubsverwaltung/restapi/DayAvailability.java
+++ src/main/java/org/synyx/urlaubsverwaltung/restapi/DayAvailability.java
@@ -1,32 +1,24 @@
 package org.synyx.urlaubsverwaltung.restapi;
 
+import org.synyx.urlaubsverwaltung.core.period.DayLength;
+
 import java.math.BigDecimal;
+
 import java.util.List;
 
 
 /**
  * Represents the availability for a person on a given day. Also contains the reason for being absent.
  *
- * @author Marc Kannegiesser - kannegiesser@synyx.de
+ * @author  Marc Kannegiesser - kannegiesser@synyx.de
+ * @author  Timo Eifler - eifler@synyx.de
  */
 class DayAvailability {
 
+    static class TimedAbsence {
 
-    static class TimedAbsence extends Absence {
-        private final BigDecimal hours;
-
-        public TimedAbsence(BigDecimal hours, Type type) {
-            super(type);
-            this.hours = hours;
-        }
-
-        public BigDecimal getHours() {
-            return hours;
-        }
-    }
-
-    static class Absence {
         enum Type {
+
             VACATION,
             SICK_NOTE,
             WORK,
@@ -35,23 +27,41 @@ class DayAvailability {
         }
 
         private final Type type;
+        private final BigDecimal availabilityRatio;
+        private final String partOfDay;
 
-        public Absence(Type type) {
+        public TimedAbsence(DayLength dayLength, Type type) {
+
             this.type = type;
+            this.availabilityRatio = dayLength.getDuration();
+            this.partOfDay = dayLength.name();
         }
 
         public Type getType() {
+
             return type;
+        }
+
+
+        public BigDecimal getAvailabilityRatio() {
+
+            return availabilityRatio;
+        }
+
+
+        public String getPartOfDay() {
+
+            return partOfDay;
         }
     }
 
-
     private final String date;
-    private final BigDecimal hoursAvailable;
-    private final List<Absence> spans;
+    private final BigDecimal availabilityRatio;
+    private final List<TimedAbsence> spans;
 
-    public DayAvailability(BigDecimal hoursAvailable, String date, List<Absence> spans) {
-        this.hoursAvailable = hoursAvailable;
+    public DayAvailability(BigDecimal availabilityRatio, String date, List<TimedAbsence> spans) {
+
+        this.availabilityRatio = availabilityRatio;
         this.date = date;
         this.spans = spans;
     }
@@ -61,11 +71,15 @@ class DayAvailability {
         return date;
     }
 
-    public List<Absence> getAbsenceSpans() {
+
+    public List<TimedAbsence> getAbsenceSpans() {
+
         return spans;
     }
 
-    public BigDecimal getHoursAvailable() {
-        return hoursAvailable;
+
+    public BigDecimal getAvailabilityRatio() {
+
+        return availabilityRatio;
     }
 }
