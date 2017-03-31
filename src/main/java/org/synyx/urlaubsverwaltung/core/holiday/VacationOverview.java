4f31ec38e456ac950735989c1d4b8d2c494b6442
andre <mail@andre-janus.de>
2017-03-31 15:02:08 +0200

    first draft

---
+++ src/main/java/org/synyx/urlaubsverwaltung/core/holiday/VacationOverview.java
@@ -0,0 +1,36 @@
+package org.synyx.urlaubsverwaltung.core.holiday;
+
+import org.synyx.urlaubsverwaltung.restapi.PersonResponse;
+
+import java.util.List;
+
+public class VacationOverview {
+
+	private PersonResponse person;
+	private Integer personID;
+	private List<ColoredDay> days;
+
+	public PersonResponse getPerson() {
+		return person;
+	}
+
+	public void setPerson(PersonResponse person) {
+		this.person = person;
+	}
+
+	public List<ColoredDay> getDays() {
+		return days;
+	}
+
+	public void setDays(List<ColoredDay> days) {
+		this.days = days;
+	}
+
+	public Integer getPersonID() {
+		return personID;
+	}
+
+	public void setPersonID(Integer personID) {
+		this.personID = personID;
+	}
+}
