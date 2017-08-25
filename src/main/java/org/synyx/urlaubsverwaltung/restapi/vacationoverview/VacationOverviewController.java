f6575efef0de0e5be341d720864c38e32daa6f7f
Daniel Hammann <hammann@synyx.de>
2017-08-25 12:40:15 +0200

    introduce VacationOverviewService for better seperation

--- src/main/java/org/synyx/urlaubsverwaltung/restapi/vacationoverview/VacationOverviewController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/restapi/vacationoverview/VacationOverviewController.java
@@ -30,18 +30,11 @@ import java.util.List;
 @RequestMapping("/api")
 public class VacationOverviewController {
 
-    public static final String DATE_FORMAT = "yyyy-MM-dd";
-    private DepartmentService departmentService;
-    private WorkingTimeService workingTimeService;
-    private PublicHolidaysService publicHolidayService;
+    private VacationOverviewService vacationOverviewService;
 
     @Autowired
-    VacationOverviewController(DepartmentService departmentService, WorkingTimeService workingTimeService,
-                               PublicHolidaysService publicHolidayService, SessionService sessionService) {
-
-        this.departmentService = departmentService;
-        this.workingTimeService = workingTimeService;
-        this.publicHolidayService = publicHolidayService;
+    VacationOverviewController(VacationOverviewService vacationOverviewService) {
+        this.vacationOverviewService = vacationOverviewService;
     }
 
     @ApiOperation(
@@ -53,69 +46,9 @@ public class VacationOverviewController {
             @RequestParam("selectedYear") Integer selectedYear,
             @RequestParam("selectedMonth") Integer selectedMonth) {
 
-
-        List<VacationOverview> holidayOverviewList = new ArrayList<>();
-
-        Department department = getDepartmentByName(selectedDepartment);
-
-        if (department != null) {
-
-            for (Person person : department.getMembers()) {
-
-                DateMidnight date = new DateMidnight();
-                int year = selectedYear != null ? selectedYear : date.getYear();
-                int month = selectedMonth != null ? selectedMonth : date.getMonthOfYear();
-                DateMidnight lastDay = DateUtil.getLastDayOfMonth(year, month);
-
-                VacationOverview holidayOverview = getVacationOverview(person);
-
-                for (int i = 1; i <= lastDay.toDate().getDate(); i++) {
-
-                    DayOfMonth dayOfMonth = new DayOfMonth();
-                    DateMidnight currentDay = new DateMidnight(year, month, i);
-                    dayOfMonth.setDayText(currentDay.toString(DATE_FORMAT));
-                    dayOfMonth.setDayNumber(i);
-
-                    dayOfMonth.setTypeOfDay(getTypeOfDay(person, currentDay));
-                    holidayOverview.getDays().add(dayOfMonth);
-                }
-                holidayOverviewList.add(holidayOverview);
-            }
-        }
+        List<VacationOverview> holidayOverviewList =
+                vacationOverviewService.getVacationOverviews(selectedDepartment, selectedYear, selectedMonth);
 
         return new ResponseWrapper<>(new VacationOverviewResponse(holidayOverviewList));
-    }
-
-    private TypeOfDay getTypeOfDay(Person person, DateMidnight currentDay) {
-        TypeOfDay typeOfDay;
-
-        FederalState state = workingTimeService.getFederalStateForPerson(person, currentDay);
-        if (DateUtil.isWorkDay(currentDay)
-                && (publicHolidayService.getWorkingDurationOfDate(currentDay, state).longValue() > 0)) {
-
-            typeOfDay = TypeOfDay.WORKDAY;
-        } else {
-            typeOfDay = TypeOfDay.WEEKEND;
-        }
-        return typeOfDay;
-    }
-
-    private Department getDepartmentByName(@RequestParam("selectedDepartment") String selectedDepartment) {
-        Department department = null;
-        for (Department item : departmentService.getAllDepartments()) {
-            if (item.getName().equals(selectedDepartment)) {
-                department = item;
-                break;
-            }
-        }
-        return department;
-    }
-
-    private VacationOverview getVacationOverview(Person person) {
-        VacationOverview vacationOverview = new VacationOverview();
-        vacationOverview.setDays(new ArrayList<>());
-        vacationOverview.setPerson(new PersonResponse(person));
-        vacationOverview.setPersonID(person.getId());
-        return vacationOverview;
     }
 }
