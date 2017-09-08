6642ea52bb6957674986a9a345a5968fe8aa8b5a
Daniel Hammann <hammann@synyx.de>
2017-09-08 16:43:58 +0200

    Start OAuth Handshake
    refs: #8

---
+++ src/main/java/org/synyx/urlaubsverwaltung/web/settings/GoogleCalendarOAuthHandshakeController.java
@@ -0,0 +1,124 @@
+package org.synyx.urlaubsverwaltung.web.settings;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets.Details;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+import org.synyx.urlaubsverwaltung.core.settings.GoogleCalendarSettings;
+import org.synyx.urlaubsverwaltung.core.settings.Settings;
+import org.synyx.urlaubsverwaltung.core.settings.SettingsService;
+import org.synyx.urlaubsverwaltung.security.SecurityRules;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Collections;
+
+@Controller
+@RequestMapping("/web")
+public class GoogleCalendarOAuthHandshakeController {
+
+    private final static Log logger = LogFactory.getLog(GoogleCalendarOAuthHandshakeController.class);
+    private static final String APPLICATION_NAME = "Urlaubsverwaltung";
+    private static final String REDIRECT_REL = "/google-api-handshake";
+    private static final String REDIRECT_URL = "http://localhost:8080" + REDIRECT_REL;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    private static HttpTransport httpTransport;
+
+    private final SettingsService settingsService;
+
+    private GoogleAuthorizationCodeFlow flow;
+
+    @Autowired
+    public GoogleCalendarOAuthHandshakeController(SettingsService settingsService) {
+        this.settingsService = settingsService;
+    }
+
+    @PreAuthorize(SecurityRules.IS_OFFICE)
+    @GetMapping(REDIRECT_REL)
+    public RedirectView googleConnectionStatus(HttpServletRequest request) throws Exception {
+        return new RedirectView(authorize());
+    }
+
+    @PreAuthorize(SecurityRules.IS_OFFICE)
+    @GetMapping(value = REDIRECT_REL, params = "code")
+    public ResponseEntity<String> oauth2Callback(@RequestParam(value = "code") String code) {
+        try {
+            TokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URL).execute();
+            Credential credential = flow.createAndStoreCredential(response, "userID");
+            com.google.api.services.calendar.Calendar client =
+                    new com.google.api.services.calendar.Calendar.Builder(httpTransport, JSON_FACTORY, credential)
+                            .setApplicationName(APPLICATION_NAME).build();
+
+            Settings settings = settingsService.getSettings();
+            HttpResponse httpResponse = checkGoogleCalendar(client, settings);
+
+            if (httpResponse.getStatusCode() == 200) {
+                logger.info("OAuth Handshake was successfull!");
+                settings.getCalendarSettings().getGoogleCalendarSettings()
+                        .setRefreshToken(credential.getRefreshToken());
+                settingsService.save(settings);
+            } else {
+                logger.warn("OAuth handshake error " + httpResponse.getStatusMessage());
+            }
+
+        } catch (Exception e) {
+            logger.error("Exception while handling OAuth2 callback (" + e.getMessage() + ")."
+                    + " Redirecting to google connection status page.");
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private HttpResponse checkGoogleCalendar(Calendar client, Settings settings) throws IOException {
+        String calendarId = settings.getCalendarSettings().getGoogleCalendarSettings().getCalendarId();
+        Calendar.Calendars.Get metadata = client.calendars().get(calendarId);
+        return metadata.buildHttpRequestUsingHead().execute();
+    }
+
+    private String authorize() throws Exception {
+        AuthorizationCodeRequestUrl authorizationUrl;
+
+        if (flow == null) {
+            Details web = new Details();
+            GoogleCalendarSettings googleCalendarSettings =
+                    settingsService.getSettings().getCalendarSettings().getGoogleCalendarSettings();
+
+            web.setClientId(googleCalendarSettings.getClientId());
+            web.setClientSecret(googleCalendarSettings.getClientSecret());
+
+            GoogleClientSecrets clientSecrets = new GoogleClientSecrets();
+
+            clientSecrets.setWeb(web);
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+            flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets,
+                    Collections.singleton(CalendarScopes.CALENDAR)).build();
+        }
+
+        authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URL);
+
+        logger.info("using authorizationUrl " + authorizationUrl);
+        return authorizationUrl.build();
+    }
+}
