498d73f02d0c42b0d4cd55e9d29066f37775b687
Daniel Hammann <hammann@synyx.de>
2017-09-08 16:43:43 +0200

    Google Calendar Settings updated
    refs: #8

--- src/main/java/org/synyx/urlaubsverwaltung/core/settings/GoogleCalendarSettings.java
+++ src/main/java/org/synyx/urlaubsverwaltung/core/settings/GoogleCalendarSettings.java
@@ -7,7 +7,7 @@ import javax.persistence.Embeddable;
 /**
  * Settings to sync absences with a Google calendar.
  *
- * @author  Aljona Murygina - murygina@synyx.de
+ * @author Aljona Murygina - murygina@synyx.de
  */
 @Embeddable
 public class GoogleCalendarSettings {
@@ -15,8 +15,41 @@ public class GoogleCalendarSettings {
     @Column(name = "calendar_google_active")
     private boolean active = false;
 
-    @Column(name = "calendar_google_calendarId")
+    @Column(name = "calendar_google_client_id")
+    private String clientId;
+
+    @Column(name = "calendar_google_client_secret")
+    private String clientSecret;
+
+    @Column(name = "calendar_google_calendar_id")
     private String calendarId;
+
+    @Column(name = "calendar_google_refresh_token")
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
 
     public boolean isActive() {
         return active;
