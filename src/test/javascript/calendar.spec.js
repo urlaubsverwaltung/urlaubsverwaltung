1e5cb62645ff9f659057330ef8cc30ef33dc1302
Benjamin Seber <seber@synyx.de>
2017-12-02 13:13:16 +0100

    added snapshot test for calendar rendering

--- src/test/javascript/calendar.spec.js
+++ src/test/javascript/calendar.spec.js
@@ -1,5 +1,29 @@
+import { setup } from './TestSetupHelper';
+
 describe ('calendar', () => {
-    it ('works', () => {
-        expect(true).toBe(true);
+    beforeEach (calendarTestSetup);
+
+    it ('renders', () => {
+        // 01.12.2017
+        const referenceDate = new Date(1512130448379);
+
+        const webPrefix = 'webPrefix';
+        const apiPrefix = 'apiPrefix';
+        const personId = 'personId';
+        const holidayService = window.Urlaubsverwaltung.HolidayService.create();
+        window.Urlaubsverwaltung.Calendar.init(holidayService, referenceDate);
+
+        expect(document.body).toMatchSnapshot();
     });
+
+    async function calendarTestSetup () {
+        await setup();
+
+        jest.spyOn(window.jQuery, 'ajax').mockReturnValue(Promise.reject());
+        window.moment = await import('moment');
+
+        document.body.innerHTML = `<div id="datepicker"></div>`;
+
+        await import('../../main/resources/static/js/calendar.js');
+    }
 });
