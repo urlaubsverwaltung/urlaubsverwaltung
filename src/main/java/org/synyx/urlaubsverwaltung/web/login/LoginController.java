44f57ece0028fa6ad71831bbaa2bc818d905a3aa
fug4zi <gloop@web.de>
2018-02-04 21:22:14 +0100

    Update LoginController.java

--- src/main/java/org/synyx/urlaubsverwaltung/web/login/LoginController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/web/login/LoginController.java
@@ -19,16 +19,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LoginController {
 
     private final String applicationVersion;
+    private final String authenticationMethod;
 
     @Autowired
-    public LoginController(@Value("${info.app.version}") String applicationVersion) {
+    public LoginController(@Value("${info.app.version}") String applicationVersion,
+			@Value("${auth}") String authenticationMethod) {
 
         this.applicationVersion = applicationVersion;
+        this.authenticationMethod = authenticationMethod;
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String login(Model model) {
+        if (authenticationMethod.equals("azure")) {
+			UUID state = UUID.randomUUID();
+			UUID nonce = UUID.randomUUID();
 
+			// Save the state and nonce in the session so we can
+			// verify after the auth process redirects back
+			HttpSession session = request.getSession();
+
+			session.setAttribute("expected_state", state);
+			session.setAttribute("expected_nonce", nonce);
+
+			String loginUrl = AuthHelper.getLoginUrl(state, nonce);
+			model.addAttribute("loginUrl", loginUrl);
+		}
+		model.addAttribute("auth", authenticationMethod);
         model.addAttribute("version", applicationVersion);
 
         return "login/login";
