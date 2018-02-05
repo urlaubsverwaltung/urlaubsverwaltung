5ff930e91ec6284df234c999f3c7bba9cb3aaa02
fug4zi <gloop@web.de>
2018-02-05 21:58:19 +0100

    Add files via upload

--- src/main/java/org/synyx/urlaubsverwaltung/web/login/LoginController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/web/login/LoginController.java
@@ -1,37 +1,45 @@
 package org.synyx.urlaubsverwaltung.web.login;
 
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.synyx.urlaubsverwaltung.security.AzureAuthHelper;
 
 /**
- * @author  David Schilling - schilling@synyx.de
+ * @author David Schilling - schilling@synyx.de
  */
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
-    private final String applicationVersion;
-    private final String authenticationMethod;
+	@Autowired
+	AzureAuthHelper AuthHelper;
+	private final String applicationVersion;
+	private final String authenticationMethod;
 
-    @Autowired
-    public LoginController(@Value("${info.app.version}") String applicationVersion,
-			@Value("${auth}") String authenticationMethod) {
+	@Autowired
+	public LoginController(@Value("${info.app.version}") String applicationVersion,
+			@Value("${auth}") String authenticationMethod
+			) {
 
-        this.applicationVersion = applicationVersion;
-        this.authenticationMethod = authenticationMethod;
-    }
+		this.applicationVersion = applicationVersion;
+		this.authenticationMethod = authenticationMethod;
+	}
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String login(Model model) {
-        if (authenticationMethod.equals("azure")) {
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String login(Model model, HttpServletRequest request) {
+		if (authenticationMethod.equals("azure")) {
 			UUID state = UUID.randomUUID();
 			UUID nonce = UUID.randomUUID();
 
@@ -46,8 +54,8 @@ public class LoginController {
 			model.addAttribute("loginUrl", loginUrl);
 		}
 		model.addAttribute("auth", authenticationMethod);
-        model.addAttribute("version", applicationVersion);
+		model.addAttribute("version", applicationVersion);
 
-        return "login/login";
-    }
+		return "login/login";
+	}
 }
