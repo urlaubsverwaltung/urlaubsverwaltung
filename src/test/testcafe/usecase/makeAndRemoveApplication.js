b76e56552c281e939f44805dedf190c1ad9a15d2
Christian Lange <lange@synyx.de>
2018-05-10 15:37:58 +0200

    [test] added use case that makes and removes application
    * is looking first for the first day in month that could be used for
      holiday application

--- src/test/testcafe/usecase/makeAndRemoveApplication.js
+++ src/test/testcafe/usecase/makeAndRemoveApplication.js
@@ -3,16 +3,26 @@ import { Selector } from 'testcafe';
 fixture `overview.jsp`
     .page `http://localhost:8080/login`;
 
-function formatDate(date) {
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
+let day;
+
+const format = (formatStr, ...args) => {
+    return formatStr.replace(/\{(\d+)\}/g, (m, i) => {
+        return args[i];
+    })
+};
+
+function formatDateYMD(formatStr, date) {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
 
-    return `${day}.${month}.${year}`;
+    return format(formatStr, year, month, day);
 }
 
-test('new application as user', async t => {
-    const dayOfApplication = formatDate(new Date(2018,0,3));
+test.only('new application as user', async t => {
 
     await t
         .maximizeWindow() // otherwise not all buttons are visible
@@ -20,6 +30,25 @@ test('new application as user', async t => {
         .typeText('#password', 'secret')
         .click('#submit')
         .click('#application_new')
+        .click('#from');
+
+    let count = await Selector('td').count;
+
+    // find first day in this month were a vacation application can be done
+    for (let i=0; i<count; ++i) {
+        let tdHandle = Selector('td').nth(i);
+
+        let td = await tdHandle();
+        const workday = td.classNames.indexOf('') > -1; // has an empty cssClass
+        day = td.innerText.replace(/[^0-9]*/g, '');
+        if (workday) {
+            break;
+        }
+    }
+
+    const dayOfApplication = formatDateYMD('{2}.{1}.{0}', new Date(year,month,day));
+
+    await t
         .typeText('#from', dayOfApplication)
         .pressKey('tab')
         .typeText('#to', dayOfApplication)
@@ -37,8 +66,8 @@ test('new application as user', async t => {
 
 });
 
-test('remove my application as user', async t => {
-    const dayOfApplication = formatDate(new Date());
+test.only('remove my application as user', async t => {
+    const dayOfApplicationDatePicker = formatDateYMD('{0}-0{1}-0{2}', new Date(year,month,day));
 
     await t
         .maximizeWindow() // otherwise not all buttons are visible
@@ -46,16 +75,17 @@ test('remove my application as user', async t => {
         .typeText('#password', 'secret')
         .click('#submit');
 
+    let headerText = await Selector('.datepicker-month').find('h3').nth(3).textContent;
 
-    const today = Selector('span').withAttribute('data-datepicker-date', '2018-04-20');
-    await t.click(today);
+    const dayToRemove = Selector('span').withAttribute('data-datepicker-date', dayOfApplicationDatePicker);
+    await t.click(dayToRemove);
 
     const rmVacation = Selector('i').withAttribute('class', 'fa fa-trash');
     await t.click(rmVacation);
 
     const stornButton = Selector('button').withAttribute('class', 'btn btn-danger col-xs-12 col-sm-5');
     await t.click(stornButton);
-    /*
+
     const location = await t.eval(() => window.location);
     // example location: /web/application/38
     const pathParts = location.pathname.split('/');
@@ -64,5 +94,4 @@ test('remove my application as user', async t => {
         .expect(pathParts[2]).eql('application')
         .expect(pathParts[3]).ok(`location: ${location.pathname}`)
         .expect(Number.isInteger(Number(pathParts[3]))).ok(`location: ${location.pathname}`);
-*/
 });
