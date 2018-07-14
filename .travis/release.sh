6091a0621e72be1f426517624800c7c7c2e38535
Christian Lange <lange@synyx.de>
2018-07-14 13:50:15 +0200

    [travis] moving from release.sh to travis.yml we do not need release.sh

--- .travis/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Ensuring that pom <version> matches $TRAVIS_TAG"
-./mvnw org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
-
-echo "Uploading to oss repo and GitHub"
-./mvnw deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
