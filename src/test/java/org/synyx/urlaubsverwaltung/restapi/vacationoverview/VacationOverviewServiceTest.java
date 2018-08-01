3faabd08291d31b6ab6ac07e82cab7e5dff021f5
Peter Bolch <pbolch@contargo.net>
2018-08-01 22:41:56 +0200

    Use Builders in a Test

--- src/test/java/org/synyx/urlaubsverwaltung/restapi/vacationoverview/VacationOverviewServiceTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/restapi/vacationoverview/VacationOverviewServiceTest.java
@@ -1,9 +1,10 @@
 package org.synyx.urlaubsverwaltung.restapi.vacationoverview;
 
 import org.joda.time.DateMidnight;
-import org.joda.time.DateTime;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import org.synyx.urlaubsverwaltung.core.department.Department;
 import org.synyx.urlaubsverwaltung.core.department.DepartmentService;
 import org.synyx.urlaubsverwaltung.core.holiday.VacationOverview;
@@ -12,18 +13,26 @@ import org.synyx.urlaubsverwaltung.core.person.Person;
 import org.synyx.urlaubsverwaltung.core.settings.FederalState;
 import org.synyx.urlaubsverwaltung.core.workingtime.PublicHolidaysService;
 import org.synyx.urlaubsverwaltung.core.workingtime.WorkingTimeService;
+import org.synyx.urlaubsverwaltung.test.DepartmentBuilder;
+import org.synyx.urlaubsverwaltung.test.PersonBuilder;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertThat;
+
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 import static org.synyx.urlaubsverwaltung.core.holiday.DayOfMonth.TypeOfDay.WORKDAY;
+
 
 public class VacationOverviewServiceTest {
 
@@ -35,29 +44,41 @@ public class VacationOverviewServiceTest {
 
     @Before
     public void setUp() throws Exception {
+
         this.departmentService = mock(DepartmentService.class);
         this.workingTimeService = mock(WorkingTimeService.class);
         this.publicHolidayService = mock(PublicHolidaysService.class);
         this.sut = new VacationOverviewService(departmentService, workingTimeService, publicHolidayService);
     }
 
+
     @Test
     public void assertVacationOverviewsForExistingDepartment() throws Exception {
-        Department department = new Department();
+
         String departmentName = "Admins";
-        department.setName(departmentName);
         String email = "muster@firma.test";
-        Person person = new Person("test", "Muster", "Max", email);
-        department.setMembers(Arrays.asList(person));
+
+        Person person = new PersonBuilder().build()
+                .withName("Max", "Muster")
+                .withEmail(email)
+                .withLoginName("test")
+                .get();
+        Department department = new DepartmentBuilder().build()
+                .withName("Admins")
+                .withMembers(Arrays.asList(person))
+                .get();
+
         DateMidnight testDate = DateMidnight.parse("2017-09-01");
         FederalState federalState = FederalState.BADEN_WUERTTEMBERG;
 
         when(departmentService.getAllDepartments()).thenReturn(Arrays.asList(department));
-        when(workingTimeService.getFederalStateForPerson(eq(person), any(DateMidnight.class))).thenReturn(federalState);
-        when(publicHolidayService.getWorkingDurationOfDate(any(DateMidnight.class), any(FederalState.class))).thenReturn(DayLength.FULL.getDuration());
+        when(workingTimeService.getFederalStateForPerson(eq(person), any(DateMidnight.class))).thenReturn(
+            federalState);
+        when(publicHolidayService.getWorkingDurationOfDate(any(DateMidnight.class), any(FederalState.class)))
+            .thenReturn(DayLength.FULL.getDuration());
 
-        List<VacationOverview> vacationOverviews =
-                sut.getVacationOverviews(departmentName, testDate.getYear(), testDate.getMonthOfYear());
+        List<VacationOverview> vacationOverviews = sut.getVacationOverviews(departmentName, testDate.getYear(),
+                testDate.getMonthOfYear());
 
         assertThat(vacationOverviews, hasSize(1));
         assertThat(vacationOverviews.get(0).getPerson().getEmail(), is(email));
