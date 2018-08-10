30919facf5530f493b0313a322ba135d8b24a439
Peter Bolch <pbolch@contargo.net>
2018-08-10 11:52:36 +0200

    Add Test for correct determination if a day is weekday or weekend + some basic refactoring

--- src/test/java/org/synyx/urlaubsverwaltung/restapi/vacationoverview/VacationOverviewServiceTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/restapi/vacationoverview/VacationOverviewServiceTest.java
@@ -8,13 +8,13 @@ import org.junit.Test;
 import org.synyx.urlaubsverwaltung.core.department.Department;
 import org.synyx.urlaubsverwaltung.core.department.DepartmentService;
 import org.synyx.urlaubsverwaltung.core.holiday.VacationOverview;
-import org.synyx.urlaubsverwaltung.core.period.DayLength;
 import org.synyx.urlaubsverwaltung.core.person.Person;
 import org.synyx.urlaubsverwaltung.core.settings.FederalState;
+import org.synyx.urlaubsverwaltung.core.settings.Settings;
+import org.synyx.urlaubsverwaltung.core.settings.SettingsServiceImpl;
 import org.synyx.urlaubsverwaltung.core.workingtime.PublicHolidaysService;
 import org.synyx.urlaubsverwaltung.core.workingtime.WorkingTimeService;
-import org.synyx.urlaubsverwaltung.test.DepartmentBuilder;
-import org.synyx.urlaubsverwaltung.test.PersonBuilder;
+import org.synyx.urlaubsverwaltung.test.TestDataCreator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,6 +31,7 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import static org.synyx.urlaubsverwaltung.core.holiday.DayOfMonth.TypeOfDay.WEEKEND;
 import static org.synyx.urlaubsverwaltung.core.holiday.DayOfMonth.TypeOfDay.WORKDAY;
 
 
@@ -41,47 +42,56 @@ public class VacationOverviewServiceTest {
     private DepartmentService departmentService;
     private WorkingTimeService workingTimeService;
     private PublicHolidaysService publicHolidayService;
+    private SettingsServiceImpl settingsServiceImpl;
+    private Department department;
+    private Person person;
 
     @Before
     public void setUp() throws Exception {
 
+        this.settingsServiceImpl = mock(SettingsServiceImpl.class);
         this.departmentService = mock(DepartmentService.class);
         this.workingTimeService = mock(WorkingTimeService.class);
-        this.publicHolidayService = mock(PublicHolidaysService.class);
+
+        this.publicHolidayService = new PublicHolidaysService(settingsServiceImpl);
         this.sut = new VacationOverviewService(departmentService, workingTimeService, publicHolidayService);
+
+        this.person = TestDataCreator.createPerson();
+        this.department = TestDataCreator.createDepartment();
+
+        FederalState federalState = FederalState.BADEN_WUERTTEMBERG;
+
+        when(departmentService.getAllDepartments()).thenReturn(Arrays.asList(department));
+        when(workingTimeService.getFederalStateForPerson(eq(person), any(DateMidnight.class))).thenReturn(
+            federalState);
+        when(settingsServiceImpl.getSettings()).thenReturn(new Settings());
     }
 
 
     @Test
     public void assertVacationOverviewsForExistingDepartment() throws Exception {
 
-        String departmentName = "Admins";
-        String email = "muster@firma.test";
-
-        Person person = new PersonBuilder().build()
-                .withName("Max", "Muster")
-                .withEmail(email)
-                .withLoginName("test")
-                .get();
-        Department department = new DepartmentBuilder().build()
-                .withName("Admins")
-                .withMembers(Arrays.asList(person))
-                .get();
-
         DateMidnight testDate = DateMidnight.parse("2017-09-01");
-        FederalState federalState = FederalState.BADEN_WUERTTEMBERG;
 
-        when(departmentService.getAllDepartments()).thenReturn(Arrays.asList(department));
-        when(workingTimeService.getFederalStateForPerson(eq(person), any(DateMidnight.class))).thenReturn(
-            federalState);
-        when(publicHolidayService.getWorkingDurationOfDate(any(DateMidnight.class), any(FederalState.class)))
-            .thenReturn(DayLength.FULL.getDuration());
-
-        List<VacationOverview> vacationOverviews = sut.getVacationOverviews(departmentName, testDate.getYear(),
-                testDate.getMonthOfYear());
+        List<VacationOverview> vacationOverviews = sut.getVacationOverviews(this.department.getName(),
+                testDate.getYear(), testDate.getMonthOfYear());
 
         assertThat(vacationOverviews, hasSize(1));
-        assertThat(vacationOverviews.get(0).getPerson().getEmail(), is(email));
+        assertThat(vacationOverviews.get(0).getPerson().getEmail(), is(this.person.getEmail()));
         assertThat(vacationOverviews.get(0).getDays().get(0).getTypeOfDay(), is(WORKDAY));
+    }
+
+
+    @Test
+    public void ensureHolidaysAreMarkedCorrectlyAsWeekend() throws Exception {
+
+        DateMidnight testDate = DateMidnight.parse("2017-12-01");
+
+        List<VacationOverview> vacationOverviews = sut.getVacationOverviews(this.department.getName(),
+                testDate.getYear(), testDate.getMonthOfYear());
+
+        assertThat(vacationOverviews, hasSize(1));
+        assertThat(vacationOverviews.get(0).getPerson().getEmail(), is(this.person.getEmail()));
+        assertThat(vacationOverviews.get(0).getDays().get(25).getTypeOfDay(), is(WEEKEND));
     }
 }
