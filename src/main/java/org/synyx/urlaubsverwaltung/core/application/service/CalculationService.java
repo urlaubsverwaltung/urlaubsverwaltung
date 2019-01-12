a2eb505e02d193ecff4bdf399f2876cdcd70171d
Christian Lange <lange@synyx.de>
2019-01-12 21:51:59 +0100

    adjusted code style

--- src/main/java/org/synyx/urlaubsverwaltung/core/application/service/CalculationService.java
+++ src/main/java/org/synyx/urlaubsverwaltung/core/application/service/CalculationService.java
@@ -48,7 +48,8 @@ public class CalculationService {
 
     @Autowired
     public CalculationService(VacationDaysService vacationDaysService, AccountService accountService,
-        AccountInteractionService accountInteractionService, WorkDaysService calendarService, OverlapService overlapService) {
+                              AccountInteractionService accountInteractionService, WorkDaysService calendarService,
+                              OverlapService overlapService) {
 
         this.vacationDaysService = vacationDaysService;
         this.accountService = accountService;
@@ -91,14 +92,17 @@ public class CalculationService {
                     DateUtil.getFirstDayOfYear(yearOfEndDate), endDate, person);
 
             return accountHasEnoughVacationDaysLeft(person, yearOfStartDate, workDaysInOldYear, application)
-                && accountHasEnoughVacationDaysLeft(person, yearOfEndDate, workDaysInNewYear, application);
+                    && accountHasEnoughVacationDaysLeft(person, yearOfEndDate, workDaysInNewYear, application);
         }
     }
 
 
-    private boolean accountHasEnoughVacationDaysLeft(Person person, int year, BigDecimal workDays, Application application) {
+    private boolean accountHasEnoughVacationDaysLeft(Person person, int year, BigDecimal workDays,
+                                                     Application application) {
+
         Optional<Account> account = getHolidaysAccount(year, person);
-        if (!account.isPresent()){
+
+        if (!account.isPresent()) {
             return false;
         }
 
@@ -108,68 +112,79 @@ public class CalculationService {
         BigDecimal alreadyUsedNextYear = BigDecimal.ZERO;
         // call accountService directly to avoid auto-creating a new account for next year
         Optional<Account> nextYear = accountService.getHolidaysAccount(year+1, person);
-        if (nextYear.isPresent()){
+        if (nextYear.isPresent()) {
             Account nextAccount = nextYear.get();
-            if (nextAccount.getRemainingVacationDays().signum() > 0){
+            if (nextAccount.getRemainingVacationDays().signum() > 0) {
                 VacationDaysLeft nextYearLeft = vacationDaysService.getVacationDaysLeft(nextAccount, Optional.empty());
                 BigDecimal totalUsedNextYear = nextAccount.getVacationDays()
                         .add(nextAccount.getRemainingVacationDays())
                         .subtract(nextYearLeft.getVacationDays())
                         .subtract(nextYearLeft.getRemainingVacationDays());
                 BigDecimal remainingUsedNextYear = totalUsedNextYear.subtract(nextAccount.getVacationDays());
-                if (remainingUsedNextYear.signum() > 0){
+                if (remainingUsedNextYear.signum() > 0) {
                     alreadyUsedNextYear = remainingUsedNextYear;
                 }
             }
         }
 
         VacationDaysLeft vacationDaysLeft = vacationDaysService.getVacationDaysLeft(account.get(), nextYear);
-        LOG.error("vacationDaysLeft: "+vacationDaysLeft);
+        LOG.info("vacationDaysLeft: " + vacationDaysLeft);
         // we may need to consider if remaining vacation days can be be used or not
         // first try without, maybe that's already enough
-        if (vacationDaysLeft.getVacationDays().add(vacationDaysLeft.getRemainingVacationDaysNotExpiring()).subtract(workDays).compareTo(alreadyUsedNextYear) >= 0){
+        if (vacationDaysLeft.getVacationDays()
+                .add(vacationDaysLeft.getRemainingVacationDaysNotExpiring())
+                .subtract(workDays)
+                .compareTo(alreadyUsedNextYear) >= 0){
             // good enough without looking at expiring remaining days
             return true;
         }
-        if (vacationDaysLeft.getVacationDays().add(vacationDaysLeft.getRemainingVacationDays()).subtract(workDays).compareTo(alreadyUsedNextYear) < 0) {
+        if (vacationDaysLeft.getVacationDays()
+                .add(vacationDaysLeft.getRemainingVacationDays())
+                .subtract(workDays)
+                .compareTo(alreadyUsedNextYear) < 0) {
             // not enough even when considering all expiring remaining days
             if (alreadyUsedNextYear.signum() > 0) {
-                LOG.info("Rejecting application by "+person+" for "+workDays+" days in "+year+" because "+alreadyUsedNextYear+" remaining days have already been used in "+(year+1));
+                LOG.info("Rejecting application by " + person + " for " + workDays + " days in " + year + " because "
+                        + alreadyUsedNextYear + " remaining days have already been used in " + (year+1));
             }
             return false;
         }
 
         // now we need to consider which remaining vacation days expire
-
         List<Interval> beforeApril = overlapService.getListOfOverlaps(
-            DateUtil.getFirstDayOfYear(year),
-            DateUtil.getLastDayOfMonth(year, DateTimeConstants.MARCH),
-            Collections.singletonList(application),
-            Collections.emptyList()
+                DateUtil.getFirstDayOfYear(year),
+                DateUtil.getLastDayOfMonth(year, DateTimeConstants.MARCH),
+                Collections.singletonList(application),
+                Collections.emptyList()
         );
 
         // this list can only have at most a single Interval entry
-
         BigDecimal workDaysBeforeApril = beforeApril.isEmpty() ?
                 BigDecimal.ZERO :
                 calendarService.getWorkDays(application.getDayLength(),
-                beforeApril.get(0).getStart().toDateMidnight(), beforeApril.get(0).getEnd().toDateMidnight(), person);
+                        beforeApril.get(0).getStart().toDateMidnight(), beforeApril.get(0).getEnd().toDateMidnight(), person);
 
 
-        BigDecimal leftUntilApril = vacationDaysLeft.getVacationDays().add(vacationDaysLeft.getRemainingVacationDays()).subtract(workDaysBeforeApril).subtract(alreadyUsedNextYear);
+        BigDecimal leftUntilApril = vacationDaysLeft.getVacationDays()
+                .add(vacationDaysLeft.getRemainingVacationDays())
+                .subtract(workDaysBeforeApril)
+                .subtract(alreadyUsedNextYear);
 
-        if (leftUntilApril.signum() < 0){
+        if (leftUntilApril.signum() < 0) {
             if (alreadyUsedNextYear.signum() > 0) {
-                LOG.info("Rejecting application by "+person+" for "+workDays+" days in "+year+" because "+alreadyUsedNextYear+" remaining days have already been used in "+(year+1));
+                LOG.info("Rejecting application by " + person + " for " + workDays + " days in " + year + " because "
+                        + alreadyUsedNextYear + " remaining days have already been used in " + (year+1));
             }
             return false;
         }
         BigDecimal workDaysAfterApril = workDays.subtract(workDaysBeforeApril);
-        BigDecimal extraRemaining = vacationDaysLeft.getRemainingVacationDays().subtract(vacationDaysLeft.getRemainingVacationDaysNotExpiring());
+        BigDecimal extraRemaining = vacationDaysLeft.getRemainingVacationDays()
+                .subtract(vacationDaysLeft.getRemainingVacationDaysNotExpiring());
 
         if (leftUntilApril.add(extraRemaining).compareTo(workDaysAfterApril) < 0){
             if (alreadyUsedNextYear.signum() > 0) {
-                LOG.info("Rejecting application by "+person+" for "+workDays+" days in "+year+" because "+alreadyUsedNextYear+" remaining days have already been used in "+(year+1));
+                LOG.info("Rejecting application by " + person + " for " + workDays + " days in " + year + " because "
+                        + alreadyUsedNextYear + " remaining days have already been used in " + (year+1));
             }
             return false;
         }
@@ -193,6 +208,6 @@ public class CalculationService {
         }
 
         return Optional.of(accountInteractionService.autoCreateOrUpdateNextYearsHolidaysAccount(
-                    lastYearsHolidaysAccount.get()));
+                lastYearsHolidaysAccount.get()));
     }
 }
