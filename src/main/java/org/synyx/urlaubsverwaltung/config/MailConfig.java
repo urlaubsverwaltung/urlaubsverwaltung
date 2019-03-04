2c4ad6450699728e43932dcadefa9f17636b394f
Daniel Fuchs <dfuchs@synyx.de>
2019-03-04 20:29:57 +0100

    remove unused freemaker import

--- src/main/java/org/synyx/urlaubsverwaltung/config/MailConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/config/MailConfig.java
@@ -3,13 +3,12 @@ package org.synyx.urlaubsverwaltung.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 
 /**
  * Configuration to send mails.
  *
- * @author  Aljona Murygina - murygina@synyx.de
+ * @author Aljona Murygina - murygina@synyx.de
  */
 @Configuration
 public class MailConfig {
