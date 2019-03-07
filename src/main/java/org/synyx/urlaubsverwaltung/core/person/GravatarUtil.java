368e06ec272dc0eaa3e70ea97a2bd939b984e36c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-03-07 16:37:03 +0100

    gravatar: use correct algorithmen to generate gravatar hash

--- src/main/java/org/synyx/urlaubsverwaltung/core/person/GravatarUtil.java
+++ src/main/java/org/synyx/urlaubsverwaltung/core/person/GravatarUtil.java
@@ -1,12 +1,13 @@
 package org.synyx.urlaubsverwaltung.core.person;
 
+import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.util.Assert;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 
 /**
@@ -14,10 +15,9 @@ import java.security.NoSuchAlgorithmException;
  *
  * @author  Aljona Murygina
  */
-public final class GravatarUtil {
+final class GravatarUtil {
 
-    static final String BASE_URL = "https://www.gravatar.com/avatar/";
-
+    private static final String BASE_URL = "https://gravatar.com/avatar/";
     private static final String DIGEST_ALGORITHM = "MD5";
 
     private static final Logger LOG = LoggerFactory.getLogger(GravatarUtil.class);
@@ -44,43 +44,18 @@ public final class GravatarUtil {
 
     private static String createHash(String string) {
 
-        Assert.notNull("String to be encrypted may not be null", string);
-
         String encryptedString = null;
 
         try {
-            MessageDigest md = MessageDigest.getInstance(DIGEST_ALGORITHM);
 
-            byte[] updatedData = md.digest(string.getBytes());
-            encryptedString = convertToHex(updatedData);
+            MessageDigest md5 = MessageDigest.getInstance(DIGEST_ALGORITHM);
+            final byte[] emailAsBytes = string.getBytes(UTF_8);
+
+            encryptedString = Hex.encodeHexString(md5.digest(emailAsBytes));
         } catch (NoSuchAlgorithmException ex) {
             LOG.error("Creation of message digest failed.", ex);
         }
 
         return encryptedString;
-    }
-
-
-    private static String convertToHex(byte[] data) {
-
-        StringBuffer buf = new StringBuffer();
-
-        for (int i = 0; i < data.length; i++) {
-            int halfByte = (data[i] >>> 4) & 0x0F; // NOSONAR
-            int twoHalves = 0;
-
-            do {
-                if ((0 <= halfByte) && (halfByte <= 9)) { // NOSONAR
-                    buf.append((char) ('0' + halfByte));
-                } else {
-                    buf.append((char) ('a' + (halfByte - 10))); // NOSONAR
-                }
-
-                halfByte = data[i] & 0x0F; // NOSONAR
-                twoHalves++;
-            } while (twoHalves < 1);
-        }
-
-        return buf.toString();
     }
 }
