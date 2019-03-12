828bcea5010bd873c2c03f61055b4cbbf80bc17c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-03-12 20:39:41 +0100

    cleanup: simplify stream api calls

--- src/test/java/org/synyx/urlaubsverwaltung/security/SecurityTestUtil.java
+++ src/test/java/org/synyx/urlaubsverwaltung/security/SecurityTestUtil.java
@@ -15,9 +15,6 @@ public class SecurityTestUtil {
 
     public static boolean authorityForRoleExists(Collection<? extends GrantedAuthority> authorities, final Role role) {
 
-        return authorities.stream()
-            .filter(authority -> authority.getAuthority().equals(role.name()))
-            .findFirst()
-            .isPresent();
+        return authorities.stream().anyMatch(authority -> authority.getAuthority().equals(role.name()));
     }
 }
