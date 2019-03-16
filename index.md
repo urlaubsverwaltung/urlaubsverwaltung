c2ce243106ba528b7d73fcd946a0b96a3ad3602d
Johannes Graf <graf@synyx.de>
2019-03-17 00:04:47 +0100

    add helm chart 0.1.0

---
+++ index.md
@@ -0,0 +1,15 @@
+# Urlaubsverwaltung Helm Repository
+
+Urlaubsverwaltung Helm chart version: 0.1.0
+
+## Usage
+
+Add the urlaubsverwaltung repo
+
+```
+helm repo add urlaubsverwaltung https://synyx.github.io/urlaubsverwaltung/chart_repo
+```
+
+
+Usage instructions can be found at [README.md](https://github.com/synyx/urlaubsverwaltung/blob/master/.examples/README.md)
+
---
+++ update-chart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -veu
+set -o pipefail
+
+temp_repo=$(mktemp -d -t urlaubsverwaltung-chart.XXX)
+function cleanup {
+    rm -rf "$temp_repo"
+}
+trap cleanup EXIT
+
+REV="$1"
+if [ -z "$REV" ]; then
+    echo "Expected revision first argument" >&2
+    exit 1
+fi
+
+git clone -b "$REV" https://github.com/synyx/urlaubsverwaltung "$temp_repo"
+helm package -d chart_repo -u "$temp_repo/.examples/kubernetes/chart/urlaubsverwaltung/"
+helm repo index chart_repo --url https://synyx.github.io/urlaubsverwaltung --merge chart_repo/index.yaml
+
