c24092608eb49395479a0e953f6a4bf8f6bf6e1e
Benjamin Seber <seber@synyx.de>
2019-03-28 12:56:13 +0100

    use window.navigator.language to format numbers

--- src/main/webapp/js/formatNumber.js
+++ src/main/webapp/js/formatNumber.js
@@ -1,6 +1,6 @@
 export default function formatNumber(number) {
-
-  return new Number(number).toLocaleString("de", {
+  const locale = window.navigator.language || 'de';
+  return Number(number).toLocaleString(locale, {
     maximumFractionDigits: 1,
     minimumFractionDigits: 0
   });
