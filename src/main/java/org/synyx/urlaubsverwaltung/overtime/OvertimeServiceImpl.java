9da92c010fe50352a283c998858795ae25ec64e3
Johannes Graf <graf@synyx.de>
2019-04-05 19:13:17 +0200

    make overtime package classes package private

--- src/main/java/org/synyx/urlaubsverwaltung/overtime/OvertimeServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/overtime/OvertimeServiceImpl.java
@@ -23,7 +23,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 @Transactional
 @Service
-public class OvertimeServiceImpl implements OvertimeService {
+class OvertimeServiceImpl implements OvertimeService {
 
     private static final Logger LOG = getLogger(lookup().lookupClass());
 
