2e567ca0ef0143bbf9390d0509e8842ed653e5df
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-05-03 10:27:50 +0200

    Fix imports

--- src/main/java/org/synyx/urlaubsverwaltung/mail/RecipientService.java
+++ src/main/java/org/synyx/urlaubsverwaltung/mail/RecipientService.java
@@ -11,7 +11,6 @@ import org.synyx.urlaubsverwaltung.person.Role;
 
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
