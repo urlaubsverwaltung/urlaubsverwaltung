39d75fdf88957629e84647bb207b7ee3ced624dd
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-05-22 13:18:24 +0200

    Use static imports

--- src/main/java/org/synyx/urlaubsverwaltung/api/config/RestApiSecurityConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/api/config/RestApiSecurityConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.NEVER;
 
 @Configuration
 @Order(1)
@@ -16,15 +17,13 @@ public class RestApiSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .antMatcher("/api/**")
                 .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                    .sessionCreationPolicy(NEVER)
             .and()
                 .httpBasic()
             .and()
                 .authorizeRequests()
                     .antMatchers("/api/sicknotes/**").hasAuthority("OFFICE")
                     .antMatchers("/api/**").authenticated()
-                    .anyRequest()
-                        .authenticated();
+                    .anyRequest().authenticated();
     }
-
 }
