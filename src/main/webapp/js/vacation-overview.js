21d303d1a87afdeee4430068bec184454244333a
Thilo Planz <thilo@cpan.org>
2019-06-14 17:03:26 +0800

    refactoring: replace JQuery.get with browser-built-in fetch function

--- src/main/webapp/js/vacation-overview.js
+++ src/main/webapp/js/vacation-overview.js
@@ -28,104 +28,103 @@ $(function () {
           + encodeURIComponent(selectedYearValue) + "&selectedMonth="
           + encodeURIComponent(selectedMonthValue) + "&selectedDepartment="
           + encodeURIComponent(selectedDepartmentValue);
+        fetch(url).then( (response) => response.json()).then( (holyDayOverviewResponse) => {
+          if (holyDayOverviewResponse && holyDayOverviewResponse.response) {
 
-        $.get(url).then( (holyDayOverviewResponse) => {
-        if (holyDayOverviewResponse && holyDayOverviewResponse.response) {
+            const overViewList = holyDayOverviewResponse.response.list;
+            overViewList
+              .forEach(function (listItem) {
+                const personId = listItem.personID;
 
-          const overViewList = holyDayOverviewResponse.response.list;
-          overViewList
-            .forEach(function (listItem) {
-              const personId = listItem.personID;
+                  listItem.days
+                    .forEach(currentDay => {
+                        let absences = listItem.absences;
 
-                listItem.days
-                  .forEach(currentDay => {
-                      let absences = listItem.absences;
+                        currentDay.cssClass = '';
 
-                      currentDay.cssClass = '';
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "WAITING", "VACATION", 'FULL'))) {
+                          currentDay.cssClass += ' vacationOverview-day-personal-holiday-status-WAITING';
+                        }
 
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "WAITING", "VACATION", 'FULL'))) {
-                        currentDay.cssClass += ' vacationOverview-day-personal-holiday-status-WAITING';
-                      }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "WAITING", "VACATION", 'MORNING'))) {
+                          currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-WAITING-morning';
+                        }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "WAITING", "VACATION", 'NOON'))) {
+                          currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-WAITING-noon';
+                        }
 
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "WAITING", "VACATION", 'MORNING'))) {
-                        currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-WAITING-morning';
-                      }
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "WAITING", "VACATION", 'NOON'))) {
-                        currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-WAITING-noon';
-                      }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "ALLOWED", "VACATION", 'MORNING'))) {
+                          currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-ALLOWED-morning';
+                        }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "ALLOWED", "VACATION", 'NOON'))) {
+                          currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-ALLOWED-noon';
+                        }
 
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "ALLOWED", "VACATION", 'MORNING'))) {
-                        currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-ALLOWED-morning';
-                      }
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "ALLOWED", "VACATION", 'NOON'))) {
-                        currentDay.cssClass += ' vacationOverview-day-personal-holiday-half-day-status-ALLOWED-noon';
-                      }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "ALLOWED", "VACATION", 'FULL'))) {
+                          currentDay.cssClass += ' vacationOverview-day-personal-holiday-status-ALLOWED';
+                        }
 
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "ALLOWED", "VACATION", 'FULL'))) {
-                        currentDay.cssClass += ' vacationOverview-day-personal-holiday-status-ALLOWED';
-                      }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "ACTIVE", "SICK_NOTE", 'FULL'))) {
+                          currentDay.cssClass += ' vacationOverview-day-sick-note';
+                        }
 
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "ACTIVE", "SICK_NOTE", 'FULL'))) {
-                        currentDay.cssClass += ' vacationOverview-day-sick-note';
-                      }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "ACTIVE", "SICK_NOTE", 'MORNING'))) {
+                          currentDay.cssClass += ' vacationOverview-day-sick-note-half-day-morning';
+                        }
+                        if (absences.find(currentValue => compare(currentDay, currentValue, "ACTIVE", "SICK_NOTE", 'NOON'))) {
+                          currentDay.cssClass += ' vacationOverview-day-sick-note-half-day-noon';
+                        }
 
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "ACTIVE", "SICK_NOTE", 'MORNING'))) {
-                        currentDay.cssClass += ' vacationOverview-day-sick-note-half-day-morning';
-                      }
-                      if (absences.find(currentValue => compare(currentDay, currentValue, "ACTIVE", "SICK_NOTE", 'NOON'))) {
-                        currentDay.cssClass += ' vacationOverview-day-sick-note-half-day-noon';
-                      }
+                        if (currentDay.cssClass !== '') {
+                          currentDay.cssClass += ' vacationOverview-day-item';
+                        }
 
-                      if (currentDay.cssClass !== '') {
-                        currentDay.cssClass += ' vacationOverview-day-item';
-                      }
+                    }, this);
 
-                  }, this);
+              });
 
-            });
-
-          const vacationOverviewTableHtml = html`<table id="vacationOverviewTable" class="list-table sortable tablesorter vacationOverview-table">
-            <thead class="hidden-xs">
-              <tr>
-                <th class="sortable-field"><spring:message code="person.data.firstName"/></th>
-                <th class="sortable-field"><spring:message code="person.data.lastName"/></th>
-                ${overViewList[0].days.map(item => item.typeOfDay === "WEEKEND"
-                  ? html`<th class="non-sortable vacationOverview-day-item vacationOverview-day-weekend">${item.dayNumber}</th>`
-                  : html`<th class="non-sortable vacationOverview-day-item">${item.dayNumber}</th>`
-                )}
-              </tr>
-            </thead>
-            <tbody class="vacationOverview-tbody">
-              ${overViewList.map(item => html`
+            const vacationOverviewTableHtml = html`<table id="vacationOverviewTable" class="list-table sortable tablesorter vacationOverview-table">
+              <thead class="hidden-xs">
                 <tr>
-                  <td class="hidden-xs">${item.person.firstName}</td>
-                  <td class="hidden-xs">${item.person.lastName}</td>
-                  ${item.days.map(dayItem => {
-                    if (dayItem.typeOfDay === "WEEKEND") {
-                      dayItem.cssClass = 'vacationOverview-day-weekend vacationOverview-day-item';
-                    } else if (!dayItem.cssClass) {
-                      dayItem.cssClass = 'vacationOverview-day vacationOverview-day-item ';
-                    }
-                    return html`<td class="${dayItem.cssClass}"></td>`;
-                  })}
+                  <th class="sortable-field"><spring:message code="person.data.firstName"/></th>
+                  <th class="sortable-field"><spring:message code="person.data.lastName"/></th>
+                  ${overViewList[0].days.map(item => item.typeOfDay === "WEEKEND"
+                    ? html`<th class="non-sortable vacationOverview-day-item vacationOverview-day-weekend">${item.dayNumber}</th>`
+                    : html`<th class="non-sortable vacationOverview-day-item">${item.dayNumber}</th>`
+                  )}
                 </tr>
-              `)}
-            </tbody>
-          </table>`;
+              </thead>
+              <tbody class="vacationOverview-tbody">
+                ${overViewList.map(item => html`
+                  <tr>
+                    <td class="hidden-xs">${item.person.firstName}</td>
+                    <td class="hidden-xs">${item.person.lastName}</td>
+                    ${item.days.map(dayItem => {
+                      if (dayItem.typeOfDay === "WEEKEND") {
+                        dayItem.cssClass = 'vacationOverview-day-weekend vacationOverview-day-item';
+                      } else if (!dayItem.cssClass) {
+                        dayItem.cssClass = 'vacationOverview-day vacationOverview-day-item ';
+                      }
+                      return html`<td class="${dayItem.cssClass}"></td>`;
+                    })}
+                  </tr>
+                `)}
+              </tbody>
+            </table>`;
 
-          document.querySelector("#vacationOverview").innerHTML = vacationOverviewTableHtml;
-        }
-      });
-
-      $("table.sortable").tablesorter({
-        sortList: [[0, 0]],
-        headers: {
-          '.non-sortable': {
-            sorter: false
+            document.querySelector("#vacationOverview").innerHTML = vacationOverviewTableHtml;
           }
-        }
-      });
-    }
+        });
+
+        $("table.sortable").tablesorter({
+          sortList: [[0, 0]],
+          headers: {
+            '.non-sortable': {
+              sorter: false
+            }
+          }
+        });
+      }
     }
 
     const selectedYear = document.querySelector('#yearSelect');
