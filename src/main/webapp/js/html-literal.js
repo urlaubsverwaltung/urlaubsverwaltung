6d412a7c72a6cc97c1ed8db7c1f3b496a57bd253
Benjamin Seber <seber@synyx.de>
2019-06-28 19:08:33 +0200

    get rid of eslint error
    for loop is ok here

--- src/main/webapp/js/html-literal.js
+++ src/main/webapp/js/html-literal.js
@@ -1,6 +1,7 @@
 export default function htmlLiteral(templateObject, ...substitutions) {
   let result = "";
 
+  // eslint-disable-next-line unicorn/no-for-loop
   for (let i = 0; i < substitutions.length; i++) {
     const subst = substitutions[i];
 
