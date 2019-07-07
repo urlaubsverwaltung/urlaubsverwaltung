45e79a5a968948d2f131a38060309d9acedb3af4
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-07-07 19:50:04 +0200

    Replace field injection by constructor injection

--- src/main/java/org/synyx/urlaubsverwaltung/FreemarkerConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/FreemarkerConfig.java
@@ -1,7 +1,6 @@
 package org.synyx.urlaubsverwaltung;
 
 import no.api.freemarker.java8.Java8ObjectWrapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerProperties;
 import org.springframework.context.ApplicationContext;
@@ -12,17 +11,15 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class FreemarkerConfig extends FreeMarkerAutoConfiguration {
 
-    @Autowired
-    private freemarker.template.Configuration configuration;
+    private final freemarker.template.Configuration configuration;
 
-    public FreemarkerConfig(ApplicationContext applicationContext, FreeMarkerProperties properties) {
+    public FreemarkerConfig(ApplicationContext applicationContext, FreeMarkerProperties properties, freemarker.template.Configuration configuration) {
         super(applicationContext, properties);
+        this.configuration = configuration;
     }
 
     @PostConstruct
     public void postConstruct() {
-        configuration.setObjectWrapper(
-            new Java8ObjectWrapper(freemarker.template.Configuration.getVersion()));
+        configuration.setObjectWrapper(new Java8ObjectWrapper(freemarker.template.Configuration.getVersion()));
     }
-
 }
