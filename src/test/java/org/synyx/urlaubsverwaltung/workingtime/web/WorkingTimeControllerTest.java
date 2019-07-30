72fa31bedef03bdc1de3284dafb48998929076e0
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-07-30 19:25:54 +0200

    Remove star import

--- src/test/java/org/synyx/urlaubsverwaltung/workingtime/web/WorkingTimeControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/workingtime/web/WorkingTimeControllerTest.java
@@ -35,7 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-import static org.synyx.urlaubsverwaltung.settings.FederalState.*;
+import static org.synyx.urlaubsverwaltung.settings.FederalState.BERLIN;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WorkingTimeControllerTest {
