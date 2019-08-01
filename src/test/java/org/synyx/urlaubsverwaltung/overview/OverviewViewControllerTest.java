c6054956d9707a872ad3af92b1ac553a418fdc1c
Khrystyna Kadyietska <kadyietska@synyx.de>
2019-08-01 15:47:04 +0200

    Extended unit test for OverviewController

--- src/test/java/org/synyx/urlaubsverwaltung/overview/OverviewViewControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/overview/OverviewViewControllerTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.synyx.urlaubsverwaltung.account.domain.Account;
+import org.synyx.urlaubsverwaltung.account.domain.VacationDaysLeft;
 import org.synyx.urlaubsverwaltung.account.service.AccountService;
 import org.synyx.urlaubsverwaltung.account.service.VacationDaysService;
 import org.synyx.urlaubsverwaltung.application.domain.Application;
@@ -17,21 +20,29 @@ import org.synyx.urlaubsverwaltung.department.DepartmentService;
 import org.synyx.urlaubsverwaltung.overtime.OvertimeService;
 import org.synyx.urlaubsverwaltung.person.Person;
 import org.synyx.urlaubsverwaltung.person.PersonService;
+import org.synyx.urlaubsverwaltung.person.UnknownPersonException;
 import org.synyx.urlaubsverwaltung.settings.SettingsService;
 import org.synyx.urlaubsverwaltung.sicknote.SickNote;
 import org.synyx.urlaubsverwaltung.sicknote.SickNoteService;
 import org.synyx.urlaubsverwaltung.workingtime.WorkDaysService;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.Collections;
 import java.util.Optional;
 
 import static java.math.BigDecimal.ONE;
 import static java.time.ZoneOffset.UTC;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -48,6 +59,8 @@ import static org.synyx.urlaubsverwaltung.person.Role.DEPARTMENT_HEAD;
 public class OverviewViewControllerTest {
 
     private OverviewViewController sut;
+
+    private static final int SOME_PERSON_ID = 1;
 
     @Mock
     private PersonService personService;
@@ -80,6 +93,98 @@ public class OverviewViewControllerTest {
         person.setId(1);
         person.setPermissions(singletonList(DEPARTMENT_HEAD));
         when(personService.getSignedInUser()).thenReturn(person);
+    }
+
+    @Test
+    public void showOverviewForUnknownPersonIdThrowsUnknownPersonException() {
+
+        final int unknownPersonId = 437;
+
+        assertThatThrownBy(() ->
+            perform(get("/web/person/" + unknownPersonId + "/overview"))
+        ).hasCauseInstanceOf(UnknownPersonException.class);
+    }
+
+    @Test
+    public void showOverviewForPersonTheSignedInUserIsNotAllowedForThrowsAccessDeniedException() {
+
+        final Person person = somePerson();
+        when(personService.getPersonByID(SOME_PERSON_ID)).thenReturn(Optional.of(person));
+
+        final Person signedInUser = somePerson();
+        when(personService.getSignedInUser()).thenReturn(signedInUser);
+
+        when(departmentService.isSignedInUserAllowedToAccessPersonData(signedInUser, person)).thenReturn(false);
+
+        assertThatThrownBy(() ->
+            perform(get("/web/person/" + SOME_PERSON_ID + "/overview"))
+        ).hasCauseInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    public void showOverviewUsesCurrentYearIfNoYearGiven() throws Exception {
+
+        final Person person = somePerson();
+        when(personService.getPersonByID(SOME_PERSON_ID)).thenReturn(Optional.of(person));
+
+        final Person signedInUser = somePerson();
+        when(personService.getSignedInUser()).thenReturn(signedInUser);
+
+        when(departmentService.isSignedInUserAllowedToAccessPersonData(signedInUser, person)).thenReturn(true);
+
+        final int currentYear = ZonedDateTime.now(UTC).getYear();
+
+        perform(get("/web/person/" + SOME_PERSON_ID + "/overview"));
+
+        verify(accountService).getHolidaysAccount(eq(currentYear), any());
+    }
+
+    @Test
+    public void showOverviewUsesYearParamIfYearGiven() throws Exception {
+
+        final Person person = somePerson();
+        when(personService.getPersonByID(SOME_PERSON_ID)).thenReturn(Optional.of(person));
+
+        final Person signedInUser = somePerson();
+        when(personService.getSignedInUser()).thenReturn(signedInUser);
+
+        when(departmentService.isSignedInUserAllowedToAccessPersonData(signedInUser, person)).thenReturn(true);
+
+        final int expectedYear = 1987;
+
+        perform(get("/web/person/" + SOME_PERSON_ID + "/overview")
+            .param("year", Integer.toString(expectedYear)));
+
+        verify(accountService).getHolidaysAccount(eq(expectedYear), any());
+    }
+
+    @Test
+    public void showOverviewAddsHolidayAccountInfoToModel() throws Exception {
+
+        when(personService.getPersonByID(SOME_PERSON_ID)).thenReturn(Optional.of(somePerson()));
+        when(departmentService.isSignedInUserAllowedToAccessPersonData(any(), any())).thenReturn(true);
+
+        final Account account = someAccount();
+        when(accountService.getHolidaysAccount(anyInt(), any())).thenReturn(Optional.of(account));
+
+        final VacationDaysLeft vacationDaysLeft = someVacationDaysLeft();
+        when(vacationDaysService.getVacationDaysLeft(account, Optional.of(account))).thenReturn(vacationDaysLeft);
+
+        perform(get("/web/person/" + SOME_PERSON_ID + "/overview"))
+            .andExpect(model().attribute("vacationDaysLeft", vacationDaysLeft))
+            .andExpect(model().attribute("account", account));
+    }
+
+    @Test
+    public void showOverviewDoesNotAddApplicationsToModelIfThereAreNoApplications() throws Exception {
+
+        when(personService.getPersonByID(SOME_PERSON_ID)).thenReturn(Optional.of(somePerson()));
+        when(departmentService.isSignedInUserAllowedToAccessPersonData(any(), any())).thenReturn(true);
+
+        when(applicationService.getApplicationsForACertainPeriodAndPerson(any(), any(), any())).thenReturn(Collections.emptyList());
+
+        perform(get("/web/person/" + SOME_PERSON_ID + "/overview"))
+            .andExpect(model().attribute("applications", nullValue()));
     }
 
     @Test
@@ -143,6 +248,33 @@ public class OverviewViewControllerTest {
         resultActions.andExpect(view().name("person/overview"));
         resultActions.andExpect(model().attribute("applications", hasSize(2)));
         resultActions.andExpect(model().attribute("sickNotes", hasSize(2)));
+    }
+
+    private Person somePerson() {
+
+        return new Person();
+    }
+
+    private Account someAccount() {
+
+        Account account = new Account();
+        account.setPerson(somePerson());
+        account.setValidFrom(LocalDate.now().minusDays(10));
+        account.setValidTo(LocalDate.now().plusDays(10));
+
+        return account;
+    }
+
+    private VacationDaysLeft someVacationDaysLeft() {
+
+        return VacationDaysLeft.builder()
+            .withAnnualVacation(BigDecimal.TEN)
+            .withRemainingVacation(BigDecimal.TEN)
+            .forUsedDaysBeforeApril(BigDecimal.ZERO)
+            .forUsedDaysAfterApril(BigDecimal.ZERO)
+            .withVacationDaysUsedNextYear(BigDecimal.ZERO)
+            .notExpiring(BigDecimal.ZERO)
+            .get();
     }
 
     private ResultActions perform(MockHttpServletRequestBuilder builder) throws Exception {
