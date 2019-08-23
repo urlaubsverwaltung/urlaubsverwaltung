819c281189e87653327382aa6c2e12ef4d89c58d
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-08-23 16:36:20 +0200

    Add absence toString test

--- src/main/java/org/synyx/urlaubsverwaltung/calendarintegration/absence/Absence.java
+++ src/main/java/org/synyx/urlaubsverwaltung/calendarintegration/absence/Absence.java
@@ -91,7 +91,8 @@ public class Absence {
     @Override
     public String toString() {
 
-        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("person", getPerson().getId())
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            .append("person", getPerson().getId())
             .append("startDate", getStartDate())
             .append("endDate", getEndDate())
             .append("isAllDay", isAllDay())
