0f739cb173db86832214e7c3f24ec071b9345fb8
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-09-10 11:29:25 +0200

    Remove duplicated imports

--- src/main/java/org/synyx/urlaubsverwaltung/security/WebSecurityConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/security/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
