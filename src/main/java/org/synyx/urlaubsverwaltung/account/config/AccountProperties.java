a046f2a3a7098623c66a6ce45847f55b23cf7228
honnel <github@honnel.de>
2019-09-10 14:08:10 +0200

    add validation for default properties

--- src/main/java/org/synyx/urlaubsverwaltung/account/config/AccountProperties.java
+++ src/main/java/org/synyx/urlaubsverwaltung/account/config/AccountProperties.java
@@ -2,15 +2,21 @@ package org.synyx.urlaubsverwaltung.account.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Component
 @ConfigurationProperties("uv.account")
+@Validated
 public class AccountProperties {
 
     @NotNull
-    private Integer defaultVacationDays;
+    @Min(0)
+    @Max(356)
+    private Integer defaultVacationDays = 20;
 
     public Integer getDefaultVacationDays() {
         return defaultVacationDays;
--- src/main/java/org/synyx/urlaubsverwaltung/workingtime/config/WorkingTimeProperties.java
+++ src/main/java/org/synyx/urlaubsverwaltung/workingtime/config/WorkingTimeProperties.java
@@ -2,16 +2,22 @@ package org.synyx.urlaubsverwaltung.workingtime.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+
 @Component
 @ConfigurationProperties("uv.workingtime")
+@Validated
 public class WorkingTimeProperties {
 
     @NotNull
-    private List<Integer> defaultWorkingDays;
+    private List<@Min(1) @Max(7) Integer> defaultWorkingDays = emptyList();
 
     public List<Integer> getDefaultWorkingDays() {
         return defaultWorkingDays;
