9157f2b8a993179bdfa6084250d7bfcf237b0084
Johannes Nicolai <jonico@github.com>
2019-11-05 11:04:30 +0100

    Add missing white space

--- src/main/java/org/synyx/urlaubsverwaltung/security/ldap/LdapPersonContextMapper.java
+++ src/main/java/org/synyx/urlaubsverwaltung/security/ldap/LdapPersonContextMapper.java
@@ -119,7 +119,7 @@ public class LdapPersonContextMapper implements UserDetailsContextMapper {
     @Override
     public void mapUserToContext(UserDetails user, DirContextAdapter ctx) {
 
-        throw new UnsupportedOperationException("LdapPersonContextMapper only supports reading from a context. Please"
+        throw new UnsupportedOperationException("LdapPersonContextMapper only supports reading from a context. Please "
             + "use a subclass if mapUserToContext() is required.");
     }
 }
