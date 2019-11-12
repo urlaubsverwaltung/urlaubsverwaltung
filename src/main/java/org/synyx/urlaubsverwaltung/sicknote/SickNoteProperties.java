b7d1cca095ce28eb2112eeb3f8e4a796ddafe662
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-11-12 20:42:40 +0100

    Add bean validation for uv.sick-note.endOfPayNotificationCron

--- src/main/java/org/synyx/urlaubsverwaltung/sicknote/SickNoteProperties.java
+++ src/main/java/org/synyx/urlaubsverwaltung/sicknote/SickNoteProperties.java
@@ -2,14 +2,19 @@ package org.synyx.urlaubsverwaltung.sicknote;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Component
 @ConfigurationProperties("uv.sick-note")
+@Validated
 public class SickNoteProperties {
 
     /**
      * Send notification about the end of sick pay by default every day at 06:00 am
      */
+    @NotEmpty
     private String endOfPayNotificationCron = "0 0 6 * * *";
 
     public String getEndOfPayNotificationCron() {
