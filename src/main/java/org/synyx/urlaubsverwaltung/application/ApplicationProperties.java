c066812dc2420afaae6fba19818b1aa93ae299e8
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-11-12 20:42:28 +0100

    Add bean validation for uv.application.reminderNotificationCron

--- src/main/java/org/synyx/urlaubsverwaltung/application/ApplicationProperties.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/ApplicationProperties.java
@@ -2,14 +2,19 @@ package org.synyx.urlaubsverwaltung.application;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Component
 @ConfigurationProperties("uv.application")
+@Validated
 public class ApplicationProperties {
 
     /**
      * Checks remind date about waiting applications by default every day at 07:00 am
      */
+    @NotEmpty
     private String reminderNotificationCron = "0 0 7 * * *";
 
     public String getReminderNotificationCron() {
