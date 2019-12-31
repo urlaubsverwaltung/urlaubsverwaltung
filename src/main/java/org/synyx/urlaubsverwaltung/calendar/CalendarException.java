35047a5102d4e0ced00e32bfce4bb8071b41aa85
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-12-31 15:47:17 +0100

    Validate every generated calendar

---
+++ src/main/java/org/synyx/urlaubsverwaltung/calendar/CalendarException.java
@@ -0,0 +1,8 @@
+package org.synyx.urlaubsverwaltung.calendar;
+
+public class CalendarException extends RuntimeException {
+
+    CalendarException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
