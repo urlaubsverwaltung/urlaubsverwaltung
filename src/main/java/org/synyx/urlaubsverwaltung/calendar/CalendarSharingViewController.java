67f6f0028ea51e818a9539cb3426ad46222e8931
Benjamin Seber <seber@synyx.de>
2020-01-17 18:43:47 +0100

    [WIP] integrate ical services into view

---
+++ src/main/java/org/synyx/urlaubsverwaltung/calendar/CalendarSharingViewController.java
@@ -0,0 +1,60 @@
+package org.synyx.urlaubsverwaltung.calendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.synyx.urlaubsverwaltung.person.PersonService;
+
+import java.util.Optional;
+
+
+@Controller
+@RequestMapping("/web/persons/{personId}/calendar/share")
+public class CalendarSharingViewController {
+
+    private final PersonService personService;
+    private final PersonCalendarService personCalendarService;
+
+    @Autowired
+    public CalendarSharingViewController(PersonService personService, PersonCalendarService personCalendarService) {
+        this.personService = personService;
+        this.personCalendarService = personCalendarService;
+    }
+
+    @GetMapping
+    public String index(@PathVariable int personId, Model model) {
+
+        final PersonCalendarDto dto = new PersonCalendarDto();
+        dto.setPersonId(personId);
+
+        final Optional<PersonCalendar> maybePersonCalendar = personCalendarService.getPersonCalendar(personId);
+        if (maybePersonCalendar.isPresent()) {
+            final PersonCalendar personCalendar = maybePersonCalendar.get();
+            dto.setCalendarUrl("https://urlaubsverwaltung.cloud/calendar/" + personCalendar.getSecret());
+        }
+
+        model.addAttribute("privateCalendarShare", dto);
+
+        return "calendarsharing/index";
+    }
+
+    @PostMapping(value = "/me")
+    public String linkPrivateCalendar(@PathVariable int personId) {
+
+        personCalendarService.createCalendarForPerson(personId);
+
+        return String.format("redirect:/web/persons/%d/calendar/share", personId);
+    }
+
+    @PostMapping(value = "/me/{calendarId}", params = "unlink")
+    public String unlinkPrivateCalendar(@PathVariable int personId) {
+
+        personCalendarService.deletePersonalCalendarForPerson(personId);
+
+        return String.format("redirect:/web/persons/%d/calendar/share", personId);
+    }
+}
--- src/main/java/org/synyx/urlaubsverwaltung/calendar/PersonCalendarRepository.java
+++ src/main/java/org/synyx/urlaubsverwaltung/calendar/PersonCalendarRepository.java
@@ -2,11 +2,16 @@ package org.synyx.urlaubsverwaltung.calendar;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.synyx.urlaubsverwaltung.person.Person;
 
 import java.util.Optional;
 
 @Repository
 interface PersonCalendarRepository extends CrudRepository<PersonCalendar, Long> {
 
+    PersonCalendar findByPerson(Person person);
+
     PersonCalendar findBySecret(String secret);
+
+    void deleteByPerson(Person person);
 }
