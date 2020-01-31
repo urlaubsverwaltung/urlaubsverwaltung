ff74fa0394aca0882210529a111e52b5aad9c1a6
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-01-31 20:23:19 +0100

    Reduce scope of absence mapping dao

--- src/main/java/org/synyx/urlaubsverwaltung/absence/AbsenceMappingDAO.java
+++ src/main/java/org/synyx/urlaubsverwaltung/absence/AbsenceMappingDAO.java
@@ -3,7 +3,7 @@ package org.synyx.urlaubsverwaltung.absence;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface AbsenceMappingDAO extends CrudRepository<AbsenceMapping, Integer> {
+interface AbsenceMappingDAO extends CrudRepository<AbsenceMapping, Integer> {
 
     AbsenceMapping findAbsenceMappingByAbsenceIdAndAbsenceType(Integer id, AbsenceType absenceType);
 }
