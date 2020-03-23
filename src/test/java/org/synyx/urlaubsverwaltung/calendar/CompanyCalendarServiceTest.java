4f86e559b98d599dd2d1fffc0795f6aeb855ffbc
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-03-23 17:44:14 +0100

    Add clock to tests

--- src/test/java/org/synyx/urlaubsverwaltung/calendar/CompanyCalendarServiceTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/calendar/CompanyCalendarServiceTest.java
@@ -15,6 +15,7 @@ import org.synyx.urlaubsverwaltung.person.Person;
 import org.synyx.urlaubsverwaltung.person.PersonService;
 import org.synyx.urlaubsverwaltung.settings.CalendarSettings;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -225,6 +226,6 @@ public class CompanyCalendarServiceTest {
         final Period period = new Period(start, end, length);
         final AbsenceTimeConfiguration timeConfig = new AbsenceTimeConfiguration(new CalendarSettings());
 
-        return new Absence(person, period, timeConfig);
+        return new Absence(person, period, timeConfig, Clock.systemUTC());
     }
 }
--- src/test/java/org/synyx/urlaubsverwaltung/calendar/DepartmentCalendarServiceTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/calendar/DepartmentCalendarServiceTest.java
@@ -17,6 +17,7 @@ import org.synyx.urlaubsverwaltung.person.Person;
 import org.synyx.urlaubsverwaltung.person.PersonService;
 import org.synyx.urlaubsverwaltung.settings.CalendarSettings;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -265,6 +266,6 @@ public class DepartmentCalendarServiceTest {
         final Period period = new Period(start, end, length);
         final AbsenceTimeConfiguration timeConfig = new AbsenceTimeConfiguration(new CalendarSettings());
 
-        return new Absence(person, period, timeConfig);
+        return new Absence(person, period, timeConfig, Clock.systemUTC());
     }
 }
