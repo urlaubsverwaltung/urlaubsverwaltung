f686fa81e7edd019f2a1aa5f5958351256e49387
Benjamin Seber <seber@synyx.de>
2020-03-27 16:03:39 +0100

    consider 'de-XX' locales for date-fns translations additionally to 'de'

---
+++ src/main/webapp/bundles/date-fns-localized.spec.js
@@ -0,0 +1,48 @@
+// eslint-disable-next-line @urlaubsverwaltung/no-date-fns
+import localeDE from 'date-fns/locale/de';
+import { setLocale } from "../lib/date-fns/locale-resolver";
+
+jest.mock('../lib/date-fns/locale-resolver');
+
+describe('date-fns-localized', () => {
+  let navigatorLanguage = '';
+  const originalNavigatorLanguage = window.navigator.language;
+
+  Object.defineProperty(window.navigator, 'language', {
+    get() {
+      return navigatorLanguage || originalNavigatorLanguage;
+    }
+  });
+
+  afterEach(() => {
+    navigatorLanguage = '';
+    jest.resetModules();
+  });
+
+  test.each([
+    ['de'],
+    ['de-DE'],
+    ['de-AT'],
+  ])('loads date-fn german locale for window.navigator.language=%s', async (givenLanguage) => {
+    navigatorLanguage = givenLanguage;
+
+    require('./date-fns-localized');
+
+    // wait for resolved promise in implementation
+    // which then calls `setLocale`
+    await wait();
+
+    // locale data is dynamically loaded on runtime with `import(..)` by date-fns-localized.js
+    // the loaded locale data module has an attached { "default": (..) } part which the `localeDE` of this test has not.
+    // this is due to babel configuration settings I think. I didn't look for details...
+    // the attached `default` part has something to do with JavaScript Module and CommonJS Module compatibility
+    // however, I don't care here. therefore just expect an object containing the original locale :o)
+    expect(setLocale).toHaveBeenCalledWith(expect.objectContaining(localeDE));
+  });
+
+  function wait(delay = 0) {
+    return new Promise(resolve => {
+      setTimeout(resolve, delay);
+    })
+  }
+});
