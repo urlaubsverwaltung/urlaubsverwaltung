6a976acc85f00a9c315f60c2b4d02c3b55834add
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-06-21 12:09:58 +0200

    Use Spring-Boot-Starter-Mail / Replace mock_javamail with greenmail
    We want the latest javax mail dependencies but mock_javamail will not ne
    updated so we have to find something new, because it does not work with
    the latest java mail versions. Greenmail is our choice. We do not have
    to change a lot. Just some minor changes and the project is well
    documented and active.
    
    With this chance we introduce spring-boot-mail-starter and greenmail as
    test mail server

--- src/test/java/org/synyx/urlaubsverwaltung/overtime/OvertimeMailServiceIT.java
+++ src/test/java/org/synyx/urlaubsverwaltung/overtime/OvertimeMailServiceIT.java
@@ -1,9 +1,10 @@
 package org.synyx.urlaubsverwaltung.overtime;
 
-import org.junit.After;
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.jvnet.mock_javamail.Mailbox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,7 +16,6 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import java.io.IOException;
-import java.util.List;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -25,10 +25,13 @@ import static org.synyx.urlaubsverwaltung.person.Role.OFFICE;
 import static org.synyx.urlaubsverwaltung.demodatacreator.DemoDataCreator.createOvertimeRecord;
 import static org.synyx.urlaubsverwaltung.demodatacreator.DemoDataCreator.createPerson;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.mail.port=3025", "spring.mail.host=localhost"})
 @RunWith(SpringRunner.class)
 @Transactional
 public class OvertimeMailServiceIT {
+
+    @Rule
+    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP_IMAP);
 
     @Autowired
     private OvertimeMailService sut;
@@ -36,19 +39,14 @@ public class OvertimeMailServiceIT {
     @Autowired
     private PersonService personService;
 
-    @After
-    public void tearDown() {
-        Mailbox.clearAll();
-    }
-
     @Test
     public void ensureOfficeWithOvertimeNotificationGetMailIfOvertimeRecorded() throws MessagingException, IOException {
 
-        final Person person = createPerson("user", "Lieschen", "Müller", "lieschen12@firma.test");
+        final Person person = createPerson("user", "Lieschen", "Müller", "lieschen12@example.org");
         final Overtime overtimeRecord = createOvertimeRecord(person);
         final OvertimeComment overtimeComment = new OvertimeComment(person, overtimeRecord, CREATED);
 
-        final Person office = createPerson("office", "Marlene", "Muster", "office@firma.test");
+        final Person office = createPerson("office", "Marlene", "Muster", "office@example.org");
         office.setPermissions(singletonList(OFFICE));
         office.setNotifications(singletonList(OVERTIME_NOTIFICATION_OFFICE));
         personService.save(office);
@@ -56,16 +54,15 @@ public class OvertimeMailServiceIT {
         sut.sendOvertimeNotification(overtimeRecord, overtimeComment);
 
         // was email sent to office?
-        List<Message> inboxOffice = Mailbox.get(office.getEmail());
-        assertThat(inboxOffice.size()).isOne();
+        assertThat(greenMail.getReceivedMessagesForDomain(office.getEmail()).length).isOne();
 
         // check attributes
-        Message msg = inboxOffice.get(0);
+        final Message msg = greenMail.getReceivedMessagesForDomain(office.getEmail())[0];
         assertThat(msg.getSubject()).contains("Es wurden Überstunden eingetragen");
         assertThat(new InternetAddress(office.getEmail())).isEqualTo(msg.getAllRecipients()[0]);
 
         // check content of email
-        String text = (String) msg.getContent();
+        final String text = (String) msg.getContent();
         assertThat(text).contains("Hallo Office");
         assertThat(text).contains("es wurden Überstunden erfasst");
         assertThat(text).contains("/web/overtime/1234");
