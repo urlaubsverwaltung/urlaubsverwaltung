6fae1d7f82bd3db9da8e9311824c17b9aab19c5d
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-07-06 11:00:00 +0200

    cleanup: use isEmpty

--- src/test/java/org/synyx/urlaubsverwaltung/sicknote/SickNoteCommentServiceImplTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/sicknote/SickNoteCommentServiceImplTest.java
@@ -2,73 +2,74 @@ package org.synyx.urlaubsverwaltung.sicknote;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.synyx.urlaubsverwaltung.person.Person;
-import org.synyx.urlaubsverwaltung.testdatacreator.TestDataCreator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.synyx.urlaubsverwaltung.sicknote.SickNoteAction.CONVERTED_TO_VACATION;
+import static org.synyx.urlaubsverwaltung.sicknote.SickNoteAction.EDITED;
+import static org.synyx.urlaubsverwaltung.testdatacreator.TestDataCreator.createPerson;
+import static org.synyx.urlaubsverwaltung.testdatacreator.TestDataCreator.createSickNote;
 
 
 /**
  * Unit test for {@link SickNoteCommentServiceImpl}.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class SickNoteCommentServiceImplTest {
 
-    private SickNoteCommentService sickNoteCommentService;
+    private SickNoteCommentService sut;
 
+    @Mock
     private SickNoteCommentDAO commentDAO;
 
     @Before
     public void setUp() {
-
-        commentDAO = mock(SickNoteCommentDAO.class);
-        when(commentDAO.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
-
-        sickNoteCommentService = new SickNoteCommentServiceImpl(commentDAO);
+        sut = new SickNoteCommentServiceImpl(commentDAO);
     }
-
 
     @Test
     public void ensureCreatesACommentAndPersistsIt() {
 
-        final Person author = TestDataCreator.createPerson("author");
-        final SickNote sickNote = TestDataCreator.createSickNote(author);
+        when(commentDAO.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
 
-        SickNoteComment comment = sickNoteCommentService.create(sickNote, SickNoteAction.EDITED, author);
+        final Person author = createPerson("author");
+        final SickNote sickNote = createSickNote(author);
 
+        final SickNoteComment comment = sut.create(sickNote, EDITED, author);
         assertThat(comment).isNotNull();
         assertThat(comment.getSickNote()).isNotNull();
         assertThat(comment.getDate()).isNotNull();
         assertThat(comment.getAction()).isNotNull();
         assertThat(comment.getPerson()).isNotNull();
         assertThat(comment.getSickNote()).isEqualTo(sickNote);
-        assertThat(comment.getAction()).isEqualTo(SickNoteAction.EDITED);
+        assertThat(comment.getAction()).isEqualTo(EDITED);
         assertThat(comment.getPerson()).isEqualTo(author);
-        assertThat(comment.getText()).isEqualTo("");
+        assertThat(comment.getText()).isEmpty();
 
         verify(commentDAO).save(eq(comment));
     }
 
-
     @Test
     public void ensureCreationOfCommentWithTextWorks() {
 
+        when(commentDAO.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
         final String givenComment = "Foo";
-        final Person givenAuthor = TestDataCreator.createPerson("author");
-        final SickNote givenSickNote = TestDataCreator.createSickNote(givenAuthor);
+        final Person givenAuthor = createPerson("author");
+        final SickNote givenSickNote = createSickNote(givenAuthor);
 
-        SickNoteComment sickNoteComment = sickNoteCommentService.create(givenSickNote, SickNoteAction.CONVERTED_TO_VACATION,
-            givenAuthor, givenComment);
-
+        final SickNoteComment sickNoteComment = sut.create(givenSickNote, CONVERTED_TO_VACATION, givenAuthor, givenComment);
         assertThat(sickNoteComment).isNotNull();
         assertThat(sickNoteComment.getDate()).isNotNull();
-
         assertThat(sickNoteComment.getSickNote()).isEqualTo(givenSickNote);
-        assertThat(sickNoteComment.getAction()).isEqualTo(SickNoteAction.CONVERTED_TO_VACATION);
+        assertThat(sickNoteComment.getAction()).isEqualTo(CONVERTED_TO_VACATION);
         assertThat(sickNoteComment.getPerson()).isEqualTo(givenAuthor);
         assertThat(sickNoteComment.getText()).isEqualTo(givenComment);
 
