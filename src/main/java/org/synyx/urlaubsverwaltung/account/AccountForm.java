1b3602c9cdb44b710deec0a957eb7ad7da8bf452
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-09-07 16:47:40 +0200

    Formatting

--- src/main/java/org/synyx/urlaubsverwaltung/account/AccountForm.java
+++ src/main/java/org/synyx/urlaubsverwaltung/account/AccountForm.java
@@ -12,28 +12,19 @@ import static java.time.ZoneOffset.UTC;
 public class AccountForm {
 
     private int holidaysAccountYear;
-
     private LocalDate holidaysAccountValidFrom;
-
     private LocalDate holidaysAccountValidTo;
-
     private BigDecimal annualVacationDays;
-
     private BigDecimal actualVacationDays;
-
     private BigDecimal remainingVacationDays;
-
     private BigDecimal remainingVacationDaysNotExpiring;
-
     private String comment;
 
     AccountForm() {
-
         this(ZonedDateTime.now(UTC).getYear());
     }
 
     AccountForm(int year) {
-
         this.holidaysAccountYear = year;
         this.holidaysAccountValidFrom = DateUtil.getFirstDayOfYear(year);
         this.holidaysAccountValidTo = DateUtil.getLastDayOfYear(year);
@@ -60,72 +51,58 @@ public class AccountForm {
     }
 
     public int getHolidaysAccountYear() {
-
         return holidaysAccountYear;
     }
 
     public void setHolidaysAccountYear(int holidaysAccountYear) {
-
         this.holidaysAccountYear = holidaysAccountYear;
     }
 
     public LocalDate getHolidaysAccountValidFrom() {
-
         return holidaysAccountValidFrom;
     }
 
     public void setHolidaysAccountValidFrom(LocalDate holidaysAccountValidFrom) {
-
         this.holidaysAccountValidFrom = holidaysAccountValidFrom;
     }
 
     public LocalDate getHolidaysAccountValidTo() {
-
         return holidaysAccountValidTo;
     }
 
     public void setHolidaysAccountValidTo(LocalDate holidaysAccountValidTo) {
-
         this.holidaysAccountValidTo = holidaysAccountValidTo;
     }
 
     public BigDecimal getAnnualVacationDays() {
-
         return annualVacationDays;
     }
 
     public void setAnnualVacationDays(BigDecimal annualVacationDays) {
-
         this.annualVacationDays = annualVacationDays;
     }
 
     public BigDecimal getActualVacationDays() {
-
         return actualVacationDays;
     }
 
     public void setActualVacationDays(BigDecimal actualVacationDays) {
-
         this.actualVacationDays = actualVacationDays;
     }
 
     public BigDecimal getRemainingVacationDays() {
-
         return remainingVacationDays;
     }
 
     public void setRemainingVacationDays(BigDecimal remainingVacationDays) {
-
         this.remainingVacationDays = remainingVacationDays;
     }
 
     public BigDecimal getRemainingVacationDaysNotExpiring() {
-
         return remainingVacationDaysNotExpiring;
     }
 
     public void setRemainingVacationDaysNotExpiring(BigDecimal remainingVacationDaysNotExpiring) {
-
         this.remainingVacationDaysNotExpiring = remainingVacationDaysNotExpiring;
     }
 
