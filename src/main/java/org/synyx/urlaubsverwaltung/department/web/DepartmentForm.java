6fb82f2724a99ab7eaa4e06087fe22c5c467c647
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-10-30 17:16:08 +0100

    Replace Department as dto with DepartmentForm

---
+++ src/main/java/org/synyx/urlaubsverwaltung/department/web/DepartmentForm.java
@@ -0,0 +1,120 @@
+package org.synyx.urlaubsverwaltung.department.web;
+
+import org.synyx.urlaubsverwaltung.person.Person;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class DepartmentForm {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDate lastModification;
+    private boolean twoStageApproval;
+    private List<Person> members = new ArrayList<>();
+    private List<Person> departmentHeads = new ArrayList<>();
+    private List<Person> secondStageAuthorities = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getLastModification() {
+        return lastModification;
+    }
+
+    public void setLastModification(LocalDate lastModification) {
+        this.lastModification = lastModification;
+    }
+
+    public boolean isTwoStageApproval() {
+        return twoStageApproval;
+    }
+
+    public void setTwoStageApproval(boolean twoStageApproval) {
+        this.twoStageApproval = twoStageApproval;
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Person> members) {
+        this.members = members;
+    }
+
+    public List<Person> getDepartmentHeads() {
+        return departmentHeads;
+    }
+
+    public void setDepartmentHeads(List<Person> departmentHeads) {
+        this.departmentHeads = departmentHeads;
+    }
+
+    public List<Person> getSecondStageAuthorities() {
+        return secondStageAuthorities;
+    }
+
+    public void setSecondStageAuthorities(List<Person> secondStageAuthorities) {
+        this.secondStageAuthorities = secondStageAuthorities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final DepartmentForm that = (DepartmentForm) o;
+        return twoStageApproval == that.twoStageApproval &&
+            Objects.equals(name, that.name) &&
+            Objects.equals(description, that.description) &&
+            Objects.equals(lastModification, that.lastModification) &&
+            Objects.equals(members, that.members) &&
+            Objects.equals(departmentHeads, that.departmentHeads) &&
+            Objects.equals(secondStageAuthorities, that.secondStageAuthorities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, lastModification, twoStageApproval, members, departmentHeads, secondStageAuthorities);
+    }
+
+    @Override
+    public String toString() {
+        return "DepartmentForm{" +
+            "name='" + name + '\'' +
+            ", description='" + description + '\'' +
+            ", lastModification=" + lastModification +
+            ", twoStageApproval=" + twoStageApproval +
+            ", members=" + members +
+            ", departmentHeads=" + departmentHeads +
+            ", secondStageAuthorities=" + secondStageAuthorities +
+            '}';
+    }
+}
