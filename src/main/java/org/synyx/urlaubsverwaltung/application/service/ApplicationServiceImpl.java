278bc4ed24918d8b36587997ec9289ee269a061f
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-11-14 20:31:31 +0100

    Cleanup application service

--- src/main/java/org/synyx/urlaubsverwaltung/application/service/ApplicationServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/service/ApplicationServiceImpl.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-
 /**
  * Implementation of interface {@link ApplicationService}.
  */
@@ -24,66 +23,49 @@ class ApplicationServiceImpl implements ApplicationService {
 
     @Autowired
     ApplicationServiceImpl(ApplicationRepository applicationRepository) {
-
         this.applicationRepository = applicationRepository;
     }
 
     @Override
     public Optional<Application> getApplicationById(Integer id) {
-
         return applicationRepository.findById(id);
     }
 
-
     @Override
     public Application save(Application application) {
-
         return applicationRepository.save(application);
     }
 
-
     @Override
     public List<Application> getApplicationsForACertainState(ApplicationStatus state) {
-
         return applicationRepository.getApplicationsForACertainState(state);
     }
 
-
     @Override
-    public List<Application> getApplicationsForACertainPeriodAndPerson(LocalDate startDate, LocalDate endDate,
-                                                                       Person person) {
-
+    public List<Application> getApplicationsForACertainPeriodAndPerson(LocalDate startDate, LocalDate endDate, Person person) {
         return applicationRepository.getApplicationsForACertainTimeAndPerson(startDate, endDate, person);
     }
 
-
     @Override
-    public List<Application> getApplicationsForACertainPeriodAndState(LocalDate startDate, LocalDate endDate,
-                                                                      ApplicationStatus status) {
-
+    public List<Application> getApplicationsForACertainPeriodAndState(LocalDate startDate, LocalDate endDate, ApplicationStatus status) {
         return applicationRepository.getApplicationsForACertainTimeAndState(startDate, endDate, status);
     }
 
 
     @Override
-    public List<Application> getApplicationsForACertainPeriodAndPersonAndState(LocalDate startDate,
-                                                                               LocalDate endDate, Person person, ApplicationStatus status) {
-
+    public List<Application> getApplicationsForACertainPeriodAndPersonAndState(LocalDate startDate, LocalDate endDate, Person person, ApplicationStatus status) {
         return applicationRepository.getApplicationsForACertainTimeAndPersonAndState(startDate, endDate, person, status);
     }
 
     @Override
     public List<Application> getForStates(List<ApplicationStatus> statuses) {
-
         return applicationRepository.findByStatusIn(statuses);
     }
 
     @Override
     public List<Application> getForStatesAndPerson(List<ApplicationStatus> statuses, List<Person> persons) {
-
         return applicationRepository.findByStatusInAndPersonIn(statuses, persons);
     }
-
 
     @Override
     public BigDecimal getTotalOvertimeReductionOfPerson(Person person) {
