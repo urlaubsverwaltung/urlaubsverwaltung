2e37df10548da24ef79c1257f8f75f023e36a67c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-12-15 13:48:14 +0100

    Use text blocks for @Query and more

--- src/main/java/org/synyx/urlaubsverwaltung/workingtime/WorkingTimeRepository.java
+++ src/main/java/org/synyx/urlaubsverwaltung/workingtime/WorkingTimeRepository.java
@@ -20,9 +20,9 @@ interface WorkingTimeRepository extends CrudRepository<WorkingTimeEntity, Intege
 
     List<WorkingTimeEntity> findByPersonIn(List<Person> persons);
 
-    @Query(
-        "SELECT x FROM working_time x WHERE x.person = ?1 "
-            + "AND x.validFrom = (SELECT MAX(w.validFrom) from working_time w WHERE w.person = ?1 AND w.validFrom <= ?2)"
-    )
+    @Query("""
+        SELECT x FROM working_time x WHERE x.person = ?1
+        AND x.validFrom = (SELECT MAX(w.validFrom) from working_time w WHERE w.person = ?1 AND w.validFrom <= ?2)
+        """)
     WorkingTimeEntity findByPersonAndValidityDateEqualsOrMinorDate(Person person, LocalDate date);
 }
--- src/test/java/org/synyx/urlaubsverwaltung/overtime/web/OvertimeFormValidatorTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/overtime/web/OvertimeFormValidatorTest.java
@@ -475,9 +475,10 @@ class OvertimeFormValidatorTest {
         when(overtimeService.getLeftOvertimeForPerson(any(Person.class))).thenReturn(Duration.ZERO);
 
         final OvertimeForm overtimeForm = new OvertimeForm(createOvertimeRecord());
-        overtimeForm.setComment(
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore "
-                + "et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores");
+        overtimeForm.setComment("""
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                """);
 
         sut.validate(overtimeForm, errors);
 
@@ -515,9 +516,10 @@ class OvertimeFormValidatorTest {
         when(overtimeService.getLeftOvertimeForPerson(any(Person.class))).thenReturn(Duration.ZERO);
 
         final OvertimeForm overtimeForm = new OvertimeForm(createOvertimeRecord());
-        overtimeForm.setComment(
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore "
-                + "et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e");
+        overtimeForm.setComment("""
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e
+                """);
 
         sut.validate(overtimeForm, errors);
 
