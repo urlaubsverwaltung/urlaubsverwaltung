f1b4795b9f6b7be7a2ab5a905af1287d09028268
Johannes Graf <graf@synyx.de>
2020-12-18 22:47:15 +0100

    splitt rest api security config

--- src/main/java/org/synyx/urlaubsverwaltung/api/RestApiSecurityConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/api/RestApiSecurityConfig.java
@@ -1,39 +1,85 @@
 package org.synyx.urlaubsverwaltung.api;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.synyx.urlaubsverwaltung.security.SecurityConfigurationProperties;
+import org.synyx.urlaubsverwaltung.security.oidc.OidcSecurityProperties;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.NEVER;
 
 @Configuration
-@Order(1)
-public class RestApiSecurityConfig extends WebSecurityConfigurerAdapter {
+public class RestApiSecurityConfig {
 
-    private final boolean isOauth2Enabled;
-
-    public RestApiSecurityConfig(SecurityConfigurationProperties properties) {
-        isOauth2Enabled = "oidc".equalsIgnoreCase(properties.getAuth().name());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    static void noSessions(HttpSecurity http) throws Exception {
 
         http
             .antMatcher("/api/**")
             .sessionManagement()
-            .sessionCreationPolicy(NEVER)
-            .and()
+            .sessionCreationPolicy(NEVER);
+
+    }
+
+    static void authenticatedApi(HttpSecurity http) throws Exception {
+
+        http
             .authorizeRequests()
             .antMatchers("/api/**").authenticated()
             .anyRequest().authenticated();
 
-        if (isOauth2Enabled) {
-            http.oauth2Login();
-        } else {
+    }
+
+    @Configuration
+    @Conditional(NonOidcSecurityAuthCondition.class)
+    @Order(1)
+    static class BasicAuthRestApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            noSessions(http);
+            authenticatedApi(http);
             http.httpBasic();
+        }
+
+    }
+
+    @Configuration
+    @ConditionalOnProperty(value = "uv.security.auth", havingValue = "oidc")
+    @Order(1)
+    static class Oauth2RestApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        private final String issuerUri;
+        private final PersonService personService;
+
+        public Oauth2RestApiSecurityConfig(OidcSecurityProperties properties, PersonService personService) {
+            this.issuerUri = properties.getIssuerUri();
+            this.personService = personService;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            noSessions(http);
+            authenticatedApi(http);
+
+            http.oauth2Login();
+        }
+    }
+
+    static class NonOidcSecurityAuthCondition implements Condition {
+
+        private static final String UV_SECURITY_AUTH = "uv.security.auth";
+        private static final String OIDC = "oidc";
+
+        @Override
+        public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+            String value = conditionContext.getEnvironment().getProperty(UV_SECURITY_AUTH);
+            return !OIDC.equalsIgnoreCase(value);
         }
     }
 }
