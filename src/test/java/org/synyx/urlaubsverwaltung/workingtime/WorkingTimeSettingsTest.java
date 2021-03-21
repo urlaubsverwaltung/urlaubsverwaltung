44e7f21aa8f0a153e753efef4376076dc23ddc3c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2021-03-21 19:19:00 +0100

    Add further WorkingTimeSettingsTest

--- src/test/java/org/synyx/urlaubsverwaltung/workingtime/WorkingTimeSettingsTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/workingtime/WorkingTimeSettingsTest.java
@@ -1,9 +1,13 @@
 package org.synyx.urlaubsverwaltung.workingtime;
 
 import org.junit.jupiter.api.Test;
-import org.synyx.urlaubsverwaltung.period.DayLength;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.synyx.urlaubsverwaltung.period.DayLength.FULL;
+import static org.synyx.urlaubsverwaltung.period.DayLength.MORNING;
+import static org.synyx.urlaubsverwaltung.period.DayLength.ZERO;
 
 class WorkingTimeSettingsTest {
 
@@ -11,8 +15,21 @@ class WorkingTimeSettingsTest {
     void ensureDefaultValues() {
 
         final WorkingTimeSettings settings = new WorkingTimeSettings();
-        assertThat(settings.getWorkingDurationForChristmasEve()).isEqualTo(DayLength.MORNING);
-        assertThat(settings.getWorkingDurationForNewYearsEve()).isEqualTo(DayLength.MORNING);
-        assertThat(settings.getFederalState()).isEqualTo(FederalState.BADEN_WUERTTEMBERG);
+        assertThat(settings.getWorkingDurationForChristmasEve()).isEqualTo(MORNING);
+        assertThat(settings.getWorkingDurationForNewYearsEve()).isEqualTo(MORNING);
+        assertThat(settings.getMonday()).isEqualTo(FULL);
+        assertThat(settings.getTuesday()).isEqualTo(FULL);
+        assertThat(settings.getWednesday()).isEqualTo(FULL);
+        assertThat(settings.getThursday()).isEqualTo(FULL);
+        assertThat(settings.getFriday()).isEqualTo(FULL);
+        assertThat(settings.getSaturday()).isEqualTo(ZERO);
+        assertThat(settings.getSunday()).isEqualTo(ZERO);
+    }
+
+    @Test
+    void setWorkingDays() {
+        final WorkingTimeSettings settings = new WorkingTimeSettings();
+        settings.setWorkingDays(List.of(1, 2, 3, 4, 5, 6, 7));
+        assertThat(settings.getWorkingDays()).isEqualTo(List.of(1, 2, 3, 4, 5, 6, 7));
     }
 }
