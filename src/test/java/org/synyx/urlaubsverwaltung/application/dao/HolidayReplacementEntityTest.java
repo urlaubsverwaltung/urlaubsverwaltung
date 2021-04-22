129deba7dda1a89e83a54cc2a3af5b630e2ec8d1
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2021-04-22 15:08:14 +0200

    Add tests for HolidayReplacementEntityTest

---
+++ src/test/java/org/synyx/urlaubsverwaltung/application/dao/HolidayReplacementEntityTest.java
@@ -0,0 +1,84 @@
+package org.synyx.urlaubsverwaltung.application.dao;
+
+import org.junit.jupiter.api.Test;
+import org.synyx.urlaubsverwaltung.application.web.HolidayReplacementDto;
+import org.synyx.urlaubsverwaltung.department.Department;
+import org.synyx.urlaubsverwaltung.person.Person;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.synyx.urlaubsverwaltung.application.dao.HolidayReplacementEntity.from;
+
+class HolidayReplacementEntityTest {
+
+    @Test
+    void ensuresConvertingDtoToEntity() {
+        final Person holidayReplacement = new Person();
+
+        final HolidayReplacementDto holidayReplacementDto = new HolidayReplacementDto();
+        holidayReplacementDto.setPerson(holidayReplacement);
+        holidayReplacementDto.setNote("some note to the holiday replacement");
+
+        final HolidayReplacementEntity holidayReplacementEntity = from(holidayReplacementDto);
+        assertThat(holidayReplacementEntity.getPerson()).isEqualTo(holidayReplacement);
+        assertThat(holidayReplacementEntity.getNote()).isEqualTo("some note to the holiday replacement");
+    }
+
+    @Test
+    void ensureEquals() {
+
+        final Person holidayReplacement = new Person();
+        final HolidayReplacementEntity holidayReplacementEntity = new HolidayReplacementEntity();
+        holidayReplacementEntity.setPerson(holidayReplacement);
+        holidayReplacementEntity.setNote("first Note");
+
+        final HolidayReplacementEntity sameHolidayReplacementEntity = new HolidayReplacementEntity();
+        sameHolidayReplacementEntity.setPerson(holidayReplacement);
+        sameHolidayReplacementEntity.setNote("first Note");
+
+        final Person otherHolidayReplacement = new Person();
+        final HolidayReplacementEntity holidayReplacementEntityOtherPerson = new HolidayReplacementEntity();
+        holidayReplacementEntityOtherPerson.setPerson(otherHolidayReplacement);
+        holidayReplacementEntityOtherPerson.setNote("first Note");
+
+        final HolidayReplacementEntity holidayReplacementEntityOtherNote = new HolidayReplacementEntity();
+        holidayReplacementEntityOtherNote.setPerson(holidayReplacement);
+        holidayReplacementEntityOtherNote.setNote("second Note");
+
+        assertThat(holidayReplacementEntity)
+            .isEqualTo(holidayReplacementEntity)
+            .isEqualTo(sameHolidayReplacementEntity)
+            .isNotEqualTo(null)
+            .isNotEqualTo(new Object())
+            .isNotEqualTo(otherHolidayReplacement)
+            .isNotEqualTo(holidayReplacementEntityOtherNote);
+    }
+
+    @Test
+    void ensureHashCode() {
+
+        final Person holidayReplacement = new Person();
+        final HolidayReplacementEntity holidayReplacementEntity = new HolidayReplacementEntity();
+        holidayReplacementEntity.setPerson(holidayReplacement);
+        holidayReplacementEntity.setNote("first Note");
+
+        final HolidayReplacementEntity sameHolidayReplacementEntity = new HolidayReplacementEntity();
+        sameHolidayReplacementEntity.setPerson(holidayReplacement);
+        sameHolidayReplacementEntity.setNote("first Note");
+
+        final Person otherHolidayReplacement = new Person();
+        final HolidayReplacementEntity holidayReplacementEntityOtherPerson = new HolidayReplacementEntity();
+        holidayReplacementEntityOtherPerson.setPerson(otherHolidayReplacement);
+        holidayReplacementEntityOtherPerson.setNote("first Note");
+
+        final HolidayReplacementEntity holidayReplacementEntityOtherNote = new HolidayReplacementEntity();
+        holidayReplacementEntityOtherNote.setPerson(holidayReplacement);
+        holidayReplacementEntityOtherNote.setNote("second Note");
+
+        assertThat(holidayReplacementEntity.hashCode())
+            .isEqualTo(holidayReplacementEntity.hashCode())
+            .isEqualTo(sameHolidayReplacementEntity.hashCode())
+            .isNotEqualTo(otherHolidayReplacement.hashCode())
+            .isNotEqualTo(new Object().hashCode())
+            .isNotEqualTo(holidayReplacementEntityOtherNote.hashCode());
+    }
+}
