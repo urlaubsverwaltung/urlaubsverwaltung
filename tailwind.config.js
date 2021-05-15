68ff4594eeb8dcede504d1d67320d541a94728ef
Benjamin Seber <seber@synyx.de>
2021-05-15 21:39:15 +0200

    do not use tailwind for styling
    we can add the config anytime, when we need `border-3` in plain html.
    for now... we're just writing simple CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,6 @@ module.exports = {
     preflight: false,
   },
   theme: {
-    borderWidth: {
-      DEFAULT: "1px",
-      0: "0",
-      2: "2px",
-      3: "3px",
-      4: "4px",
-    },
     extend: {
       screens: {
         print: { raw: "print" },
