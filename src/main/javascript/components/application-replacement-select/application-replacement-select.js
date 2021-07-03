bad9a352b098ff3605932c56159a44d039266add
Benjamin Seber <seber@synyx.de>
2021-07-03 20:25:10 +0200

    add loading feedback when replacement is selected
    delay rendering a bit to provide a smooth transition for the user

--- src/main/javascript/components/application-replacement-select/application-replacement-select.js
+++ src/main/javascript/components/application-replacement-select/application-replacement-select.js
@@ -5,23 +5,54 @@ const submitButton = selectElement.parentNode.parentNode.querySelector("button")
 
 const formaction = submitButton.getAttribute("formaction");
 
+const spinner = `
+  <svg class="tw-animate-spin tw-transition-all tw-h-4 tw-w-0 tw-text-black" fill="none" viewBox="0 0 24 24" width="16px" height="16px" xmlns="http://www.w3.org/2000/svg">
+    <circle class="tw-opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    <path class="tw-opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+  </svg>
+`;
+
+const svg = htmlStringToNode(spinner);
+submitButton.prepend(svg);
+submitButton.classList.add("tw-flex", "tw-items-center");
+
 selectElement.addEventListener("change", async function (event) {
-  const response = await post(`${formaction}/replacements`, {
-    body: new FormData(event.target.closest("form")),
-    headers: {
-      Accept: "text/html",
-    },
-  });
+  submitButton.firstElementChild.classList.add("tw-w-4", "tw-mr-2");
+  submitButton.setAttribute("disabled", "");
+
+  const [{ value: response }] = await Promise.allSettled([
+    post(`${formaction}/replacements`, {
+      body: new FormData(event.target.closest("form")),
+      headers: {
+        Accept: "text/html",
+      },
+    }),
+    wait(300),
+  ]);
 
   if (response.ok) {
     const renderedHtml = await response.text();
     if (renderedHtml) {
-      const template = document.createElement("template");
-      template.innerHTML = renderedHtml;
-      document.querySelector("#replacement-section-container ul").prepend(template.content);
+      const li = htmlStringToNode(renderedHtml);
+      document.querySelector("#replacement-section-container ul").prepend(li);
 
       selectElement.querySelector('option[value="' + selectElement.value + '"]').remove();
       selectElement.blur();
     }
   }
+
+  submitButton.firstElementChild.classList.remove("tw-w-4", "tw-mr-2");
+  submitButton.removeAttribute("disabled");
 });
+
+function wait(delay) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+function htmlStringToNode(htmlText) {
+  const template = document.createElement("template");
+  template.innerHTML = htmlText;
+  return template.content;
+}
