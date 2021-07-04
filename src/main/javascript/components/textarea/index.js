596ef3151361fa63f66e8851a0484dcd35529f7c
Benjamin Seber <seber@synyx.de>
2021-07-04 13:50:07 +0200

    consider original textarea row value on blur

--- src/main/javascript/components/textarea/index.js
+++ src/main/javascript/components/textarea/index.js
@@ -1,14 +1,16 @@
-const standardNumberOfRows = 1;
 const expandedNumberOfRows = 4;
+
+const textareas = new WeakMap();
 
 document.addEventListener("focusin", function (event) {
   if (event.target.tagName === "TEXTAREA") {
+    textareas.set(event.target, event.target.rows);
     event.target.rows = expandedNumberOfRows;
   }
 });
 
 document.addEventListener("focusout", function (event) {
   if (event.target.tagName === "TEXTAREA") {
-    event.target.rows = standardNumberOfRows;
+    event.target.rows = textareas.get(event.target) || 1;
   }
 });
