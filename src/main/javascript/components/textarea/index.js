b2c3ef6e03b2c67f8ad902109a351c47eb6cbaf5
Benjamin Seber <seber@synyx.de>
2021-07-09 09:52:40 +0200

    add textarea unit test

--- src/main/javascript/components/textarea/index.js
+++ src/main/javascript/components/textarea/index.js
@@ -1,16 +1,16 @@
-const expandedNumberOfRows = 4;
+const expandedNumberOfRows = "4";
 
 const textareas = new WeakMap();
 
 document.addEventListener("focusin", function (event) {
   if (event.target.tagName === "TEXTAREA") {
-    textareas.set(event.target, event.target.rows);
-    event.target.rows = expandedNumberOfRows;
+    textareas.set(event.target, event.target.getAttribute("rows"));
+    event.target.setAttribute("rows", expandedNumberOfRows);
   }
 });
 
 document.addEventListener("focusout", function (event) {
   if (event.target.tagName === "TEXTAREA") {
-    event.target.rows = textareas.get(event.target) || 1;
+    event.target.setAttribute("rows", textareas.get(event.target) || "1");
   }
 });
