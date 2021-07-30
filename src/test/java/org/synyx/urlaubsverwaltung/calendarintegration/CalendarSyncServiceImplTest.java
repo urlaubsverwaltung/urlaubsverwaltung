41c2db2314c326ec0ae0d9c1cb2c536821a842bc
honnel <github@honnel.de>
2021-07-30 17:20:00 +0200

    Fix test setup for calender sync service

--- src/test/java/org/synyx/urlaubsverwaltung/calendarintegration/CalendarSyncServiceImplTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/calendarintegration/CalendarSyncServiceImplTest.java
@@ -2,7 +2,9 @@ package org.synyx.urlaubsverwaltung.calendarintegration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.synyx.urlaubsverwaltung.absence.Absence;
 import org.synyx.urlaubsverwaltung.calendarintegration.providers.exchange.ExchangeCalendarProvider;
 import org.synyx.urlaubsverwaltung.calendarintegration.settings.CalendarSettingsEntity;
@@ -11,12 +13,15 @@ import org.synyx.urlaubsverwaltung.calendarintegration.settings.CalendarSettings
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 /**
  * Unit test for {@link CalendarSyncServiceImpl}.
  */
+@ExtendWith(MockitoExtension.class)
 class CalendarSyncServiceImplTest {
 
     private CalendarSyncService calendarSyncService;
@@ -30,11 +35,9 @@ class CalendarSyncServiceImplTest {
     @BeforeEach
     void setUp() {
 
-        calendarSettingsEntity.setProvider(ExchangeCalendarProvider.class.getSimpleName());
+        calendarSettingsEntity.setProvider(exchangeCalendarProvider.getClass().getSimpleName());
         when(calendarSettingsService.getSettings()).thenReturn(calendarSettingsEntity);
 
-
-        exchangeCalendarProvider = mock(ExchangeCalendarProvider.class);
         calendarSyncService = new CalendarSyncServiceImpl(calendarSettingsService, List.of(exchangeCalendarProvider));
     }
 
