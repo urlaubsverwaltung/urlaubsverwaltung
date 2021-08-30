91f14a1f48401291dfe5798fd040226c8df34fc8
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2021-08-30 22:03:13 +0200

    If no overtime or specialLeave is found ignore hiding

--- src/main/javascript/js/application/vacation-type-changed.js
+++ src/main/javascript/js/application/vacation-type-changed.js
@@ -6,14 +6,14 @@ function vacationTypeChanged(value) {
   const specialLeave = document.querySelector("#special-leave");
 
   if (value === SPECIAL_LEAVE) {
-    overtime.classList.add("hidden");
-    specialLeave.classList.remove("hidden");
+    if (overtime) overtime.classList.add("hidden");
+    if (specialLeave) specialLeave.classList.remove("hidden");
   } else if (value === OVERTIME) {
-    overtime.classList.remove("hidden");
-    specialLeave.classList.add("hidden");
+    if (overtime) overtime.classList.remove("hidden");
+    if (specialLeave) specialLeave.classList.add("hidden");
   } else {
-    overtime.classList.add("hidden");
-    specialLeave.classList.add("hidden");
+    if (overtime) overtime.classList.add("hidden");
+    if (specialLeave) specialLeave.classList.add("hidden");
   }
 }
 
