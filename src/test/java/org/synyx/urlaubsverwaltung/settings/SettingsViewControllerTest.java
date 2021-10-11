2baafa0e7395c563424492e71e7ffa575b8ffa1c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2021-10-11 13:32:20 +0200

    Remove deprecated WorkingTimeProperties

--- src/test/java/org/synyx/urlaubsverwaltung/settings/SettingsViewControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/settings/SettingsViewControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.validation.Errors;
@@ -18,7 +17,6 @@ import org.synyx.urlaubsverwaltung.workingtime.FederalState;
 import java.time.Clock;
 import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
