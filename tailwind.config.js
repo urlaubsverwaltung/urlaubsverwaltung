201f957cd2505d87e2aab34c7ae4a82cccff4443
Benjamin Seber <seber@synyx.de>
2021-11-07 21:44:35 +0100

    style calendar with themed variables
    NOTE:
    this needs https://github.com/tailwindlabs/tailwindcss/pull/5871 to be released.
    currently you can patch tailwindcss/lib/util/transformThemeValue.js yourself, however :o)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 const defaultConfig = require("tailwindcss/defaultConfig.js");
 
+function withOpacity(variable) {
+  return ({ opacityValue }) => (opacityValue ? `rgba(var(${variable}), ${opacityValue})` : `rgb(var(${variable}))`);
+}
+
 module.exports = {
   content: [
     "./src/main/webapp/**/*.jsp",
@@ -30,6 +34,47 @@ module.exports = {
         "black-almost": "#444444",
         "bootstrap-green": "#5cb85c",
         "bootstrap-green-dark": "#449d44",
+      },
+      borderColor: {
+        calendar: {
+          DEFAULT: withOpacity("--uv-cal-border-color"),
+        },
+      },
+      textColor: {
+        calendar: {
+          DEFAULT: withOpacity("--uv-cal-default-color"),
+          hover: withOpacity("--uv-cal-default-color-hover"),
+          past: withOpacity("--uv-cal-past-color"),
+          "past-hover": withOpacity("--uv-cal-past-color-hover"),
+          weekend: withOpacity("--uv-cal-weekend-color"),
+          "weekend-hover": withOpacity("--uv-cal-weekend-color-hover"),
+        },
+        "sick-note": withOpacity("--uv-cal-sick-note-color"),
+        "sick-note-hover": withOpacity("--uv-cal-sick-note-color-hover"),
+        "public-holiday": withOpacity("--uv-cal-public-holiday-color"),
+        "public-holiday-hover": withOpacity("--uv-cal-public-holiday-color-hover"),
+        "personal-holiday": withOpacity("--uv-cal-personal-holiday-color"),
+        "personal-holiday-hover": withOpacity("--uv-cal-personal-holiday-color-hover"),
+        "personal-holiday-approved": withOpacity("--uv-cal-personal-holiday-approved-color"),
+        "personal-holiday-approved-hover": withOpacity("--uv-cal-personal-holiday-approved-color-hover"),
+      },
+      backgroundColor: {
+        calendar: {
+          DEFAULT: withOpacity("--uv-cal-default-bg"),
+          hover: withOpacity("--uv-cal-default-bg-hover"),
+          past: withOpacity("--uv-cal-past-bg"),
+          "past-hover": withOpacity("--uv-cal-past-hover-bg"),
+          weekend: withOpacity("--uv-cal-weekend-bg"),
+          "weekend-hover": withOpacity("--uv-cal-weekend-bg-hover"),
+        },
+        "sick-note": withOpacity("--uv-cal-sick-note-bg"),
+        "sick-note-hover": withOpacity("--uv-cal-sick-note-bg-hover"),
+        "public-holiday": withOpacity("--uv-cal-public-holiday-bg"),
+        "public-holiday-hover": withOpacity("--uv-cal-public-holiday-bg-hover"),
+        "personal-holiday": withOpacity("--uv-cal-personal-holiday-bg"),
+        "personal-holiday-hover": withOpacity("--uv-cal-personal-holiday-bg-hover"),
+        "personal-holiday-approved": withOpacity("--uv-cal-personal-holiday-approved-bg"),
+        "personal-holiday-approved-hover": withOpacity("--uv-cal-personal-holiday-approved-bg-hover"),
       },
     },
     screens: {
