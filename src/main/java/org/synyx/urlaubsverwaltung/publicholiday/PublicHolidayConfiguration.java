342fa7f97350f2c4275c99351b650bfe5acaa86a
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-01-05 23:17:27 +0100

    Add public holidays for switzerland

--- src/main/java/org/synyx/urlaubsverwaltung/publicholiday/PublicHolidayConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/publicholiday/PublicHolidayConfiguration.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @Configuration
 class PublicHolidayConfiguration {
 
-    private static final List<String> COUNTRIES = List.of("de", "at");
+    private static final List<String> COUNTRIES = List.of("de", "at", "ch");
 
     @Bean
     Map<String, HolidayManager> holidayManagerMap() {
