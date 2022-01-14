dcc3c0075866a777a3d19e2b1c23f7b0d48f0c3f
Benjamin Seber <seber@synyx.de>
2022-01-14 14:28:57 +0100

    fix department list date sorting

--- src/main/javascript/bundles/department-list.js
+++ src/main/javascript/bundles/department-list.js
@@ -1,9 +1,16 @@
 import $ from "jquery";
 import "tablesorter";
 import "../components/popover";
+import { dateParser } from "../components/table-sortable/parser-date";
 
 $(document).ready(function () {
-  $("table.sortable").tablesorter({
+  $.tablesorter.addParser(dateParser);
+
+  $("#department-table").tablesorter({
     sortList: [[0, 0]],
+    headers: {
+      3: { sorter: dateParser.id },
+      4: false,
+    },
   });
 });
