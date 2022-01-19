68917ce93614603df7a8d3a60c3675e5b3550e29
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-01-19 20:38:07 +0100

    Cleanups

--- src/main/java/org/synyx/urlaubsverwaltung/UrlaubsverwaltungApplication.java
+++ src/main/java/org/synyx/urlaubsverwaltung/UrlaubsverwaltungApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * Spring Boot Entry Point.
- */
 @SpringBootApplication
 @EnableScheduling
 public class UrlaubsverwaltungApplication {
