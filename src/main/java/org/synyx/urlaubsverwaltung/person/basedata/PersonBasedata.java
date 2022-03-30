f79a9e28aca5f6a1653c176a4e69b92a90aa9676
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-03-30 23:50:26 +0200

    Remove not used personBasedata builder

--- src/main/java/org/synyx/urlaubsverwaltung/person/basedata/PersonBasedata.java
+++ src/main/java/org/synyx/urlaubsverwaltung/person/basedata/PersonBasedata.java
@@ -12,35 +12,6 @@ public class PersonBasedata {
         this.additionalInformation = additionalInformation;
     }
 
-    static PersonBasedata.Builder builder() {
-        return new PersonBasedata.Builder();
-    }
-
-    static class Builder {
-        private int personId;
-        private String personnelNumber;
-        private String additionalInformation;
-
-        public Builder withPersonId(int personId) {
-            this.personId = personId;
-            return this;
-        }
-
-        public Builder withPersonnelNumber(String personnelNumber) {
-            this.personnelNumber = personnelNumber;
-            return this;
-        }
-
-        public Builder withAdditionalInformation(String additionalInformation) {
-            this.additionalInformation = additionalInformation;
-            return this;
-        }
-
-        public PersonBasedata build() {
-            return new PersonBasedata(personId, personnelNumber, additionalInformation);
-        }
-    }
-
     public int getPersonId() {
         return personId;
     }
