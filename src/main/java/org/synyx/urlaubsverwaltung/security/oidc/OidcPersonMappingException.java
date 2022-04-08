eabc1a3afa22d651b1809b4294df5588bade53a6
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-04-08 11:55:52 +0200

    OIDC: username, firstname, lastname and email has to be provided

--- src/main/java/org/synyx/urlaubsverwaltung/security/oidc/OidcPersonMappingException.java
+++ src/main/java/org/synyx/urlaubsverwaltung/security/oidc/OidcPersonMappingException.java
@@ -3,7 +3,6 @@ package org.synyx.urlaubsverwaltung.security.oidc;
 import org.springframework.security.core.AuthenticationException;
 
 public class OidcPersonMappingException extends AuthenticationException {
-
     OidcPersonMappingException(String message) {
         super(message);
     }
