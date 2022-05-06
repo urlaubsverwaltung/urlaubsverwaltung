1179c1c690760560cf2b1a6674b1d68050be366b
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-06 16:34:31 +0200

    Cleanup

--- src/main/java/org/synyx/urlaubsverwaltung/person/web/PersonPropertyEditor.java
+++ src/main/java/org/synyx/urlaubsverwaltung/person/web/PersonPropertyEditor.java
@@ -1,12 +1,12 @@
 package org.synyx.urlaubsverwaltung.person.web;
 
-import org.springframework.util.StringUtils;
 import org.synyx.urlaubsverwaltung.person.Person;
 import org.synyx.urlaubsverwaltung.person.PersonService;
 
 import java.beans.PropertyEditorSupport;
 import java.util.Optional;
 
+import static org.springframework.util.StringUtils.hasText;
 
 /**
  * Convert {@link Person}'s id to {@link Person} object.
@@ -16,32 +16,25 @@ public class PersonPropertyEditor extends PropertyEditorSupport {
     private final PersonService personService;
 
     public PersonPropertyEditor(PersonService personService) {
-
         this.personService = personService;
     }
 
     @Override
     public String getAsText() {
-
         if (this.getValue() == null) {
             return "";
         }
-
         return ((Person) this.getValue()).getId().toString();
     }
 
-
     @Override
     public void setAsText(String text) {
-
-        if (!StringUtils.hasText(text)) {
+        if (!hasText(text)) {
             return;
         }
 
-        Integer id = Integer.valueOf(text);
-
-        Optional<Person> person = personService.getPersonByID(id);
-
+        final Integer id = Integer.valueOf(text);
+        final Optional<Person> person = personService.getPersonByID(id);
         if (person.isPresent()) {
             setValue(person.get());
         } else {
