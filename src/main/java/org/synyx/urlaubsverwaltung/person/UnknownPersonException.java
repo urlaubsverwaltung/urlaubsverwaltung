ce9b01c76be60b29c9d551801223734db111034d
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-06 17:02:59 +0200

    Cleanup

--- src/main/java/org/synyx/urlaubsverwaltung/person/UnknownPersonException.java
+++ src/main/java/org/synyx/urlaubsverwaltung/person/UnknownPersonException.java
@@ -2,20 +2,16 @@ package org.synyx.urlaubsverwaltung.person;
 
 import org.synyx.urlaubsverwaltung.web.AbstractNoResultFoundException;
 
-
 /**
  * Thrown in case no person found for a certain ID.
  */
 public class UnknownPersonException extends AbstractNoResultFoundException {
 
     public UnknownPersonException(Integer id) {
-
         super(id, "person");
     }
 
-
     public UnknownPersonException(String username) {
-
         super(username, "person");
     }
 }
