2c01c51155901043f27b67410b782031e85dad49
Benjamin Seber <seber@synyx.de>
2022-05-16 18:21:04 +0200

    add keyboard navigation to color-picker

--- src/main/javascript/components/color-picker/color-picker.js
+++ src/main/javascript/components/color-picker/color-picker.js
@@ -6,6 +6,9 @@ class ColorPicker extends HTMLDivElement {
   #open = false;
   #dialogToggleButton;
   #dialogToggleCheckbox;
+  #dialog;
+  #colorOptions;
+  #focusedElementIndex;
 
   attributeChangedCallback(name, oldValue, newValue) {
     this.#open = typeof newValue === "string";
@@ -15,6 +18,16 @@ class ColorPicker extends HTMLDivElement {
   connectedCallback() {
     this.#dialogToggleButton = this.querySelector("[class~='color-picker-button']");
     this.#dialogToggleCheckbox = this.querySelector(`#${this.#dialogToggleButton.getAttribute("for")}`);
+    this.#dialog = this.querySelector(".color-picker-dialog");
+    this.#colorOptions = this.#dialog.querySelectorAll("label");
+
+    this.#focusedElementIndex = 0; // TODO set to actual color
+
+    this.setAttribute("tabindex", "0");
+    this.classList.add("focus:tw-outline-2", "focus:tw-outline-blue-500");
+    for (let input of this.querySelectorAll("input")) {
+      input.setAttribute("tabindex", "-1");
+    }
 
     // prevent native checkbox selection.
     // otherwise the browser jumps to the checkbox element (which is positioned absolutely on top of the screen)
@@ -40,12 +53,62 @@ class ColorPicker extends HTMLDivElement {
       }
     };
 
+    const handleKeyDown = (event) => {
+      if (document.activeElement === this) {
+        // toggle dialog with keyboard
+        // closing it with 'Escape' is handled globally
+        if (event.key === "Enter") {
+          if (this.#open) {
+            // close dialog
+            delete this.dataset.open;
+            // and update selected color
+            if (this.#focusedElementIndex !== undefined) {
+              for (let index = 0; index < this.#colorOptions.length; index++) {
+                let colorInput = this.#colorOptions[index].querySelector("input");
+                colorInput.checked = index === this.#focusedElementIndex;
+                if (index === this.#focusedElementIndex) {
+                  this.#dialogToggleButton.style.setProperty("background-color", colorInput.value);
+                }
+              }
+            }
+          } else {
+            this.dataset.open = "";
+          }
+        }
+
+        if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+          event.preventDefault();
+          this.dataset.open = "";
+        }
+
+        // enable keyboard navigation through color options
+        if (event.key === "ArrowDown") {
+          if (this.#focusedElementIndex < this.#colorOptions.length - 1) {
+            this.#focusedElementIndex++;
+            this.#render();
+          }
+        } else if (event.key === "ArrowUp" && this.#focusedElementIndex > 0) {
+          this.#focusedElementIndex--;
+          this.#render();
+        }
+      }
+    };
+
+    const handleFocusOut = () => {
+      delete this.dataset.open;
+      // TODO reset focusedElementIndex to actual selected color
+    };
+
     this.#dialogToggleButton.addEventListener("click", handleDialogToggleButtonClick);
     this.addEventListener("click", handleClick);
+    this.addEventListener("keydown", handleKeyDown);
+    this.addEventListener("focusout", handleFocusOut);
 
     this.cleanup = function () {
       this.#dialogToggleButton.removeEventListener("click", handleDialogToggleButtonClick);
       this.removeEventListener("click", handleClick);
+      this.removeEventListener("keydown", handleKeyDown);
+      this.removeEventListener("focusout", handleFocusOut);
     };
   }
 
@@ -55,6 +118,16 @@ class ColorPicker extends HTMLDivElement {
 
   #render() {
     this.#dialogToggleCheckbox.checked = this.#open;
+
+    // update currently focused color option
+    const focusedColorOption = this.#colorOptions[this.#focusedElementIndex];
+    for (let colorOption of this.#colorOptions) {
+      if (colorOption === focusedColorOption) {
+        colorOption.classList.add("tw-opacity-80");
+      } else {
+        colorOption.classList.remove("tw-opacity-80");
+      }
+    }
   }
 }
 
