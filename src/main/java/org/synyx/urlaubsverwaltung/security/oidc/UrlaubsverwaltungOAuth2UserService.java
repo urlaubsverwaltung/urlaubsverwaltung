e47dd64438653e0082d00c1521d57eb21133b706
Johannes Graf <graf@synyx.de>
2022-05-24 15:35:06 +0200

    introduce UrlaubsverwaltungOAuth2UserService
    this change expects that the OidcUser contains a claim with name
    "groups" and includes a group called "urlaubsverwaltung_user"
    
    both expectations have to be met, otherwise the access to
    urlaubsverwaltung is not possible

---
+++ src/main/java/org/synyx/urlaubsverwaltung/security/oidc/UrlaubsverwaltungOAuth2UserService.java
@@ -0,0 +1,55 @@
+package org.synyx.urlaubsverwaltung.security.oidc;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class UrlaubsverwaltungOAuth2UserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
+
+    // TODO make this constants configurable from application.properties
+    private static final String PERMISSION_CLAIM = "groups";
+    private final OidcUserService delegate;
+
+    public UrlaubsverwaltungOAuth2UserService(OidcUserService delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+
+        final OidcUser oidcUser = delegate.loadUser(userRequest);
+
+        final List<GrantedAuthority> fromGroupsClaim = parseAuthoritiesFromGroupsClaim(oidcUser.getClaims());
+
+        final List<GrantedAuthority> combinedAuthorities = Stream.concat(oidcUser.getAuthorities().stream(), fromGroupsClaim.stream()).collect(toList());
+
+        return new DefaultOidcUser(combinedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+    }
+
+    private List<GrantedAuthority> parseAuthoritiesFromGroupsClaim(Map<String, Object> claims) {
+        return extractFromList(claims, PERMISSION_CLAIM)
+            .stream()
+            .map(role -> new SimpleGrantedAuthority(String.valueOf(role)))
+            .collect(toList());
+    }
+
+    private List<String> extractFromList(Map<String, Object> myMap, String key) {
+        Object roles = myMap.get(key);
+        if (roles instanceof List) {
+            return (List<String>) roles;
+        }
+        return Collections.emptyList();
+    }
+}
