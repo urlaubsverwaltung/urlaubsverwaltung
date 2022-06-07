6bdab49abc0ea319e7fdc7c9c9f264bf61f9ae58
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-06-07 14:13:46 +0200

    Remove old imports in settings-form

--- src/main/javascript/bundles/settings-form.js
+++ src/main/javascript/bundles/settings-form.js
@@ -1,5 +1,4 @@
 import "../components/color-picker";
 import "../components/tabs";
-import "../js/settings/settings-form";
 import "../js/settings/absence-types";
 import "../js/settings/special-leave-table";
