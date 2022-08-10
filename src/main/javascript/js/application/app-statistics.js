d1f9e564e6629d938f4ddc28bc8ea85d4bb98ff2
Benjamin Seber <seber@synyx.de>
2022-08-10 11:01:39 +0200

    remove sortable table stuff from application statistics
    must happen on the server now with pagination

--- src/main/javascript/js/application/app-statistics.js
+++ src/main/javascript/js/application/app-statistics.js
@@ -1,40 +1,4 @@
-import $ from "jquery";
-import "tablesorter";
-import { dataValueNumberParser } from "../../components/table-sortable/parser-data-value-number";
 import { createDatepicker } from "../../components/datepicker";
-
-$(document).ready(function () {
-  $.tablesorter.addParser(dataValueNumberParser);
-
-  const { length: columnCount } = document.querySelectorAll("#application-statistic-table thead th");
-  const isPersonnelNumberColumnRendered = columnCount === 11;
-
-  if (isPersonnelNumberColumnRendered) {
-    $("#application-statistic-table").tablesorter({
-      sortList: [[2, 0]],
-      headers: {
-        0: { sorter: false },
-        4: { sorter: false },
-        5: { sorter: dataValueNumberParser.id },
-        6: { sorter: dataValueNumberParser.id },
-        7: { sorter: dataValueNumberParser.id },
-        8: { sorter: dataValueNumberParser.id },
-      },
-    });
-  } else {
-    $("#application-statistic-table").tablesorter({
-      sortList: [[1, 0]],
-      headers: {
-        0: { sorter: false },
-        3: { sorter: false },
-        4: { sorter: dataValueNumberParser.id },
-        5: { sorter: dataValueNumberParser.id },
-        6: { sorter: dataValueNumberParser.id },
-        7: { sorter: dataValueNumberParser.id },
-      },
-    });
-  }
-});
 
 const getPersonId = () => {};
 const urlPrefix = ""; // not required, no absences are fetched since we have no single person (personId)
