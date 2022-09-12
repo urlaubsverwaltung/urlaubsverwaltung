227d2842ee737e87a5fb324bc7b76fb39240df50
Benjamin Seber <benjamin.seber@focus-shift.de>
2022-09-12 11:44:37 +0200

    enable/disable input RemainingVacationDaysExpire fields

--- src/main/javascript/js/account/account-form.js
+++ src/main/javascript/js/account/account-form.js
@@ -10,3 +10,19 @@ function getPersonId() {
 createDatepicker("#holidaysAccountValidFrom", { urlPrefix, getPersonId });
 createDatepicker("#holidaysAccountValidTo", { urlPrefix, getPersonId });
 createDatepicker("#expiryDate", { urlPrefix, getPersonId });
+
+document.addEventListener("change", function enabledDisableVacationDaysExpireElements(event) {
+  if (event.target.matches("[name='doRemainingVacationDaysExpireLocally']")) {
+    const dateElement = document.querySelector("[name='expiryDate']");
+    const notExpiringElement = document.querySelector("[name='remainingVacationDaysNotExpiring']");
+
+    const enable = event.target.dataset.value === "true";
+    if (enable) {
+      dateElement.removeAttribute("disabled");
+      notExpiringElement.removeAttribute("disabled");
+    } else {
+      dateElement.setAttribute("disabled", "");
+      notExpiringElement.setAttribute("disabled", "");
+    }
+  }
+});
