ea481f57ef9166eeaf8ae44c33d0ad4523856805
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-10-25 11:03:15 +0200

    Overtime details now in thymeleaf

--- src/test/java/org/synyx/urlaubsverwaltung/overtime/web/OvertimeViewControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/overtime/web/OvertimeViewControllerTest.java
@@ -338,7 +338,7 @@ class OvertimeViewControllerTest {
 
         perform(get("/web/overtime/2"))
             .andExpect(status().isOk())
-            .andExpect(view().name("overtime/overtime_details"))
+            .andExpect(view().name("thymeleaf/overtime/overtime_details"))
             .andExpect(model().attribute("signedInUser", is(overtimePerson)))
             .andExpect(model().attribute("overtimeTotal", is(Duration.ofHours(1))))
             .andExpect(model().attribute("overtimeLeft", is(Duration.ZERO)))
