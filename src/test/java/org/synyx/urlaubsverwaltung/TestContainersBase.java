2a4d371369ad1f0963f3f6deb8d75004d9958404
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-11-17 17:07:14 +0100

    Use TestMariaDBContainer in TestContainersBase

--- src/test/java/org/synyx/urlaubsverwaltung/TestContainersBase.java
+++ src/test/java/org/synyx/urlaubsverwaltung/TestContainersBase.java
@@ -2,20 +2,14 @@ package org.synyx.urlaubsverwaltung;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MariaDBContainer;
-
-import static org.testcontainers.containers.MariaDBContainer.NAME;
 
 public abstract class TestContainersBase {
 
-    static final MariaDBContainer<?> mariaDB = new MariaDBContainer<>(NAME + ":10.6.11")
-        .withCommand("--character-set-server=utf8mb4", "--collation-server=utf8mb4_unicode_ci", "--max-connections=300");
+    static final TestMariaDBContainer mariaDB = new TestMariaDBContainer();
 
     @DynamicPropertySource
     static void mariaDBProperties(DynamicPropertyRegistry registry) {
         mariaDB.start();
-        registry.add("spring.datasource.url", mariaDB::getJdbcUrl);
-        registry.add("spring.datasource.username", mariaDB::getUsername);
-        registry.add("spring.datasource.password", mariaDB::getPassword);
+        mariaDB.configureSpringDataSource(registry);
     }
 }
