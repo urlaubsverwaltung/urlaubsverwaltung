d5e647afd1f80cf83b12d7e8516374a713ec40ef
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-09 20:13:17 +0100

    Reduce visibility of MissingGroupsClaimAuthorityException and remove not used constructor

--- src/main/java/org/synyx/urlaubsverwaltung/security/oidc/MissingGroupsClaimAuthorityException.java
+++ src/main/java/org/synyx/urlaubsverwaltung/security/oidc/MissingGroupsClaimAuthorityException.java
@@ -10,17 +10,7 @@ public class MissingGroupsClaimAuthorityException extends AccountStatusException
      * Constructs a <code>MissingAuthorityException</code> with the specified message.
      * @param msg the detail message
      */
-    public MissingGroupsClaimAuthorityException(String msg) {
+    MissingGroupsClaimAuthorityException(String msg) {
         super(msg);
-    }
-
-    /**
-     * Constructs a <code>MissingAuthorityException</code> with the specified message and root
-     * cause.
-     * @param msg the detail message
-     * @param cause root cause
-     */
-    public MissingGroupsClaimAuthorityException(String msg, Throwable cause) {
-        super(msg, cause);
     }
 }
