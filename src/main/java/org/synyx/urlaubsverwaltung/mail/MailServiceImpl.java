7dbbe8115bd94537691be94ed97a33893dc77603
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-17 21:39:30 +0100

    Improve 'could not send mail' logging

--- src/main/java/org/synyx/urlaubsverwaltung/mail/MailServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/mail/MailServiceImpl.java
@@ -64,7 +64,7 @@ class MailServiceImpl implements MailService {
                     () -> mailSenderService.sendEmail(sender, email, subject, body)
                 );
             } else {
-                LOG.info("No valid E-Mail-Address of {} is null - no e-mail will be send.", recipient.getId());
+                LOG.info("Could not send mail to E-Mail-Address of person with id {}, because email is null.", recipient.getId());
             }
         });
     }
