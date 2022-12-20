086ab338a136554d944898a7f962cce311f617c5
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-20 10:30:34 +0100

    Finalize

--- src/main/java/org/synyx/urlaubsverwaltung/person/PersonUpdatedEvent.java
+++ src/main/java/org/synyx/urlaubsverwaltung/person/PersonUpdatedEvent.java
@@ -9,7 +9,7 @@ public class PersonUpdatedEvent extends ApplicationEvent {
     private final String personNiceName;
     private final String username;
     private final String email;
-    private boolean active;
+    private final boolean active;
 
     PersonUpdatedEvent(Object source, Integer personId, String personNiceName, String username, @Nullable String email, boolean active) {
         super(source);
