4d71ee3568af96e07db36efa696728a4ab03a2b1
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-21 23:31:20 +0100

    Remove war

--- src/main/java/org/synyx/urlaubsverwaltung/UrlaubsverwaltungApplication.java
+++ src/main/java/org/synyx/urlaubsverwaltung/UrlaubsverwaltungApplication.java
@@ -2,17 +2,9 @@ package org.synyx.urlaubsverwaltung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UrlaubsverwaltungApplication extends SpringBootServletInitializer {
-
-    @Override
-    @Deprecated
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(UrlaubsverwaltungApplication.class);
-    }
+public class UrlaubsverwaltungApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UrlaubsverwaltungApplication.class, args);
