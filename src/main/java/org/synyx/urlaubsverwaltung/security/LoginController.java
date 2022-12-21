5fc496f6f7cad0461d550e83e5295f281773f84b
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-22 00:06:48 +0100

    Remove version from login

--- src/main/java/org/synyx/urlaubsverwaltung/security/LoginController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/security/LoginController.java
@@ -1,9 +1,6 @@
 package org.synyx.urlaubsverwaltung.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,16 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login")
 public class LoginController {
 
-    private final String applicationVersion;
-
-    @Autowired
-    public LoginController(@Value("${info.app.version}") String applicationVersion) {
-        this.applicationVersion = applicationVersion;
-    }
-
     @GetMapping("")
-    public String login(Model model) {
-        model.addAttribute("version", applicationVersion);
+    public String login() {
         return "thymeleaf/login/login";
     }
 }
--- src/test/java/org/synyx/urlaubsverwaltung/security/LoginControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/security/LoginControllerTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -15,17 +13,14 @@ class LoginControllerTest {
 
     private LoginController sut;
 
-    private static final String APPLICATION_VERSION = "1.1.1";
-
     @BeforeEach
     void setUp() {
-        sut = new LoginController(APPLICATION_VERSION);
+        sut = new LoginController();
     }
 
     @Test
     void ensureLoginHasCorrectVersionAndView() throws Exception {
         perform(get("/login"))
-            .andExpect(model().attribute("version", equalTo(APPLICATION_VERSION)))
             .andExpect(view().name("thymeleaf/login/login"));
     }
 
