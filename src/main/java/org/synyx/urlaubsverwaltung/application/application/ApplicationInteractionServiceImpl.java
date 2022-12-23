ddb25302b8db27773442dea65241fdc94baa5838
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-23 23:29:02 +0100

    Do not save TimeSettings as member variable. Call it from SettingsService to get new one from database

--- src/main/java/org/synyx/urlaubsverwaltung/application/application/ApplicationInteractionServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/application/ApplicationInteractionServiceImpl.java
@@ -62,7 +62,7 @@ class ApplicationInteractionServiceImpl implements ApplicationInteractionService
     private final ApplicationMailService applicationMailService;
     private final CalendarSyncService calendarSyncService;
     private final AbsenceMappingService absenceMappingService;
-    private final TimeSettings timeSettings;
+    private final SettingsService settingsService;
     private final DepartmentService departmentService;
     private final Clock clock;
 
@@ -82,7 +82,7 @@ class ApplicationInteractionServiceImpl implements ApplicationInteractionService
         this.applicationMailService = applicationMailService;
         this.calendarSyncService = calendarSyncService;
         this.absenceMappingService = absenceMappingService;
-        this.timeSettings = settingsService.getSettings().getTimeSettings();
+        this.settingsService = settingsService;
         this.departmentService = departmentService;
         this.clock = clock;
     }
@@ -130,6 +130,7 @@ class ApplicationInteractionServiceImpl implements ApplicationInteractionService
         accountInteractionService.updateRemainingVacationDays(savedApplication.getStartDate().getYear(), person);
 
         if (calendarSyncService.isRealProviderConfigured()) {
+            final TimeSettings timeSettings = settingsService.getSettings().getTimeSettings();
             final AbsenceTimeConfiguration absenceTimeConfiguration = new AbsenceTimeConfiguration(timeSettings);
             final Absence absence = new Absence(savedApplication.getPerson(), savedApplication.getPeriod(), absenceTimeConfiguration);
             calendarSyncService.addAbsence(absence)
@@ -206,6 +207,7 @@ class ApplicationInteractionServiceImpl implements ApplicationInteractionService
         accountInteractionService.updateRemainingVacationDays(savedApplication.getStartDate().getYear(), person);
 
         if (calendarSyncService.isRealProviderConfigured()) {
+            final TimeSettings timeSettings = settingsService.getSettings().getTimeSettings();
             final AbsenceTimeConfiguration absenceTimeConfiguration = new AbsenceTimeConfiguration(timeSettings);
             final Absence absence = new Absence(savedApplication.getPerson(), savedApplication.getPeriod(), absenceTimeConfiguration);
             calendarSyncService.addAbsence(absence)
