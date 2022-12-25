54990bfbff8b5d3cc17fd34f2b4193ef6aec17c8
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-12-25 21:07:22 +0100

    add default special leave entities

--- src/main/java/org/synyx/urlaubsverwaltung/application/specialleave/SpecialLeaveSettingsRepository.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/specialleave/SpecialLeaveSettingsRepository.java
@@ -1,10 +1,11 @@
 package org.synyx.urlaubsverwaltung.application.specialleave;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-interface SpecialLeaveSettingsRepository extends CrudRepository<SpecialLeaveSettingsEntity, Long> {
+interface SpecialLeaveSettingsRepository extends JpaRepository<SpecialLeaveSettingsEntity, Long> {
 
     @Override
     List<SpecialLeaveSettingsEntity> findAll();
