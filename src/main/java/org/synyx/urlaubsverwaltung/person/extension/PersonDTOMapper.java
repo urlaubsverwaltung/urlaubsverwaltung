0d8d1d4d2f0248e6f624e9e8d5057834db15bea8
Johannes Graf <graf.johannes@gmail.com>
2023-01-13 15:04:52 +0100

    implement PersonServiceExtension

---
+++ src/main/java/org/synyx/urlaubsverwaltung/person/extension/PersonDTOMapper.java
@@ -0,0 +1,52 @@
+package org.synyx.urlaubsverwaltung.person.extension;
+
+import de.focus_shift.urlaubsverwaltung.extension.api.person.PersonDTO;
+import de.focus_shift.urlaubsverwaltung.extension.api.person.RoleDTO;
+import org.synyx.urlaubsverwaltung.person.Person;
+import org.synyx.urlaubsverwaltung.person.Role;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class PersonDTOMapper {
+
+    private PersonDTOMapper() {
+        // Utility classes should not have public constructors java:S1118
+    }
+    static Person toPerson(PersonDTO personDTO) {
+        final Person person = new Person(personDTO.getUsername(), personDTO.getLastName(), personDTO.getFirstName(), personDTO.getEmail());
+        person.setId(personDTO.getId());
+        person.setPermissions(toRoles(personDTO));
+        return person;
+    }
+
+    static PersonDTO toPersonDTO(Person person) {
+        return PersonDTO.builder()
+            .id(person.getId())
+            .username(person.getUsername())
+            .lastName(person.getLastName())
+            .firstName(person.getFirstName())
+            .email(person.getEmail())
+            .enabled(person.isActive())
+            .permissions(toRoleDTOs(person))
+            .build();
+    }
+
+    private static Set<Role> toRoles(PersonDTO personDTO) {
+        if (!personDTO.isEnabled()) {
+            return Set.of(Role.INACTIVE);
+        }
+        return personDTO.getPermissions()
+            .stream()
+            .map(roleDTO -> Role.valueOf(roleDTO.name()))
+            .collect(Collectors.toSet());
+    }
+
+    private static Set<RoleDTO> toRoleDTOs(Person person) {
+        return person.getPermissions()
+            .stream()
+            .filter(role -> !Role.INACTIVE.equals(role))
+            .map(role -> RoleDTO.valueOf(role.name()))
+            .collect(Collectors.toSet());
+    }
+}
---
+++ src/test/java/org/synyx/urlaubsverwaltung/person/extension/PersonServiceExtensionImplTest.java
@@ -0,0 +1,323 @@
+package org.synyx.urlaubsverwaltung.person.extension;
+
+
+import de.focus_shift.urlaubsverwaltung.extension.api.person.PersonDTO;
+import de.focus_shift.urlaubsverwaltung.extension.api.person.RoleDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
+import org.synyx.urlaubsverwaltung.person.Person;
+import org.synyx.urlaubsverwaltung.person.PersonService;
+import org.synyx.urlaubsverwaltung.person.Role;
+import org.synyx.urlaubsverwaltung.search.PageableSearchQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PersonServiceExtensionImplTest {
+
+    @Mock
+    private PersonService personService;
+
+    @InjectMocks
+    private PersonServiceExtensionImpl sut;
+
+    private static Person anyPerson() {
+        PersonDTO personDTO = anyPersonDTO(1);
+        final Person createdPerson = new Person(personDTO.getUsername(), personDTO.getLastName(), personDTO.getFirstName(), personDTO.getEmail());
+        createdPerson.setId(personDTO.getId());
+        createdPerson.setPermissions(Set.of(Role.USER));
+        return createdPerson;
+    }
+
+    private static PersonDTO anyPersonDTO() {
+        return anyPersonDTO(null);
+    }
+
+    private static PersonDTO anyPersonDTO(Integer id) {
+        return PersonDTO.builder()
+            .id(id)
+            .username("muster")
+            .lastName("Muster")
+            .firstName("Marlene")
+            .email("muster@example.org")
+            .enabled(true)
+            .permissions(Set.of(RoleDTO.USER))
+            .build();
+    }
+
+    @Test
+    void createHappyPath() {
+
+        final PersonDTO personDTO = anyPersonDTO();
+        final Person createdPerson = anyPerson();
+
+        when(personService.create(any())).thenReturn(createdPerson);
+
+        final PersonDTO createdDTO = sut.create(personDTO);
+
+        assertThat(createdDTO).isNotNull();
+        assertThat(createdDTO.getId()).isOne();
+        assertThat(createdDTO.getUsername()).isEqualTo("muster");
+        assertThat(createdDTO.getLastName()).isEqualTo("Muster");
+        assertThat(createdDTO.getFirstName()).isEqualTo("Marlene");
+        assertThat(createdDTO.getEmail()).isEqualTo("muster@example.org");
+        assertThat(createdDTO.getPermissions()).containsOnly(RoleDTO.USER);
+        assertThat(createdDTO.isEnabled()).isTrue();
+
+        ArgumentCaptor<Person> personArgumentCaptor = ArgumentCaptor.forClass(Person.class);
+        verify(personService).create(personArgumentCaptor.capture());
+
+        final Person value = personArgumentCaptor.getValue();
+        assertThat(value).isNotNull();
+        assertThat(value.getId()).isNull();
+        assertThat(value.getUsername()).isEqualTo("muster");
+    }
+
+    @Test
+    void updateHappyPath() {
+        final PersonDTO personDTO = anyPersonDTO(1);
+        final Person createdPerson = anyPerson();
+
+        when(personService.update(any())).thenReturn(createdPerson);
+
+        final PersonDTO updatedPersonDTO = sut.update(personDTO);
+
+        assertThat(updatedPersonDTO).isNotNull();
+        assertThat(updatedPersonDTO.getId()).isOne();
+        assertThat(updatedPersonDTO.getUsername()).isEqualTo("muster");
+        assertThat(updatedPersonDTO.getLastName()).isEqualTo("Muster");
+        assertThat(updatedPersonDTO.getFirstName()).isEqualTo("Marlene");
+        assertThat(updatedPersonDTO.getEmail()).isEqualTo("muster@example.org");
+        assertThat(updatedPersonDTO.getPermissions()).containsOnly(RoleDTO.USER);
+        assertThat(updatedPersonDTO.isEnabled()).isTrue();
+
+
+        ArgumentCaptor<Person> personArgumentCaptor = ArgumentCaptor.forClass(Person.class);
+        verify(personService).update(personArgumentCaptor.capture());
+
+        final Person value = personArgumentCaptor.getValue();
+        assertThat(value).isNotNull();
+        assertThat(value.getId()).isOne();
+        assertThat(value.getUsername()).isEqualTo("muster");
+    }
+
+    @Test
+    void deleteHappyPath() {
+        final Person signedInUserId = new Person("boss", "Scherer", "Theresa", "boss@example.org");
+        when(personService.getPersonByID(any())).thenReturn(Optional.of(signedInUserId));
+
+        sut.delete(anyPersonDTO(1), 42);
+
+        ArgumentCaptor<Person> personArgumentCaptor = ArgumentCaptor.forClass(Person.class);
+
+        verify(personService).getPersonByID(42);
+
+        verify(personService).delete(personArgumentCaptor.capture(), eq(signedInUserId));
+
+        final Person personToDelete = personArgumentCaptor.getValue();
+        assertThat(personToDelete).isNotNull();
+        assertThat(personToDelete.getId()).isOne();
+        assertThat(personToDelete.getUsername()).isEqualTo("muster");
+    }
+
+    @Test
+    void ensureDeleteHandlesSignedInUserUnknown() {
+
+        sut.delete(anyPersonDTO(), 42);
+
+        verify(personService).getPersonByID(42);
+        verifyNoMoreInteractions(personService);
+    }
+
+    @Test
+    void getPersonByIdHappyPath() {
+
+        final PersonDTO personDTO = anyPersonDTO(1);
+        when(personService.getPersonByID(any())).thenReturn(Optional.of(anyPerson()));
+
+        Optional<PersonDTO> personById = sut.getPersonById(personDTO.getId());
+
+        verify(personService).getPersonByID(personDTO.getId());
+        assertThat(personById).isPresent()
+            .contains(personDTO);
+    }
+
+    @Test
+    void ensureGetPersonByIdHandlesNotFound() {
+
+        Optional<PersonDTO> personById = sut.getPersonById(1);
+
+        verify(personService).getPersonByID(1);
+        assertThat(personById).isEmpty();
+    }
+
+    @Test
+    void getPersonByUsernameHappyPath() {
+
+        final PersonDTO personDTO = anyPersonDTO(1);
+        when(personService.getPersonByUsername(any())).thenReturn(Optional.of(anyPerson()));
+
+        Optional<PersonDTO> personById = sut.getPersonByUsername(personDTO.getUsername());
+
+        verify(personService).getPersonByUsername(personDTO.getUsername());
+        assertThat(personById).isPresent()
+            .contains(personDTO);
+    }
+
+    @Test
+    void ensureGetPersonByUsernameHandlesNotFound() {
+
+        Optional<PersonDTO> personById = sut.getPersonByUsername("muster");
+
+        verify(personService).getPersonByUsername("muster");
+        assertThat(personById).isEmpty();
+    }
+
+    @Test
+    void getPersonByMailAddressHappyPath() {
+
+        final PersonDTO personDTO = anyPersonDTO(1);
+        when(personService.getPersonByMailAddress(any())).thenReturn(Optional.of(anyPerson()));
+
+        Optional<PersonDTO> personById = sut.getPersonByMailAddress(personDTO.getEmail());
+
+        verify(personService).getPersonByMailAddress(personDTO.getEmail());
+        assertThat(personById).isPresent()
+            .contains(personDTO);
+    }
+
+    @Test
+    void ensureGetPersonByMailAddressHandlesNotFound() {
+
+        Optional<PersonDTO> personById = sut.getPersonByUsername("muster");
+
+        verify(personService).getPersonByUsername("muster");
+        assertThat(personById).isEmpty();
+    }
+
+    @Test
+    void getActivePersonsHappyPath() {
+
+        PersonDTO anyPersonDTO = anyPersonDTO(1);
+        Person anyPerson = anyPerson();
+
+        Page<Person> page = new PageImpl<>(List.of(anyPerson));
+        when(personService.getActivePersons(any())).thenReturn(page);
+
+        Stream<PersonDTO> result = sut.getActivePersons();
+
+        ArgumentCaptor<PageableSearchQuery> argumentCaptor = ArgumentCaptor.forClass(PageableSearchQuery.class);
+
+        verify(personService).getActivePersons(argumentCaptor.capture());
+
+        PageableSearchQuery pageableSearchQueryCaptor = argumentCaptor.getValue();
+        assertThat(pageableSearchQueryCaptor).isNotNull();
+        assertThat(pageableSearchQueryCaptor.getQuery()).isEmpty();
+        assertThat(pageableSearchQueryCaptor.getPageable().getSort()).isEqualTo(Sort.unsorted());
+        assertThat(pageableSearchQueryCaptor.getPageable().getPageNumber()).isZero();
+        assertThat(pageableSearchQueryCaptor.getPageable().getPageSize()).isEqualTo(25);
+
+        assertThat(result).containsOnly(anyPersonDTO);
+    }
+
+    @Test
+    void ensureGetActivePersonsHandlesEmptyPage() {
+
+        when(personService.getActivePersons(any())).thenReturn(Page.empty());
+
+        Stream<PersonDTO> result = sut.getActivePersons();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getInactivePersonsHappyPath() {
+
+        PersonDTO anyPersonDTO = anyPersonDTO(1);
+        Person anyPerson = anyPerson();
+
+        Page<Person> page = new PageImpl<>(List.of(anyPerson));
+        when(personService.getInactivePersons(any())).thenReturn(page);
+
+        Stream<PersonDTO> result = sut.getInactivePersons();
+
+
+        ArgumentCaptor<PageableSearchQuery> argumentCaptor = ArgumentCaptor.forClass(PageableSearchQuery.class);
+
+        verify(personService).getInactivePersons(argumentCaptor.capture());
+
+        PageableSearchQuery pageableSearchQueryCaptor = argumentCaptor.getValue();
+        assertThat(pageableSearchQueryCaptor).isNotNull();
+        assertThat(pageableSearchQueryCaptor.getQuery()).isEmpty();
+        assertThat(pageableSearchQueryCaptor.getPageable().getSort()).isEqualTo(Sort.unsorted());
+        assertThat(pageableSearchQueryCaptor.getPageable().getPageNumber()).isZero();
+        assertThat(pageableSearchQueryCaptor.getPageable().getPageSize()).isEqualTo(25);
+
+        assertThat(result).containsOnly(anyPersonDTO);
+    }
+
+    @Test
+    void ensureGetInactivePersonsHandlesEmptyPage() {
+
+        when(personService.getInactivePersons(any())).thenReturn(Page.empty());
+
+        Stream<PersonDTO> result = sut.getInactivePersons();
+
+        assertThat(result).isEmpty();
+    }
+
+
+    @Test
+    void getSignedInUserHappyPath() {
+        when(personService.getSignedInUser()).thenReturn(anyPerson());
+
+        final PersonDTO signedInUser = sut.getSignedInUser();
+
+        verify(personService).getSignedInUser();
+        assertThat(signedInUser).isNotNull();
+    }
+
+    @Test
+    void appointAsInitialUserIfNoInitialUserPresentHappyPath() {
+
+        final PersonDTO personDTO = anyPersonDTO(1);
+        final Person person = anyPerson();
+        when(personService.appointAsOfficeUserIfNoOfficeUserPresent(any())).thenReturn(person);
+
+        final PersonDTO appointedPerson = sut.appointAsInitialUserIfNoInitialUserPresent(personDTO);
+
+        assertThat(appointedPerson).isNotNull();
+        assertThat(appointedPerson.getId()).isOne();
+        assertThat(appointedPerson.getUsername()).isEqualTo("muster");
+        assertThat(appointedPerson.getLastName()).isEqualTo("Muster");
+        assertThat(appointedPerson.getFirstName()).isEqualTo("Marlene");
+        assertThat(appointedPerson.getEmail()).isEqualTo("muster@example.org");
+        assertThat(appointedPerson.getPermissions()).containsOnly(RoleDTO.USER);
+        assertThat(appointedPerson.isEnabled()).isTrue();
+
+        ArgumentCaptor<Person> personArgumentCaptor = ArgumentCaptor.forClass(Person.class);
+        verify(personService).appointAsOfficeUserIfNoOfficeUserPresent(personArgumentCaptor.capture());
+
+        final Person personToBeAppointed = personArgumentCaptor.getValue();
+        assertThat(personToBeAppointed).isNotNull();
+        assertThat(personToBeAppointed.getId()).isOne();
+        assertThat(personToBeAppointed.getUsername()).isEqualTo("muster");
+    }
+}
