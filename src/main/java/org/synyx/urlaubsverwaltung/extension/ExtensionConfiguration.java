fd870e6f6a245d95028de2a50e62a79c3d08d957
Johannes Graf <graf.johannes@gmail.com>
2023-01-21 23:05:07 +0100

    write log message that extensions are enabled

--- src/main/java/org/synyx/urlaubsverwaltung/extension/ExtensionConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/extension/ExtensionConfiguration.java
@@ -1,11 +1,24 @@
 package org.synyx.urlaubsverwaltung.extension;
 
+import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
 @ConditionalOnProperty(value = "uv.extensions.enabled", havingValue = "true")
 @EnableConfigurationProperties(ExtensionConfigurationProperties.class)
 public class ExtensionConfiguration {
+
+    private static final Logger LOG = getLogger(lookup().lookupClass());
+
+    @PostConstruct
+    public void logThatExtensionsAreEnabled() {
+        LOG.info(">> Extension mode is enabled (uv.extensions.enabled=true)");
+    }
 }
