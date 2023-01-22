8bd6a0232dbe589054f51dffb2703784d59c1472
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-01-22 15:44:44 +0100

    fix pageable configuration
    `SpringDataWebAutoConfiguration` does not have an impact anymore due to some conditionals.

--- src/main/java/org/synyx/urlaubsverwaltung/web/pagination/PaginationConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/web/pagination/PaginationConfiguration.java
@@ -2,24 +2,34 @@ package org.synyx.urlaubsverwaltung.web.pagination;
 
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.SortHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
+import org.springframework.data.web.config.SortHandlerMethodArgumentResolverCustomizer;
 import org.springframework.data.web.config.SpringDataWebConfiguration;
 import org.synyx.urlaubsverwaltung.person.PersonService;
 import org.synyx.urlaubsverwaltung.user.pagination.UserPaginationSettingsSupplier;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(SpringDataWebProperties.class)
 public class PaginationConfiguration extends SpringDataWebConfiguration {
 
+    private final SpringDataWebProperties properties;
+
     public PaginationConfiguration(ApplicationContext context,
-                                   @Qualifier("mvcConversionService") ObjectFactory<ConversionService> conversionService) {
+                                   @Qualifier("mvcConversionService") ObjectFactory<ConversionService> conversionService,
+                                   SpringDataWebProperties properties) {
 
         super(context, conversionService);
+        this.properties = properties;
     }
 
     @Bean
@@ -34,5 +44,24 @@ public class PaginationConfiguration extends SpringDataWebConfiguration {
         customizePageableResolver(pageableResolver);
 
         return pageableResolver;
+    }
+
+    @Bean
+    public PageableHandlerMethodArgumentResolverCustomizer pageableCustomizer() {
+        return (resolver) -> {
+            SpringDataWebProperties.Pageable pageable = this.properties.getPageable();
+            resolver.setPageParameterName(pageable.getPageParameter());
+            resolver.setSizeParameterName(pageable.getSizeParameter());
+            resolver.setOneIndexedParameters(pageable.isOneIndexedParameters());
+            resolver.setPrefix(pageable.getPrefix());
+            resolver.setQualifierDelimiter(pageable.getQualifierDelimiter());
+            resolver.setFallbackPageable(PageRequest.of(0, pageable.getDefaultPageSize()));
+            resolver.setMaxPageSize(pageable.getMaxPageSize());
+        };
+    }
+
+    @Bean
+    public SortHandlerMethodArgumentResolverCustomizer sortCustomizer() {
+        return (resolver) -> resolver.setSortParameter(this.properties.getSort().getSortParameter());
     }
 }
