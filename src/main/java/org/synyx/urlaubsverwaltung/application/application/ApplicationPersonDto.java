bf901c32abd8a83034219b1360f962d4bd180aa1
Klaus Zipfel <zipfel@iqo.uni-hannover.de>
2023-01-25 10:46:34 +0100

    Added inactive-account indicator to locked accounts at various locations

--- src/main/java/org/synyx/urlaubsverwaltung/application/application/ApplicationPersonDto.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/application/ApplicationPersonDto.java
@@ -4,13 +4,15 @@ public class ApplicationPersonDto {
 
     private final String name;
     private final String avatarUrl;
+    private final Boolean isInactive;
 
     private final Integer id;
 
-    ApplicationPersonDto(String name, String avatarUrl, Integer id) {
+    ApplicationPersonDto(String name, String avatarUrl, Boolean isInactive, Integer id) {
         this.name = name;
         this.avatarUrl = avatarUrl;
         this.id = id;
+        this.isInactive = isInactive;
     }
 
     public String getName() {
@@ -19,6 +21,10 @@ public class ApplicationPersonDto {
 
     public String getAvatarUrl() {
         return avatarUrl;
+    }
+
+    public Boolean getIsInactive() {
+        return isInactive;
     }
 
     public Integer getId() {
