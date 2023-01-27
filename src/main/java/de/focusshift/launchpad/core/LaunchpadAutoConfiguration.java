a03651e5223cde0bcb53e4df0d07e34a0a6d4737
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2023-01-27 21:53:36 +0100

    Fix static name convention

--- src/main/java/de/focusshift/launchpad/core/LaunchpadAutoConfiguration.java
+++ src/main/java/de/focusshift/launchpad/core/LaunchpadAutoConfiguration.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties(LaunchpadConfigProperties.class)
-@ConditionalOnProperty(prefix="launchpad", name = "name-default-locale")
+@ConditionalOnProperty(prefix = "launchpad", name = "name-default-locale")
 public class LaunchpadAutoConfiguration {
 
     @Conditional(LaunchpadAppsCondition.class)
@@ -44,24 +44,22 @@ public class LaunchpadAutoConfiguration {
         }
     }
 
-
     /**
      * Checks if 'launchpad.apps' are configured appropriate.
      */
     // copied and adjusted from org.springframework.boot.autoconfigure.condition.OnPropertyListCondition
     static class LaunchpadAppsCondition extends SpringBootCondition {
         private static final Bindable<List<LaunchpadConfigProperties.App>> APP_LIST = Bindable.listOf(LaunchpadConfigProperties.App.class);
-
-        private static final String propertyName = "launchpad.apps";
+        private static final String PROPERTY_NAME = "launchpad.apps";
 
         @Override
         public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            final BindResult<?> property = Binder.get(context.getEnvironment()).bind(propertyName, APP_LIST);
+            final BindResult<?> property = Binder.get(context.getEnvironment()).bind(PROPERTY_NAME, APP_LIST);
             final ConditionMessage.Builder messageBuilder = ConditionMessage.forCondition("apps");
             if (property.isBound()) {
-                return ConditionOutcome.match(messageBuilder.found("property").items(propertyName));
+                return ConditionOutcome.match(messageBuilder.found("property").items(PROPERTY_NAME));
             }
-            return ConditionOutcome.noMatch(messageBuilder.didNotFind("property").items(propertyName));
+            return ConditionOutcome.noMatch(messageBuilder.didNotFind("property").items(PROPERTY_NAME));
         }
     }
 }
