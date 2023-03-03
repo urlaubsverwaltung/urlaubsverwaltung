41f621f7b125aff7e8d9aa0ef654c8a4a77f60b0
honnel <github@honnel.de>
2023-03-03 22:02:10 +0100

    Add oidc login_hint parameter aware OAuth2AuthorizationRequestResolver
    to pass login_hint from application to idp
    
    its needed to pass the login_hint originated by application because
    nonce and state need to be right in place during redirect to application
    
    see:
    * spring-projects/spring-security#5521
    *
    https://docs.spring.io/spring-security/reference/servlet/oauth2/client/authorization-grants.html#_customizing_the_authorization_request
    *
    https://github.com/spring-projects/spring-security/blob/779597af2a6ed777707f08ae8106818e0b8e299e/oauth2/oauth2-client/src/test/java/org/springframework/security/oauth2/client/web/OAuth2AuthorizationRequestRedirectFilterTests.java#L337

---
+++ src/main/java/org/synyx/urlaubsverwaltung/security/LoginHintAwareResolver.java
@@ -0,0 +1,49 @@
+package org.synyx.urlaubsverwaltung.security;
+
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
+
+public class LoginHintAwareResolver implements OAuth2AuthorizationRequestResolver {
+
+    private static final String OIDC_PARAMETER_LOGIN_HINT = "login_hint";
+
+    private final DefaultOAuth2AuthorizationRequestResolver defaultResolver;
+
+    LoginHintAwareResolver(ClientRegistrationRepository clientRegistrationRepository) {
+        this.defaultResolver = new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository,
+            DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request) {
+        return addLoginHintIfPresent(request, defaultResolver.resolve(request));
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request, String clientRegistrationId) {
+        return addLoginHintIfPresent(request, defaultResolver.resolve(request, clientRegistrationId));
+    }
+
+    private OAuth2AuthorizationRequest addLoginHintIfPresent(HttpServletRequest request, OAuth2AuthorizationRequest defaultAuthorizationRequest) {
+        final String loginHint = request.getParameter(OIDC_PARAMETER_LOGIN_HINT);
+
+        if (loginHint == null || loginHint.isEmpty()) {
+            return defaultAuthorizationRequest;
+        } else {
+            final Map<String, Object> additionalParameters = new HashMap<>(defaultAuthorizationRequest.getAdditionalParameters());
+            additionalParameters.put(OIDC_PARAMETER_LOGIN_HINT, loginHint);
+
+            return OAuth2AuthorizationRequest.from(defaultAuthorizationRequest)
+                .additionalParameters(additionalParameters)
+                .build();
+        }
+    }
+}
---
+++ src/test/java/org/synyx/urlaubsverwaltung/security/LoginHintAwareResolverTest.java
@@ -0,0 +1,112 @@
+package org.synyx.urlaubsverwaltung.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
+import static org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE;
+import static org.springframework.security.oauth2.core.ClientAuthenticationMethod.CLIENT_SECRET_BASIC;
+
+class LoginHintAwareResolverTest {
+
+    private ClientRegistration clientRegistration;
+    private InMemoryClientRegistrationRepository clientRegistrationRepository;
+
+    @BeforeEach
+    void setUp() {
+        this.clientRegistration = ClientRegistration.withRegistrationId("registration")
+            .clientId("client")
+            .clientSecret("secret")
+            .clientAuthenticationMethod(CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AUTHORIZATION_CODE)
+            .scope("user")
+            .authorizationUri("https://provider.com/oauth2/authorize")
+            .tokenUri("https://provider.com/oauth2/token")
+            .userInfoUri("https://provider.com/oauth2/user")
+            .redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId}")
+            .userNameAttributeName("id")
+            .clientName("client")
+            .build();
+
+        this.clientRegistrationRepository = new InMemoryClientRegistrationRepository(this.clientRegistration);
+    }
+
+    @Test
+    void authorizationRequestRedirectWithLoginHint() throws ServletException, IOException {
+
+        final String requestUri = DEFAULT_AUTHORIZATION_REQUEST_BASE_URI + "/" + this.clientRegistration.getRegistrationId();
+        final MockHttpServletRequest request = new MockHttpServletRequest("GET", requestUri);
+        request.setServletPath(requestUri);
+
+        final String loginHintParamName = "login_hint";
+        request.addParameter(loginHintParamName, "office");
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        final FilterChain filterChain = mock(FilterChain.class);
+
+        final OAuth2AuthorizationRequestResolver sut = new LoginHintAwareResolver(this.clientRegistrationRepository);
+        final OAuth2AuthorizationRequestRedirectFilter filter = new OAuth2AuthorizationRequestRedirectFilter(sut);
+
+        filter.doFilter(request, response, filterChain);
+
+        assertThat(response.getRedirectedUrl())
+            .matches("https://provider.com/oauth2/authorize\\?response_type=code&client_id=client&scope=user&state=.{15,}" +
+                "&redirect_uri=http://localhost/login/oauth2/code/registration&login_hint=office");
+    }
+
+    @Test
+    void authorizationRequestRedirectWithoutLoginHint() throws ServletException, IOException {
+
+        final String requestUri = DEFAULT_AUTHORIZATION_REQUEST_BASE_URI + "/" + this.clientRegistration.getRegistrationId();
+        final MockHttpServletRequest request = new MockHttpServletRequest("GET", requestUri);
+        request.setServletPath(requestUri);
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        FilterChain filterChain = mock(FilterChain.class);
+
+        final OAuth2AuthorizationRequestResolver sut = new LoginHintAwareResolver(this.clientRegistrationRepository);
+        final OAuth2AuthorizationRequestRedirectFilter filter = new OAuth2AuthorizationRequestRedirectFilter(sut);
+
+        filter.doFilter(request, response, filterChain);
+
+        assertThat(response.getRedirectedUrl())
+            .matches("https://provider.com/oauth2/authorize\\?response_type=code" +
+                "&client_id=client&scope=user&state=.{15,}&redirect_uri=http://localhost/login/oauth2/code/registration");
+    }
+
+    @Test
+    void authorizationRequestRedirectWithUnknownParameter() throws ServletException, IOException {
+
+        final String requestUri = DEFAULT_AUTHORIZATION_REQUEST_BASE_URI + "/" + this.clientRegistration.getRegistrationId();
+        final MockHttpServletRequest request = new MockHttpServletRequest("GET", requestUri);
+        request.setServletPath(requestUri);
+
+        final String loginHintParamName = "some_parameter";
+        request.addParameter(loginHintParamName, "foobar");
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        final FilterChain filterChain = mock(FilterChain.class);
+
+        final OAuth2AuthorizationRequestResolver sut = new LoginHintAwareResolver(this.clientRegistrationRepository);
+        final OAuth2AuthorizationRequestRedirectFilter filter = new OAuth2AuthorizationRequestRedirectFilter(sut);
+
+        filter.doFilter(request, response, filterChain);
+
+        assertThat(response.getRedirectedUrl())
+            .matches("https://provider.com/oauth2/authorize\\?response_type=code&client_id=client&scope=user" +
+                "&state=.{15,}&redirect_uri=http://localhost/login/oauth2/code/registration");
+    }
+}
+
