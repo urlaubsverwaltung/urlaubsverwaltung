861e631a84bb9479e500e0e160faf5abcf9fbc32
honnel <github@honnel.de>
2023-03-22 08:12:56 +0100

    Use email for prevention of regeneration of demo data
    Using oidc as security provider will cause uuids as username instead of
    internally generated usernames. Therefore we use the same fallback as
    for security context with users email adress.
    
    fixes #3725

--- src/main/java/org/synyx/urlaubsverwaltung/dev/DemoUser.java
+++ src/main/java/org/synyx/urlaubsverwaltung/dev/DemoUser.java
@@ -7,23 +7,30 @@ import org.synyx.urlaubsverwaltung.person.Role;
  */
 enum DemoUser {
 
-    USER("user", Role.USER),
-    DEPARTMENT_HEAD("departmentHead", Role.USER, Role.DEPARTMENT_HEAD),
-    SECOND_STAGE_AUTHORITY("secondStageAuthority", Role.USER, Role.SECOND_STAGE_AUTHORITY),
-    BOSS("boss", Role.USER, Role.BOSS),
-    OFFICE("office", Role.USER, Role.OFFICE),
-    ADMIN("admin", Role.USER, Role.ADMIN);
+    USER("user", "user@urlaubsverwaltung.cloud", Role.USER),
+    DEPARTMENT_HEAD("departmentHead", "departmentHead@urlaubsverwaltung.cloud", Role.USER, Role.DEPARTMENT_HEAD),
+    SECOND_STAGE_AUTHORITY("secondStageAuthority", "secondStageAuthority@urlaubsverwaltung.cloud", Role.USER, Role.SECOND_STAGE_AUTHORITY),
+    BOSS("boss", "boss@urlaubsverwaltung.cloud", Role.USER, Role.BOSS),
+    OFFICE("office", "office@urlaubsverwaltung.cloud", Role.USER, Role.OFFICE),
+    ADMIN("admin", "admin@urlaubsverwaltung.cloud", Role.USER, Role.ADMIN);
 
     private final String username;
+
+    private final String email;
     private final Role[] roles;
 
-    DemoUser(String username, Role... roles) {
+    DemoUser(String username, String email, Role... roles) {
         this.username = username;
+        this.email = email;
         this.roles = roles;
     }
 
     String getUsername() {
         return username;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     Role[] getRoles() {
