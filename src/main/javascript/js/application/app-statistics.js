f3eb2906a3d10f29b2073db619b849b3a95274ff
Benjamin Seber <benjamin.seber@mailbox.org>
2023-04-28 16:53:27 +0200

    enable date range submit button after history.back

--- src/main/javascript/js/application/app-statistics.js
+++ src/main/javascript/js/application/app-statistics.js
@@ -1,10 +1,16 @@
 import { createDatepicker } from "../../components/datepicker";
+import { onTurboBeforeRenderRestore } from "../turbo";
 
 const getPersonId = () => {};
 const urlPrefix = ""; // not required, no absences are fetched since we have no single person (personId)
 
 createDatepicker("#from-date-input", { urlPrefix, getPersonId });
 createDatepicker("#to-date-input", { urlPrefix, getPersonId });
+
+onTurboBeforeRenderRestore(function (event) {
+  // enable submit buttons which has been disabled by 'hotwire/turbo' after submitting the form.
+  event.detail.newBody.querySelector("form#form-date-from-to button[type='submit']").removeAttribute("disabled");
+});
 
 // close `<details-dropdown>` on advancing client side navigation.
 // (it's visible as long as response is loading)
