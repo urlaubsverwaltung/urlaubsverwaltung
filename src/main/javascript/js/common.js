a2c974576d446afa6f4224b0af2c4d85e77cd8aa
Benjamin Seber <benjamin.seber@mailbox.org>
2023-08-21 15:29:02 +0200

    remove table-selectable js
    navigate on click is handled by <tr onclick>

--- src/main/javascript/js/common.js
+++ src/main/javascript/js/common.js
@@ -2,7 +2,6 @@ import $ from "jquery";
 import "../lib/bootstrap";
 import "../components/avatar";
 import "../components/navigation";
-import "../components/table-selectable";
 import "../components/textarea";
 import "../components/feedback";
 import tooltip from "../components/tooltip";
