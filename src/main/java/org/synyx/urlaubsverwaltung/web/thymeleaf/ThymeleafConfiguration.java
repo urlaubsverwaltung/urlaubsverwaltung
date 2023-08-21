8fd79ac6ae52ad646c1c15765b49af6bc1a87a72
Benjamin Seber <benjamin.seber@mailbox.org>
2023-08-21 13:07:29 +0200

    add conditionalOnClass *thymeleaf to ThymeleafConfiguration

--- src/main/java/org/synyx/urlaubsverwaltung/web/thymeleaf/ThymeleafConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/web/thymeleaf/ThymeleafConfiguration.java
@@ -1,14 +1,18 @@
 package org.synyx.urlaubsverwaltung.web.thymeleaf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
+@ConditionalOnClass({TemplateMode.class, SpringTemplateEngine.class})
 class ThymeleafConfiguration implements WebMvcConfigurer {
 
     private MessageSource messageSource;
