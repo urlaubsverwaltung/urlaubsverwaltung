7d33ee853f3ebe5ca62b8fc4def489993772c04b
Benjamin Seber <benjamin.seber@mailbox.org>
2023-10-04 22:14:56 +0200

    remove datalist-polyfill
    - saves 3,4 kb gziped!
    - datalist is supported by all major browsers

--- src/main/javascript/js/common.js
+++ src/main/javascript/js/common.js
@@ -5,7 +5,6 @@ import "../components/textarea";
 import "../components/feedback";
 import tooltip from "../components/tooltip";
 import "@ungap/custom-elements";
-import "datalist-polyfill";
 import "./date-fns-localized";
 
 tooltip();
