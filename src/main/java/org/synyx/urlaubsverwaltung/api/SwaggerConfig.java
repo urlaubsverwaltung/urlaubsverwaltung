4e0aa478a7bb62526ce01a977f216eb6d1e4b755
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2023-10-17 22:00:31 +0200

    Add bearer authentication to swagger config

--- src/main/java/org/synyx/urlaubsverwaltung/api/SwaggerConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/api/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package org.synyx.urlaubsverwaltung.api;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.properties.SpringDocConfigProperties;
 import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
@@ -17,8 +20,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
+
 @Configuration
 public class SwaggerConfig {
+
+    private static final String BEARER_AUTH = "bearerAuth";
 
     @Value(value = "${info.app.version}")
     private String version;
@@ -43,6 +50,16 @@ public class SwaggerConfig {
                             .name("Apache 2.0")
                             .url("https://github.com/urlaubsverwaltung/urlaubsverwaltung/blob/main/LICENSE.txt")
                     )
+            )
+            .addSecurityItem(new SecurityRequirement()
+                .addList(BEARER_AUTH)
+            )
+            .components(new Components()
+                .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                    .name(BEARER_AUTH)
+                    .type(HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"))
             );
 
     }
