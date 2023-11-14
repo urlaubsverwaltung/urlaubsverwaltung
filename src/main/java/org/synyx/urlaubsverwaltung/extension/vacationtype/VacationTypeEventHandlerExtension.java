4550c0f652209ad512662e44fdac3ac51de1f9ed
Benjamin Seber <benjamin.seber@mailbox.org>
2023-11-14 13:08:52 +0100

    add extension VacationType

---
+++ src/main/java/org/synyx/urlaubsverwaltung/extension/vacationtype/VacationTypeEventHandlerExtension.java
@@ -0,0 +1,82 @@
+package org.synyx.urlaubsverwaltung.extension.vacationtype;
+
+import de.focus_shift.urlaubsverwaltung.extension.api.tenancy.TenantSupplier;
+import de.focus_shift.urlaubsverwaltung.extension.api.vacationtype.VacationTypeUpdatedEventDTO;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.synyx.urlaubsverwaltung.application.vacationtype.CustomVacationType;
+import org.synyx.urlaubsverwaltung.application.vacationtype.ProvidedVacationType;
+import org.synyx.urlaubsverwaltung.application.vacationtype.VacationType;
+import org.synyx.urlaubsverwaltung.application.vacationtype.VacationTypeLabel;
+import org.synyx.urlaubsverwaltung.application.vacationtype.VacationTypeUpdatedEvent;
+import org.synyx.urlaubsverwaltung.settings.SupportedLanguages;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+@Component
+@ConditionalOnProperty(value = "uv.extensions.enabled", havingValue = "true")
+class VacationTypeEventHandlerExtension {
+
+    private final TenantSupplier tenantSupplier;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+
+    VacationTypeEventHandlerExtension(TenantSupplier tenantSupplier,
+                                      ApplicationEventPublisher applicationEventPublisher) {
+        this.tenantSupplier = tenantSupplier;
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @EventListener
+    @Async
+    void on(VacationTypeUpdatedEvent event) {
+        final String tenant = tenantSupplier.get();
+        final VacationTypeUpdatedEventDTO updatedEventDTO = toVacationTypeDTO(tenant, event.updatedVacationType());
+        applicationEventPublisher.publishEvent(updatedEventDTO);
+    }
+
+    private VacationTypeUpdatedEventDTO toVacationTypeDTO(String tenantId, VacationType<?> vacationType) {
+        return VacationTypeUpdatedEventDTO.builder()
+            .id(UUID.randomUUID())
+            .tenantId(tenantId)
+            .sourceId(vacationType.getId())
+            .category(vacationType.getCategory().name())
+            .requiresApprovalToApply(vacationType.isRequiresApprovalToApply())
+            .requiresApprovalToCancel(vacationType.isRequiresApprovalToCancel())
+            .color(vacationType.getColor().name())
+            .visibleToEveryone(vacationType.isVisibleToEveryone())
+            .label(toLabels(vacationType))
+            .build();
+    }
+
+    private Map<Locale, String> toLabels(VacationType<?> vacationType) {
+        if (vacationType instanceof ProvidedVacationType providedVacationType) {
+            return toLabels(providedVacationType);
+        } else if (vacationType instanceof CustomVacationType customVacationType) {
+            return toLabels(customVacationType);
+        } else {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    private Map<Locale, String> toLabels(ProvidedVacationType providedVacationType) {
+        return Arrays.stream(SupportedLanguages.values())
+            .map(SupportedLanguages::getLocale)
+            .collect(toMap(identity(), providedVacationType::getLabel));
+    }
+
+    private Map<Locale, String> toLabels(CustomVacationType customVacationType) {
+        return customVacationType.labelsByLocale().values()
+            .stream()
+            .collect(toMap(VacationTypeLabel::locale, VacationTypeLabel::label));
+    }
+}
---
+++ src/main/java/org/synyx/urlaubsverwaltung/extension/vacationtype/VacationTypeEventRepublisher.java
@@ -0,0 +1,51 @@
+package org.synyx.urlaubsverwaltung.extension.vacationtype;
+
+import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.synyx.urlaubsverwaltung.application.vacationtype.VacationType;
+import org.synyx.urlaubsverwaltung.application.vacationtype.VacationTypeService;
+import org.synyx.urlaubsverwaltung.application.vacationtype.VacationTypeUpdatedEvent;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Component
+@ConditionalOnProperty(value = "uv.extensions.vacationtype.republish.enabled", havingValue = "true")
+@ConditionalOnBean(VacationTypeEventHandlerExtension.class)
+class VacationTypeEventRepublisher {
+
+    private static final Logger LOG = getLogger(lookup().lookupClass());
+
+    private final VacationTypeService vacationTypeService;
+    private final VacationTypeEventHandlerExtension vacationTypeEventHandlerExtension;
+
+    VacationTypeEventRepublisher(VacationTypeService vacationTypeService,
+                                 VacationTypeEventHandlerExtension vacationTypeEventHandlerExtension) {
+        this.vacationTypeService = vacationTypeService;
+        this.vacationTypeEventHandlerExtension = vacationTypeEventHandlerExtension;
+    }
+
+    @Async
+    @EventListener(ApplicationStartedEvent.class)
+    void republishEvents() {
+
+        LOG.info("Republishing all events with type=VacationTypeUpdatedEvent");
+
+        vacationTypeService.getAllVacationTypes()
+            .stream()
+            .map(VacationTypeUpdatedEvent::of)
+            .forEach(event -> {
+                VacationType<?> updatedVacationType = event.updatedVacationType();
+                LOG.info("Publishing vacationTypeUpdatedEvent with id={}, vacationTypeId={} for category={} with active={} and requiresApprovalToApply={}",
+                    event.id(), updatedVacationType.getId(), updatedVacationType.getCategory(), updatedVacationType.isActive(), updatedVacationType.isRequiresApprovalToApply());
+                vacationTypeEventHandlerExtension.on(event);
+            });
+
+        LOG.info("Republished all events with type=VacationTypeUpdatedEvent");
+    }
+}
