8150ab2a6682ade4f9b9e0d9758dda9262e5c796
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2023-11-15 09:56:10 +0100

    Remove print

--- src/test/java/org/synyx/urlaubsverwaltung/department/api/DepartmentApiControllerTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/department/api/DepartmentApiControllerTest.java
@@ -20,7 +20,6 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,7 +58,6 @@ class DepartmentApiControllerTest {
 
         perform(get("/api/departments"))
                 .andExpect(status().isOk())
-                .andDo(print())
                 .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(jsonPath("$.departments").exists())
                 .andExpect(jsonPath("$.departments", hasSize(2)))
