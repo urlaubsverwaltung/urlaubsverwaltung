d4ed1329c0b96eb7aab17ad5551e4129785d16ce
Benjamin Seber <benjamin.seber@mailbox.org>
2024-07-19 13:07:36 +0200

    split SickNoteExtend from SickNoteViewController

--- src/main/java/org/synyx/urlaubsverwaltung/sicknote/sicknote/SickNoteViewController.java
+++ src/main/java/org/synyx/urlaubsverwaltung/sicknote/sicknote/SickNoteViewController.java
@@ -50,7 +50,6 @@ import static java.util.Comparator.comparing;
 import static java.util.Objects.requireNonNullElse;
 import static java.util.stream.Collectors.toList;
 import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.util.StringUtils.hasText;
 import static org.synyx.urlaubsverwaltung.application.application.ApplicationStatus.ALLOWED;
 import static org.synyx.urlaubsverwaltung.application.vacationtype.VacationCategory.OVERTIME;
 import static org.synyx.urlaubsverwaltung.person.Role.BOSS;
@@ -201,125 +200,6 @@ class SickNoteViewController implements HasLaunchpad {
         addVacationTypeColorsToModel(model);
 
         return "sicknote/sick_note_form";
-    }
-
-    @GetMapping("/sicknote/extend")
-    public String getExtendSickNote(Model model) {
-
-        final Person signedInUser = personService.getSignedInUser();
-        final Optional<SickNote> maybeSickNote = getSickNoteOfYesterdayOrLastWorkDay(signedInUser);
-        if (maybeSickNote.isEmpty()) {
-            return "sicknote/sick_note_extended_not_found";
-        }
-
-        final SickNote sickNote = maybeSickNote.get();
-
-        // TODO use correct workingdays value
-        final SickNoteExtendDto sickNoteDto = new SickNoteExtendDto(sickNote.getId(), sickNote.getStartDate(), sickNote.getEndDate(), 42, sickNote.isAubPresent());
-
-        model.addAttribute("sickNote", sickNoteDto);
-        model.addAttribute("sickNotePersonId", signedInUser.getId());
-        model.addAttribute("today", LocalDate.now(clock));
-        model.addAttribute("extendToDate", LocalDate.now(clock));
-        // TODO model attributes
-        model.addAttribute("sickNoteEndDateWord", "heute");
-        model.addAttribute("extensionDatePlusOne", LocalDate.now());
-        model.addAttribute("extensionDatePlusTwo", LocalDate.now().plusDays(1));
-        model.addAttribute("extensionDateEndOfWeek", LocalDate.now().plusDays(4));
-        model.addAttribute("plusOneWorkdayWord", "heute");
-        model.addAttribute("plusTwoWorkdaysWord", "morgen");
-        model.addAttribute("untilEndOfWeekWord", "xxx");
-
-        return "sicknote/sick_note_extend";
-    }
-
-    @PostMapping("/sicknote/extend")
-    public String extendSickNotePreview(@RequestParam(value = "extend", required = false) String extend,
-                                        @RequestParam(value = "extendToDate", required = false) LocalDate extendToDate,
-                                        @RequestParam(value = "custom-date-preview", required = false) Optional<String> customDateSubmit,
-                                        @ModelAttribute("sickNoteExtended") SickNoteExtendDto sickNoteExtendDto, Errors errors,
-                                        Model model) {
-
-        final Person signedInUser = personService.getSignedInUser();
-        final Optional<SickNote> maybeSickNote = getSickNoteOfYesterdayOrLastWorkDay(signedInUser);
-        if (maybeSickNote.isEmpty()) {
-            return "sicknote/sick_note_extended_not_found";
-        }
-
-        if (hasText(extend) || customDateSubmit.isPresent()) {
-            // form submit with a +x button or a custom date
-            final SickNote sickNote = maybeSickNote.get();
-            return sickNoteExtendPreview(signedInUser, sickNote, extend, extendToDate, customDateSubmit, model);
-        } else {
-            // form submit to extend the sick note
-            return extendSickNote(signedInUser, sickNoteExtendDto, errors, model);
-        }
-    }
-
-    private Optional<SickNote> getSickNoteOfYesterdayOrLastWorkDay(Person signedInUser) {
-        final Optional<SickNote> maybeSickNote = sickNoteService.getSickNoteOfYesterdayOrLastWorkDay(signedInUser);
-        if (maybeSickNote.isPresent()) {
-            final SickNote sickNote = maybeSickNote.get();
-            if (!sickNote.getPerson().equals(signedInUser)) {
-                throw new AccessDeniedException("SickNote is not of User '%s'".formatted(signedInUser.getId()));
-            }
-        }
-        return maybeSickNote;
-    }
-
-    /**
-     * Handles preview rendering of the desired sick note to extend.
-     */
-    private String sickNoteExtendPreview(Person signedInUser, SickNote sickNote, String extend, LocalDate extendToDate, Optional<String> customDateSubmit, Model model) {
-
-        // TODO use correct workingdays value
-        final SickNoteExtendDto currentSickNoteDto = new SickNoteExtendDto(sickNote.getId(), sickNote.getStartDate(), sickNote.getEndDate(), 42, sickNote.isAubPresent());
-
-        model.addAttribute("sickNote", currentSickNoteDto);
-        model.addAttribute("sickNotePersonId", signedInUser.getId());
-        model.addAttribute("today", LocalDate.now(clock));
-        model.addAttribute("extendToDate", extendToDate == null ? LocalDate.now(clock) : extendToDate);
-        // TODO model attributes
-        model.addAttribute("sickNoteEndDateWord", "heute");
-        model.addAttribute("extensionDatePlusOne", LocalDate.now());
-        model.addAttribute("extensionDatePlusTwo", LocalDate.now().plusDays(1));
-        model.addAttribute("extensionDateEndOfWeek", LocalDate.now().plusDays(4));
-        model.addAttribute("plusOneWorkdayWord", "heute");
-        model.addAttribute("plusTwoWorkdaysWord", "morgen");
-        model.addAttribute("untilEndOfWeekWord", "xxx");
-
-        final SickNoteExtendDto sickNoteExtendedDto;
-
-        // TODO use correct values
-        if ("1".equals(extend)) {
-            model.addAttribute("selectedExtend", "1");
-            sickNoteExtendedDto = new SickNoteExtendDto(sickNote.getId(), sickNote.getStartDate(), sickNote.getEndDate().plusDays(1), 2, sickNote.isAubPresent());
-        } else if ("2".equals(extend)) {
-            model.addAttribute("selectedExtend", "2");
-            sickNoteExtendedDto = new SickNoteExtendDto(sickNote.getId(), sickNote.getStartDate(), sickNote.getEndDate().plusDays(2), 3, sickNote.isAubPresent());
-        } else if ("end-of-week".equals(extend)) {
-            model.addAttribute("selectedExtend", "end-of-week");
-            sickNoteExtendedDto = new SickNoteExtendDto(sickNote.getId(), sickNote.getStartDate(), sickNote.getEndDate().plusDays(1), 4, sickNote.isAubPresent());
-        } else if (customDateSubmit.isPresent()) {
-            model.addAttribute("selectedExtend", "custom");
-            sickNoteExtendedDto = new SickNoteExtendDto(sickNote.getId(), sickNote.getStartDate(), extendToDate, 5, sickNote.isAubPresent());
-        } else {
-            model.addAttribute("selectedExtend", "");
-            sickNoteExtendedDto = null;
-        }
-
-        model.addAttribute("sickNoteExtended", sickNoteExtendedDto);
-
-        // TODO use redirect with flashAttributes?
-        return "sicknote/sick_note_extend";
-    }
-
-    /**
-     * Extends the sick note with the desired information.
-     */
-    private String extendSickNote(Person signedInUser, SickNoteExtendDto sickNoteExtendDto, Errors errors, Model model) {
-        // TODO extend sick note
-        throw new RuntimeException("not implemented yet");
     }
 
     @PostMapping("/sicknote")
