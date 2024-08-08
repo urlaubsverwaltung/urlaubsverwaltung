5c31cc08e00ad448092f677ed44929aeee38d79f
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-08-08 14:26:28 +0200

    Add test for reason is given for sick note convert

--- src/test/java/org/synyx/urlaubsverwaltung/sicknote/sicknote/SickNoteConvertFormValidatorTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/sicknote/sicknote/SickNoteConvertFormValidatorTest.java
@@ -2,24 +2,39 @@ package org.synyx.urlaubsverwaltung.sicknote.sicknote;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.validation.Errors;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 /**
  * Unit test for {@link SickNoteConvertFormValidator}.
  */
+@ExtendWith(MockitoExtension.class)
 class SickNoteConvertFormValidatorTest {
 
     private SickNoteConvertFormValidator validator;
 
+    @Mock
     private Errors errors;
 
     @BeforeEach
     void setUp() {
         validator = new SickNoteConvertFormValidator();
-        errors = mock(Errors.class);
+    }
+
+    @Test
+    void ensureIfReasonIsGivenNoError() {
+
+        final SickNoteConvertForm convertForm = new SickNoteConvertForm(SickNote.builder().build());
+        convertForm.setReason("reason");
+
+        validator.validate(convertForm, errors);
+
+        verifyNoInteractions(errors);
     }
 
     @Test
