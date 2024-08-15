efb7bcbb143abc6c65c7b4eaf450c2a4c8fd9bbf
Johannes Graf <graf.johannes@gmail.com>
2024-08-15 23:04:54 +0200

    Adjust Google Calendar Integration
    adjust behavior to create new instances of GoogleAuthorizationCodeFlow
    or Calendar so we don't share existing instances

---
+++ src/main/java/org/synyx/urlaubsverwaltung/calendarintegration/GoogleAuthorizationCodeFlowFactory.java
@@ -0,0 +1,42 @@
+package org.synyx.urlaubsverwaltung.calendarintegration;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+
+import com.google.api.services.calendar.CalendarScopes;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+public class GoogleAuthorizationCodeFlowFactory {
+
+    private final JsonFactory jsonFactory;
+    private final NetHttpTransport netHttpTransport;
+
+    GoogleAuthorizationCodeFlowFactory(JsonFactory jsonFactory, NetHttpTransport netHttpTransport) {
+        this.jsonFactory = jsonFactory;
+        this.netHttpTransport = netHttpTransport;
+    }
+
+    public GoogleAuthorizationCodeFlow create(String clientId, String clientSecret) throws GeneralSecurityException, IOException {
+
+        final GoogleClientSecrets.Details web = new GoogleClientSecrets.Details();
+        web.setClientId(clientId);
+        web.setClientSecret(clientSecret);
+
+        final GoogleClientSecrets clientSecrets = new GoogleClientSecrets();
+
+        clientSecrets.setWeb(web);
+
+        return new GoogleAuthorizationCodeFlow.Builder(netHttpTransport, jsonFactory, clientSecrets,
+            Collections.singleton(CalendarScopes.CALENDAR))
+            .setApprovalPrompt("force")
+            .setAccessType("offline")
+            .build();
+
+
+    }
+}
