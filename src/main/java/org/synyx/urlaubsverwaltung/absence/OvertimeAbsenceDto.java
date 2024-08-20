dd6206965c6fc947d2d377f453b3b956e8dfdfc1
Jannik Möll <42173084+MoellJ@users.noreply.github.com>
2024-08-20 13:44:05 +0200

    Fix OpenAPI for overtime endpoint gh-4547

--- src/main/java/org/synyx/urlaubsverwaltung/absence/OvertimeAbsenceDto.java
+++ src/main/java/org/synyx/urlaubsverwaltung/absence/OvertimeAbsenceDto.java
@@ -1,6 +1,7 @@
 package org.synyx.urlaubsverwaltung.absence;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.time.Duration;
@@ -9,6 +10,10 @@ public class OvertimeAbsenceDto extends RepresentationModel<OvertimeAbsenceDto> 
 
     private final Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Schema(
+        example = "PT8H",
+        type = "string",
+        format = "duration")
     private final Duration usedOvertimeDuration;
 
     public OvertimeAbsenceDto(Long id, Duration usedOvertimeDuration) {
