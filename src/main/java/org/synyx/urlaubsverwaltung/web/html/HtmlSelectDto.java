42ce228175a36ad49f8ade466b679d0c2bbd0da8
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-02-21 13:13:31 +0100

    Convert HtmlSelectDto to record

--- src/main/java/org/synyx/urlaubsverwaltung/web/html/HtmlSelectDto.java
+++ src/main/java/org/synyx/urlaubsverwaltung/web/html/HtmlSelectDto.java
@@ -2,15 +2,7 @@ package org.synyx.urlaubsverwaltung.web.html;
 
 import java.util.List;
 
-public class HtmlSelectDto {
-
-    private final List<HtmlOptgroupDto> optgroups;
-
-    public HtmlSelectDto(List<HtmlOptgroupDto> optgroups) {
-        this.optgroups = optgroups;
-    }
-
-    public List<HtmlOptgroupDto> getOptgroups() {
-        return optgroups;
-    }
+public record HtmlSelectDto(
+    List<HtmlOptgroupDto> optgroups
+) {
 }
