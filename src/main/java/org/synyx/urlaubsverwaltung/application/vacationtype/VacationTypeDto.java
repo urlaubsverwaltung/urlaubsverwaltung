af1be3ebfd148d2626d052b24f846d6b4a8653e9
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-02-24 11:23:58 +0100

    Convert VacationTypeDto to record

--- src/main/java/org/synyx/urlaubsverwaltung/application/vacationtype/VacationTypeDto.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/vacationtype/VacationTypeDto.java
@@ -1,34 +1,7 @@
 package org.synyx.urlaubsverwaltung.application.vacationtype;
 
-import java.util.Objects;
-
-public class VacationTypeDto {
-    private final Long id;
-    private final VacationTypeColor color;
-
-    public VacationTypeDto(Long id, VacationTypeColor color) {
-        this.id = id;
-        this.color = color;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public VacationTypeColor getColor() {
-        return color;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        VacationTypeDto that = (VacationTypeDto) o;
-        return Objects.equals(id, that.id) && Objects.equals(color, that.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, color);
-    }
+public record VacationTypeDto(
+    Long id,
+    VacationTypeColor color
+) {
 }
