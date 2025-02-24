63a8aee96231a84fd401044102b1b4d590bc8f81
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-02-24 11:22:53 +0100

    Convert NavigationDto to record

--- src/main/java/org/synyx/urlaubsverwaltung/web/NavigationDto.java
+++ src/main/java/org/synyx/urlaubsverwaltung/web/NavigationDto.java
@@ -2,15 +2,7 @@ package org.synyx.urlaubsverwaltung.web;
 
 import java.util.List;
 
-class NavigationDto {
-
-    private final List<NavigationItemDto> elements;
-
-    NavigationDto(List<NavigationItemDto> elements) {
-        this.elements = elements;
-    }
-
-    public List<NavigationItemDto> getElements() {
-        return elements;
-    }
+record NavigationDto(
+    List<NavigationItemDto> elements
+) {
 }
