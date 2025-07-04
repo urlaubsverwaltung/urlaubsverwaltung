e920edfffc5dbf4eeef42146764a28959bd7fc90
Benjamin Seber <benjamin.seber@mailbox.org>
2025-07-04 14:49:35 +0200

    WIP: sicknote statistics graph

--- src/main/javascript/bundles/sick-notes-statistics.js
+++ src/main/javascript/bundles/sick-notes-statistics.js
@@ -1,1 +1,26 @@
 import "../js/common";
+import ApexCharts from "apexcharts";
+
+const options = {
+  chart: {
+    type: "bar",
+    stacked: true,
+  },
+  theme: {
+    mode: "light",
+  },
+  colors: ["var(--sick-note-color)", "var(--sick-note-child-color)"],
+  xaxis: {
+    categories: globalThis.sicknoteStatistic.xaxisLabels,
+  },
+  yaxis: {
+    title: {
+      text: globalThis.sicknoteStatistic.yaxisTitle,
+      rotate: 0,
+    },
+  },
+  series: globalThis.sicknoteStatistic.dataseries,
+};
+
+const chart = new ApexCharts(document.querySelector("#sicknote-statistic-chart"), options);
+chart.render();
