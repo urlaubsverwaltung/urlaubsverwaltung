5fdf0c72255db60d8575afce02cdce7cb86628c6
Benjamin Seber <benjamin.seber@mailbox.org>
2025-08-22 17:01:31 +0200

    throw when PersonId value is null

--- src/main/java/org/synyx/urlaubsverwaltung/person/PersonId.java
+++ src/main/java/org/synyx/urlaubsverwaltung/person/PersonId.java
@@ -1,4 +1,15 @@
 package org.synyx.urlaubsverwaltung.person;
 
+import static org.springframework.util.Assert.notNull;
+
+/**
+ * Identifies a {@link Person}.
+ *
+ * @param value the unique identifier of the person, must not be {@code null}
+ */
 public record PersonId(Long value) {
+
+    public PersonId {
+        notNull(value, "PersonId value must not be null");
+    }
 }
