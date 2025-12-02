b62cae528421c5ae24433f04a941d7c7b7ada5a9
honnel <github@honnel.de>
2025-12-02 11:58:03 +0100

    Send company vacation events with turn of year

--- src/main/java/org/synyx/urlaubsverwaltung/account/TurnOfTheYearAccountUpdaterConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/account/TurnOfTheYearAccountUpdaterConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.synyx.urlaubsverwaltung.config.ScheduleLocking;
+import org.synyx.urlaubsverwaltung.extension.companyvacation.SettingsEventRepublisher;
 import org.synyx.urlaubsverwaltung.tenancy.configuration.single.ConditionalOnSingleTenantMode;
 
 @Configuration
@@ -16,23 +17,28 @@ class TurnOfTheYearAccountUpdaterConfiguration implements SchedulingConfigurer {
     private final TurnOfTheYearAccountUpdaterService turnOfTheYearAccountUpdaterService;
     private final ScheduleLocking scheduleLocking;
     private final TaskScheduler taskScheduler;
+    private final SettingsEventRepublisher settingsEventRepublisher;
 
     @Autowired
     TurnOfTheYearAccountUpdaterConfiguration(
         AccountProperties accountProperties, TurnOfTheYearAccountUpdaterService turnOfTheYearAccountUpdaterService,
-        ScheduleLocking scheduleLocking, TaskScheduler taskScheduler
+        SettingsEventRepublisher settingsEventRepublisher, ScheduleLocking scheduleLocking, TaskScheduler taskScheduler
     ) {
         this.accountProperties = accountProperties;
         this.turnOfTheYearAccountUpdaterService = turnOfTheYearAccountUpdaterService;
         this.scheduleLocking = scheduleLocking;
         this.taskScheduler = taskScheduler;
+        this.settingsEventRepublisher = settingsEventRepublisher;
     }
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         taskRegistrar.setTaskScheduler(taskScheduler);
         taskRegistrar.addCronTask(
-            scheduleLocking.withLock("UpdateAccountsForNextPeriod", turnOfTheYearAccountUpdaterService::updateAccountsForNextPeriod),
+            scheduleLocking.withLock("UpdateAccountsForNextPeriod", () -> {
+                turnOfTheYearAccountUpdaterService.updateAccountsForNextPeriod();
+                settingsEventRepublisher.republishEvents();
+            }),
             accountProperties.getUpdate().getCron()
         );
     }
