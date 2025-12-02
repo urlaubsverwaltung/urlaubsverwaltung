f57ad263346452740f86f10203e86f83faeb85c4
honnel <github@honnel.de>
2025-12-02 11:55:49 +0100

    Publish application event when work duration on christmas/new years eve changes

---
+++ src/main/java/org/synyx/urlaubsverwaltung/settings/WorkingDurationForChristmasEveUpdatedEvent.java
@@ -0,0 +1,6 @@
+package org.synyx.urlaubsverwaltung.settings;
+
+import org.synyx.urlaubsverwaltung.period.DayLength;
+
+public record WorkingDurationForChristmasEveUpdatedEvent(DayLength workingDurationForChristmasEve) {
+}
