2bae7e497d93fe3c26c8849767c6da20f8569a2a
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-02-09 16:39:57 +0100

    Revert "session jdbc: directly enable jdbc http session via annotation"
    This reverts commit 1df3866086d472957af8e080b3147a7532b455e0.

--- src/main/java/org/synyx/urlaubsverwaltung/security/SessionConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/security/SessionConfiguration.java
@@ -3,12 +3,10 @@ package org.synyx.urlaubsverwaltung.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.jdbc.PostgreSqlJdbcIndexedSessionRepositoryCustomizer;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.synyx.urlaubsverwaltung.tenancy.configuration.single.ConditionalOnSingleTenantMode;
 
 @Configuration
 @ConditionalOnSingleTenantMode
-@EnableJdbcHttpSession
 class SessionConfiguration {
     @Bean
     PostgreSqlJdbcIndexedSessionRepositoryCustomizer sessionRepositoryCustomizer() {
