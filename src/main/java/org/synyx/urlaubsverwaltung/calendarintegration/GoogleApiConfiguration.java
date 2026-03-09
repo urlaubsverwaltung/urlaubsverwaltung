1394f96acaa387463a35d03fa50ca175da6bbca6
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-03-09 15:14:39 +0100

    Replace deprecated GoogleNetHttpTransport with NetHttpTransport

--- src/main/java/org/synyx/urlaubsverwaltung/calendarintegration/GoogleApiConfiguration.java
+++ src/main/java/org/synyx/urlaubsverwaltung/calendarintegration/GoogleApiConfiguration.java
@@ -1,19 +1,16 @@
 package org.synyx.urlaubsverwaltung.calendarintegration;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 @Configuration
 public class GoogleApiConfiguration {
 
     @Bean
-    GoogleAuthorizationCodeFlowFactory googleAuthorizationCodeFlowFactory() throws GeneralSecurityException, IOException {
-        return new GoogleAuthorizationCodeFlowFactory(GsonFactory.getDefaultInstance(), GoogleNetHttpTransport.newTrustedTransport());
+    GoogleAuthorizationCodeFlowFactory googleAuthorizationCodeFlowFactory() {
+        return new GoogleAuthorizationCodeFlowFactory(GsonFactory.getDefaultInstance(), new NetHttpTransport());
     }
 
 }
