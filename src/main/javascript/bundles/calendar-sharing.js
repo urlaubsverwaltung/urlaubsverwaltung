b0c0af56fafe93a07790834d73b93b24200a608f
Benjamin Seber <benjamin.seber@mailbox.org>
2026-03-26 19:32:26 +0100

    replace scroll-padding-top with turbo-frame
    has been introduced for calendar-sharing links
    with commit 7c8d6c525fda6623e9fd1271be22c47484fb8833 "Add anchor on calendar sharing buttons"
    
    scroll position is the same due to added urbo-frame.
    only the frame is updated now, with javascript.
    
    without javascript, the user will be at the top of the page :shrug:
    however, I don't want the side effect on every link to ids.
    scroll-padding-top is "not required" anymore due to the css grid layout.
    there is no fixed or sticky header covering content. (grid ftw)
    
    closes #5229

--- src/main/javascript/bundles/calendar-sharing.js
+++ src/main/javascript/bundles/calendar-sharing.js
@@ -1,2 +1,6 @@
+import * as Turbo from "@hotwired/turbo";
 import "../js/common";
 import "../components/copy-to-clipboard-input";
+
+// opt-in to turbo with `data-turbo="true"`
+Turbo.session.drive = false;
