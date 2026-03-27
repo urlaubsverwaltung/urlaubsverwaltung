0885ed625cbde6b06fd8632eb2e22a781b822277
Benjamin Seber <benjamin.seber@mailbox.org>
2026-03-27 14:01:42 +0100

    add loading info to subnav link
    unsure about this... we will see...
    
    maybe we should just append this loading spinner at the end of the clicked navigation item?
    this could also be done with general buttons -> loading spinner next to the button text?
    
    dunno, the linea gradient animation at top level nav links is nice, too.
    
    closes #5229

--- src/main/javascript/components/navigation/navigation.js
+++ src/main/javascript/components/navigation/navigation.js
@@ -1,9 +1,30 @@
 document.addEventListener("click", function (event) {
   /** @type HTMLElement */
   const target = event.target;
-  const link = target.closest(".navigation-link");
+  navlink(target);
+  subnavlink(target);
+});
+
+/**
+ *
+ * @param {HTMLElement} element
+ */
+function navlink(element) {
+  const link = element.closest(".navigation-link");
   if (link) {
     // element is replaced on response and loading class removed
     link.classList.add("navigation-link--loading");
   }
-});
+}
+
+/**
+ *
+ * @param {HTMLElement} element
+ */
+function subnavlink(element) {
+  const link = element.closest(".navigation-sublink");
+  if (link) {
+    // element is replaced on response and loading class removed
+    link.classList.add("navigation-sublink--loading");
+  }
+}
