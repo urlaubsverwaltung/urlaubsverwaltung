d6f3af55009465e7907c667bdcc71e13aadc2099
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-04-02 14:36:18 +0200

    Add ApplicationsViewControllerTest
    closes #6016

--- src/main/java/org/synyx/urlaubsverwaltung/application/me/ApplicationDto.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/me/ApplicationDto.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
 
-class ApplicationDto {
+public class ApplicationDto {
 
     private Long id;
     private Long personId;
