3d564565a62f0acfe90ab7c4513a3071d3ecfe12
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-04-02 15:00:02 +0200

    Add tests for YearlySickDaysSummary and YearlyUsedDaysSummary
    closes #6016

---
+++ src/test/java/org/synyx/urlaubsverwaltung/overview/YearlyUsedDaysSummaryTest.java
@@ -0,0 +1,222 @@
+package org.synyx.urlaubsverwaltung.overview;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.support.StaticMessageSource;
+import org.synyx.urlaubsverwaltung.application.application.Application;
+import org.synyx.urlaubsverwaltung.period.DayLength;
+import org.synyx.urlaubsverwaltung.person.Person;
+import org.synyx.urlaubsverwaltung.workingtime.WorkDaysCountService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.ZERO;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.synyx.urlaubsverwaltung.TestDataCreator.anyApplication;
+import static org.synyx.urlaubsverwaltung.TestDataCreator.createApplication;
+import static org.synyx.urlaubsverwaltung.TestDataCreator.createVacationType;
+import static org.synyx.urlaubsverwaltung.application.application.ApplicationStatus.ALLOWED;
+import static org.synyx.urlaubsverwaltung.application.application.ApplicationStatus.ALLOWED_CANCELLATION_REQUESTED;
+import static org.synyx.urlaubsverwaltung.application.application.ApplicationStatus.TEMPORARY_ALLOWED;
+import static org.synyx.urlaubsverwaltung.application.application.ApplicationStatus.WAITING;
+import static org.synyx.urlaubsverwaltung.application.vacationtype.VacationCategory.HOLIDAY;
+import static org.synyx.urlaubsverwaltung.application.vacationtype.VacationCategory.OVERTIME;
+import static org.synyx.urlaubsverwaltung.application.vacationtype.VacationCategory.SPECIALLEAVE;
+import static org.synyx.urlaubsverwaltung.application.vacationtype.VacationCategory.UNPAIDLEAVE;
+
+
+@ExtendWith(MockitoExtension.class)
+class YearlyUsedDaysSummaryTest {
+
+    @Mock
+    private WorkDaysCountService workDaysCountService;
+
+    @Test
+    void ensureGeneratesCorrectUsedDaysOverviewWithApplicationSpanningMultipleYears() {
+
+        final Application holidayAllowed = anyApplication();
+        holidayAllowed.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holidayAllowed.setStartDate(LocalDate.of(2014, 3, 5));
+        holidayAllowed.setEndDate(LocalDate.of(2016, 12, 30));
+        holidayAllowed.setStatus(ALLOWED);
+
+        final BigDecimal workingDays = BigDecimal.valueOf(499);
+        when(workDaysCountService.getWorkDaysCount(any(DayLength.class), any(LocalDate.class),
+            any(LocalDate.class), any(Person.class)))
+            .thenReturn(workingDays);
+
+        final YearlyUsedDaysSummary usedDaysOverview = new YearlyUsedDaysSummary(List.of(holidayAllowed), 2014, workDaysCountService);
+        final UsedDays holidayDays = usedDaysOverview.getHolidayDays();
+        assertThat(holidayDays.getDays())
+            .containsEntry("WAITING", ZERO)
+            .containsEntry("ALLOWED", workingDays)
+            .containsEntry("ALLOWED_CANCELLATION_REQUESTED", ZERO);
+    }
+
+    @Test
+    void ensureGeneratesCorrectUsedDaysOverview() {
+
+        final Application holiday = anyApplication();
+        holiday.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holiday.setStartDate(LocalDate.of(2014, 10, 13));
+        holiday.setEndDate(LocalDate.of(2014, 10, 13));
+        holiday.setStatus(WAITING);
+
+        final Application holidayAllowed = anyApplication();
+        holidayAllowed.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holidayAllowed.setStartDate(LocalDate.of(2014, 10, 14));
+        holidayAllowed.setEndDate(LocalDate.of(2014, 10, 14));
+        holidayAllowed.setStatus(ALLOWED);
+
+        final Application holidayAllowedRequestCancellation = anyApplication();
+        holidayAllowedRequestCancellation.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holidayAllowedRequestCancellation.setStartDate(LocalDate.of(2014, 10, 14));
+        holidayAllowedRequestCancellation.setEndDate(LocalDate.of(2014, 10, 14));
+        holidayAllowedRequestCancellation.setStatus(ALLOWED_CANCELLATION_REQUESTED);
+
+        final Application specialLeave = anyApplication();
+        specialLeave.setVacationType(createVacationType(1L, SPECIALLEAVE, new StaticMessageSource()));
+        specialLeave.setStartDate(LocalDate.of(2014, 10, 15));
+        specialLeave.setEndDate(LocalDate.of(2014, 10, 15));
+        specialLeave.setStatus(WAITING);
+
+        final Application specialLeaveAllowed = anyApplication();
+        specialLeaveAllowed.setVacationType(createVacationType(1L, SPECIALLEAVE, new StaticMessageSource()));
+        specialLeaveAllowed.setStartDate(LocalDate.of(2014, 10, 16));
+        specialLeaveAllowed.setEndDate(LocalDate.of(2014, 10, 16));
+        specialLeaveAllowed.setStatus(ALLOWED);
+
+        final Application unpaidLeave = anyApplication();
+        unpaidLeave.setVacationType(createVacationType(1L, UNPAIDLEAVE, new StaticMessageSource()));
+        unpaidLeave.setStartDate(LocalDate.of(2014, 10, 17));
+        unpaidLeave.setEndDate(LocalDate.of(2014, 10, 17));
+        unpaidLeave.setStatus(WAITING);
+
+        final Application unpaidLeaveAllowed = anyApplication();
+        unpaidLeaveAllowed.setVacationType(createVacationType(1L, UNPAIDLEAVE, new StaticMessageSource()));
+        unpaidLeaveAllowed.setStartDate(LocalDate.of(2014, 10, 20));
+        unpaidLeaveAllowed.setEndDate(LocalDate.of(2014, 10, 20));
+        unpaidLeaveAllowed.setStatus(ALLOWED);
+
+        final Application overtimeLeave = anyApplication();
+        overtimeLeave.setVacationType(createVacationType(1L, OVERTIME, new StaticMessageSource()));
+        overtimeLeave.setStartDate(LocalDate.of(2014, 10, 21));
+        overtimeLeave.setEndDate(LocalDate.of(2014, 10, 21));
+        overtimeLeave.setStatus(WAITING);
+
+        final Application overtimeLeaveAllowed = anyApplication();
+        overtimeLeaveAllowed.setVacationType(createVacationType(1L, OVERTIME, new StaticMessageSource()));
+        overtimeLeaveAllowed.setStartDate(LocalDate.of(2014, 10, 22));
+        overtimeLeaveAllowed.setEndDate(LocalDate.of(2014, 10, 22));
+        overtimeLeaveAllowed.setStatus(ALLOWED);
+
+        final Application overtimeLeaveRequestCancellation = anyApplication();
+        overtimeLeaveRequestCancellation.setVacationType(createVacationType(1L, OVERTIME, new StaticMessageSource()));
+        overtimeLeaveRequestCancellation.setStartDate(LocalDate.of(2014, 10, 22));
+        overtimeLeaveRequestCancellation.setEndDate(LocalDate.of(2014, 10, 22));
+        overtimeLeaveRequestCancellation.setStatus(ALLOWED_CANCELLATION_REQUESTED);
+
+        final List<Application> applications = Arrays.asList(holiday, holidayAllowed, holidayAllowedRequestCancellation,
+            specialLeave, specialLeaveAllowed, unpaidLeave, unpaidLeaveAllowed, overtimeLeave, overtimeLeaveAllowed,
+            overtimeLeaveRequestCancellation);
+
+        // just return 1 day for each application for leave
+        when(workDaysCountService.getWorkDaysCount(any(DayLength.class), any(LocalDate.class),
+            any(LocalDate.class), any(Person.class)))
+            .thenReturn(ONE);
+
+        final YearlyUsedDaysSummary yearlyUsedDaysSummary = new YearlyUsedDaysSummary(applications, 2014, workDaysCountService);
+
+        final UsedDays holidayDays = yearlyUsedDaysSummary.getHolidayDays();
+        assertThat(holidayDays.getDays())
+            .containsEntry("WAITING", ONE)
+            .containsEntry("ALLOWED", ONE)
+            .containsEntry("ALLOWED_CANCELLATION_REQUESTED", ONE);
+
+        final UsedDays holidayDaysAllowed = yearlyUsedDaysSummary.getHolidayDaysAllowed();
+        assertThat(holidayDaysAllowed.getDays())
+            .containsEntry("ALLOWED", ONE)
+            .containsEntry("ALLOWED_CANCELLATION_REQUESTED", ONE);
+
+        final UsedDays otherDays = yearlyUsedDaysSummary.getOtherDays();
+        assertThat(otherDays.getDays())
+            .containsEntry("WAITING", BigDecimal.valueOf(3))
+            .containsEntry("ALLOWED", BigDecimal.valueOf(3))
+            .containsEntry("ALLOWED_CANCELLATION_REQUESTED", ONE);
+
+        final UsedDays otherDaysAllowed = yearlyUsedDaysSummary.getOtherDaysAllowed();
+        assertThat(otherDaysAllowed.getDays())
+            .containsEntry("ALLOWED", BigDecimal.valueOf(3))
+            .containsEntry("ALLOWED_CANCELLATION_REQUESTED", ONE);
+    }
+
+    @Test
+    void ensureCalculatesDaysForGivenYearForApplicationsSpanningTwoYears() {
+
+        Person person = new Person("muster", "Muster", "Marlene", "muster@example.org");
+        LocalDate startDate = LocalDate.of(2013, 12, 24);
+        LocalDate endDate = LocalDate.of(2014, 1, 6);
+
+        // 3 days in 2013, 2 days in 2014
+        Application holiday = createApplication(person, createVacationType(1L, HOLIDAY, new StaticMessageSource()), startDate, endDate, DayLength.FULL);
+
+        when(workDaysCountService.getWorkDaysCount(DayLength.FULL, LocalDate.of(2014, 1, 1), endDate, person))
+            .thenReturn(BigDecimal.valueOf(2));
+
+        final YearlyUsedDaysSummary yearlyUsedDaysSummary = new YearlyUsedDaysSummary(singletonList(holiday), 2014, workDaysCountService);
+
+        final UsedDays holidayDays = yearlyUsedDaysSummary.getHolidayDays();
+        assertThat(holidayDays.getDays())
+            .containsEntry("WAITING", BigDecimal.valueOf(2))
+            .containsEntry("ALLOWED", ZERO);
+
+        final UsedDays otherDays = yearlyUsedDaysSummary.getOtherDays();
+        assertThat(otherDays.getDays())
+            .containsEntry("WAITING", ZERO)
+            .containsEntry("ALLOWED", ZERO);
+    }
+
+    @Test
+    void ensureGeneratesCorrectUsedDaysDtoConsideringTemporaryAllowedApplicationsForLeave() {
+
+        final Application holiday = anyApplication();
+        holiday.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holiday.setStartDate(LocalDate.of(2014, 10, 13));
+        holiday.setEndDate(LocalDate.of(2014, 10, 13));
+        holiday.setStatus(WAITING);
+
+        final Application holidayAllowed = anyApplication();
+        holidayAllowed.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holidayAllowed.setStartDate(LocalDate.of(2014, 10, 14));
+        holidayAllowed.setEndDate(LocalDate.of(2014, 10, 14));
+        holidayAllowed.setStatus(ALLOWED);
+
+        final Application holidayTemporaryAllowed = anyApplication();
+        holidayTemporaryAllowed.setVacationType(createVacationType(1L, HOLIDAY, new StaticMessageSource()));
+        holidayTemporaryAllowed.setStartDate(LocalDate.of(2014, 10, 15));
+        holidayTemporaryAllowed.setEndDate(LocalDate.of(2014, 10, 15));
+        holidayTemporaryAllowed.setStatus(TEMPORARY_ALLOWED);
+
+        List<Application> applications = Arrays.asList(holiday, holidayTemporaryAllowed, holidayAllowed);
+
+        // just return 1 day for each application for leave
+        when(workDaysCountService.getWorkDaysCount(any(DayLength.class), any(LocalDate.class),
+            any(LocalDate.class), any(Person.class))).thenReturn(ONE);
+
+        final YearlyUsedDaysSummary yearlyUsedDaysSummary = new YearlyUsedDaysSummary(applications, 2014, workDaysCountService);
+        assertThat(yearlyUsedDaysSummary.getHolidayDays().getDays())
+            .containsEntry("WAITING", ONE)
+            .containsEntry("TEMPORARY_ALLOWED", ONE)
+            .containsEntry("ALLOWED", ONE);
+    }
+}
+
---
+++ src/test/java/org/synyx/urlaubsverwaltung/sicknote/me/YearlySickDaysSummaryTest.java
@@ -0,0 +1,221 @@
+package org.synyx.urlaubsverwaltung.sicknote.me;
+
+import org.junit.jupiter.api.Test;
+import org.synyx.urlaubsverwaltung.person.Person;
+import org.synyx.urlaubsverwaltung.sicknote.sickdays.SickDays;
+import org.synyx.urlaubsverwaltung.sicknote.sicknote.SickNote;
+import org.synyx.urlaubsverwaltung.sicknote.sicknotetype.SickNoteType;
+import org.synyx.urlaubsverwaltung.workingtime.WorkDaysCountService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.math.BigDecimal.ONE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.synyx.urlaubsverwaltung.period.DayLength.FULL;
+import static org.synyx.urlaubsverwaltung.sicknote.sicknote.SickNoteCategory.SICK_NOTE;
+import static org.synyx.urlaubsverwaltung.sicknote.sicknote.SickNoteCategory.SICK_NOTE_CHILD;
+import static org.synyx.urlaubsverwaltung.sicknote.sicknote.SickNoteStatus.ACTIVE;
+import static org.synyx.urlaubsverwaltung.sicknote.sicknote.SickNoteStatus.CANCELLED;
+
+class YearlySickDaysSummaryTest {
+
+    @Test
+    void ensureGeneratesCorrectSickDaysOverviewWithSickNoteSpanningMultipleYears() {
+
+        final Person person = new Person("username", "last name", "first name", "email@example.org");
+        person.setId(1L);
+
+        final SickNoteType sickNoteType = new SickNoteType();
+        sickNoteType.setCategory(SICK_NOTE);
+        sickNoteType.setMessageKey("Krankmeldung");
+
+        final LocalDate startDate = LocalDate.of(2016, 1, 1);
+        final LocalDate endDate = LocalDate.of(2016, 12, 31);
+        final LocalDate aubStartDate = LocalDate.of(2016, 5, 5);
+        final LocalDate aubEndDate = LocalDate.of(2016, 6, 6);
+
+        final SickNote sickNote = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteType)
+            .status(ACTIVE)
+            .startDate(startDate)
+            .endDate(endDate)
+            .aubStartDate(aubStartDate)
+            .aubEndDate(aubEndDate)
+            .build();
+
+        final WorkDaysCountService workDaysCountService = mock(WorkDaysCountService.class);
+
+        final BigDecimal workingDays = BigDecimal.valueOf(209);
+        when(workDaysCountService.getWorkDaysCount(FULL, startDate, endDate, person)).thenReturn(workingDays);
+
+        final BigDecimal workingDaysAub = BigDecimal.valueOf(100);
+        when(workDaysCountService.getWorkDaysCount(FULL, aubStartDate, aubEndDate, person)).thenReturn(workingDaysAub);
+
+        final LocalDate from = LocalDate.of(2015, 1, 1);
+        final LocalDate to = LocalDate.of(2017, 12, 31);
+        final YearlySickDaysSummary sickDaysOverview = new YearlySickDaysSummary(List.of(sickNote), workDaysCountService, from, to);
+        final SickDays sickDays = sickDaysOverview.getSickDays();
+        assertThat(sickDays.getDays())
+            .containsEntry("TOTAL", workingDays)
+            .containsEntry("WITH_AUB", workingDaysAub);
+    }
+
+    @Test
+    void ensureGeneratesCorrectSickDaysOverviewWithSickNoteSpanningMultipleYearsButAubEndsInLastYear() {
+
+        final Person person = new Person("username", "last name", "first name", "email@example.org");
+        person.setId(1L);
+
+        final SickNoteType sickNoteType = new SickNoteType();
+        sickNoteType.setCategory(SICK_NOTE);
+        sickNoteType.setMessageKey("Krankmeldung");
+
+        final LocalDate intervalStart = LocalDate.of(2026, 1, 1);
+
+        final LocalDate sickNoteStartDate = LocalDate.of(2025, 12, 29);
+        final LocalDate sickNoteEndDate = LocalDate.of(2026, 1, 9);
+
+        final SickNote sickNote = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteType)
+            .status(ACTIVE)
+            // extend beyond turn of the year (actually it just has to extend the requested interval below)
+            .startDate(sickNoteStartDate)
+            .endDate(sickNoteEndDate)
+            // aub must end before the start of the requested interval below
+            .aubStartDate(sickNoteStartDate)
+            .aubEndDate(LocalDate.of(2025, 12,  31))
+            .build();
+
+        final WorkDaysCountService workDaysCountService = mock(WorkDaysCountService.class);
+
+        final BigDecimal sickNoteWorkingDays = BigDecimal.valueOf(42);
+        when(workDaysCountService.getWorkDaysCount(FULL, intervalStart, sickNoteEndDate, person)).thenReturn(sickNoteWorkingDays);
+
+        final LocalDate from = LocalDate.of(2026, 1, 1);
+        final LocalDate to = LocalDate.of(2026, 12, 31);
+        final YearlySickDaysSummary sut = new YearlySickDaysSummary(List.of(sickNote), workDaysCountService, from, to);
+
+        final SickDays actual = sut.getSickDays();
+
+        assertThat(actual.getDays())
+            .containsEntry("TOTAL", sickNoteWorkingDays)
+            .containsEntry("WITH_AUB", BigDecimal.ZERO);
+    }
+
+    @Test
+    void ensureGeneratesCorrectSickDaysOverview() {
+
+        final Person person = new Person("username", "last name", "first name", "email@example.org");
+        person.setId(1L);
+
+        final SickNoteType sickNoteType = new SickNoteType();
+        sickNoteType.setCategory(SICK_NOTE);
+        sickNoteType.setMessageKey("Krankmeldung");
+
+        final SickNoteType sickNoteTypeChild = new SickNoteType();
+        sickNoteTypeChild.setCategory(SICK_NOTE_CHILD);
+        sickNoteTypeChild.setMessageKey("Kind-Krankmeldung");
+
+        final WorkDaysCountService workDaysCountService = mock(WorkDaysCountService.class);
+
+        final SickNote sickNoteWithoutAUB = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteType)
+            .status(ACTIVE)
+            .startDate(LocalDate.of(2014, 10, 13))
+            .endDate(LocalDate.of(2014, 10, 13))
+            .build();
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 13), LocalDate.of(2014, 10, 13), person))
+            .thenReturn(ONE);
+
+        final SickNote sickNoteWithAUB = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteType)
+            .status(ACTIVE)
+            .startDate(LocalDate.of(2014, 10, 14))
+            .endDate(LocalDate.of(2014, 10, 16))
+            .aubStartDate(LocalDate.of(2014, 10, 15))
+            .aubEndDate(LocalDate.of(2014, 10, 15))
+            .build();
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 14), LocalDate.of(2014, 10, 16), person))
+            .thenReturn(BigDecimal.valueOf(3));
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 15), LocalDate.of(2014, 10, 15), person))
+            .thenReturn(ONE);
+
+        final SickNote childSickNoteWithoutAUB = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteTypeChild)
+            .status(ACTIVE)
+            .startDate(LocalDate.of(2014, 10, 15))
+            .endDate(LocalDate.of(2014, 10, 15))
+            .build();
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 15), LocalDate.of(2014, 10, 15), person))
+            .thenReturn(ONE);
+
+        final SickNote childSickNoteWithAUB = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteTypeChild)
+            .status(ACTIVE)
+            .startDate(LocalDate.of(2014, 10, 16))
+            .endDate(LocalDate.of(2014, 10, 18))
+            .aubStartDate(LocalDate.of(2014, 10, 16))
+            .aubEndDate(LocalDate.of(2014, 10, 17))
+            .build();
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 16), LocalDate.of(2014, 10, 18), person))
+            .thenReturn(BigDecimal.valueOf(3));
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 16), LocalDate.of(2014, 10, 17), person))
+            .thenReturn(BigDecimal.valueOf(2));
+
+        final SickNote inactiveSickNote = SickNote.builder()
+            .person(person)
+            .dayLength(FULL)
+            .sickNoteType(sickNoteTypeChild)
+            .status(CANCELLED)
+            .startDate(LocalDate.of(2014, 10, 17))
+            .endDate(LocalDate.of(2014, 10, 17))
+            .build();
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 17), LocalDate.of(2014, 10, 17), person))
+            .thenReturn(ONE);
+
+        final SickNote inactiveChildSickNote = SickNote.builder()
+            .person(person)
+            .sickNoteType(sickNoteTypeChild)
+            .status(CANCELLED)
+            .startDate(LocalDate.of(2014, 10, 18))
+            .endDate(LocalDate.of(2014, 10, 18))
+            .build();
+        when(workDaysCountService.getWorkDaysCount(FULL, LocalDate.of(2014, 10, 18), LocalDate.of(2014, 10, 18), person))
+            .thenReturn(ONE);
+
+        final List<SickNote> sickNotes = List.of(sickNoteWithoutAUB, sickNoteWithAUB, childSickNoteWithoutAUB,
+            childSickNoteWithAUB, inactiveSickNote, inactiveChildSickNote);
+
+        final YearlySickDaysSummary sickDaysOverview = new YearlySickDaysSummary(sickNotes, workDaysCountService,
+            LocalDate.of(2014, 10, 10), LocalDate.of(2014, 10, 18));
+
+        final SickDays sickDays = sickDaysOverview.getSickDays();
+        assertThat(sickDays.getDays())
+            .isNotNull()
+            .containsEntry("TOTAL", new BigDecimal("4"))
+            .containsEntry("WITH_AUB", ONE);
+
+        final SickDays childSickDays = sickDaysOverview.getChildSickDays();
+        assertThat(childSickDays.getDays())
+            .isNotNull()
+            .containsEntry("TOTAL", new BigDecimal("4"))
+            .containsEntry("WITH_AUB", new BigDecimal("2"));
+    }
+}
+
