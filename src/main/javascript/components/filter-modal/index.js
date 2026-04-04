1188e860049f2588073ca00c9a7143aaefcf7d7f
Benjamin Seber <benjamin.seber@mailbox.org>
2026-04-04 20:08:16 +0200

    delete unused javascript

--- src/main/javascript/components/filter-modal/index.js
+++
@@ -1,7 +0,0 @@
-import { createDatepicker } from "../datepicker";
-
-const getPersonId = () => {};
-const urlPrefix = ""; // not required, no absences are fetched since we have no single person (personId)
-
-createDatepicker("#startDate", { urlPrefix, getPersonId });
-createDatepicker("#endDate", { urlPrefix, getPersonId });
