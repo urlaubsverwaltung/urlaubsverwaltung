31dea1467d20e56ad0aaf66163c4e125a40bb3df
Benjamin Seber <benjamin.seber@mailbox.org>
2026-04-04 20:11:42 +0200

    migrate javascript to css :has selector

--- src/main/javascript/components/list/list-selectable.js
+++
@@ -1,7 +0,0 @@
-globalThis.addEventListener("change", function (event) {
-  if (event.target.closest("[data-list-item-selection-toggle]")) {
-    // enable/disable clicked
-    const item = event.target.closest("li");
-    item.dataset.enabled = event.target.checked;
-  }
-});
