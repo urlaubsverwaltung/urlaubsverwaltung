efc5ccef2cbefd60db837e33917556892248c2b8
Benjamin Seber <benjamin.seber@mailbox.org>
2026-05-22 09:13:31 +0200

    fix javascript coverage report
    closes #5838

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -9,9 +9,10 @@ export default defineConfig({
         url: "http://localhost",
       },
     },
-    include: ['src/main/javascript/**/*.spec.js'],
+    include: ["src/main/javascript/**/*.spec.js"],
     coverage: {
       provider: "v8",
+      reporter: ["lcov", "text", "html"],
       reportsDirectory: "target/js-coverage",
       include: ["src/main/javascript/**/*.js"],
       exclude: ["**/*.{test,spec}.js", "**/__tests__/**"],
