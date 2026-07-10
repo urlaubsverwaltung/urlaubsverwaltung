6ee168e17154a085524c048c00cd576db158515f
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-07-10 09:28:56 +0200

    enable unicorn/prefer-top-level-await rule
    closes #50
    9125
    5

--- src/main/javascript/js/date-fns-localized.js
+++ src/main/javascript/js/date-fns-localized.js
@@ -3,33 +3,28 @@ import { setLocale } from "../lib/date-fns/locale-resolver";
 const language = globalThis.uv.language.slice(0, 2);
 const subtag = globalThis.uv.language.slice(3);
 
-async function loadLocale() {
-  // our webpack build cannot handle top-level-await, so this is wrapped in an async function
-  switch (language) {
-    case "de": {
-      if (subtag.startsWith("AT")) {
-        const { deAT } = await import("date-fns/locale/de-AT");
-        setLocale(deAT);
-      } else {
-        const { de } = await import("date-fns/locale/de");
-        setLocale(de);
-      }
-      break;
+switch (language) {
+  case "de": {
+    if (subtag.startsWith("AT")) {
+      const { deAT } = await import("date-fns/locale/de-AT");
+      setLocale(deAT);
+    } else {
+      const { de } = await import("date-fns/locale/de");
+      setLocale(de);
     }
-    case "en": {
-      if (subtag.startsWith("GB")) {
-        const { enGB } = await import("date-fns/locale/en-GB");
-        setLocale(enGB);
-      }
-      break;
-    }
-    case "el": {
-      const { el } = await import("date-fns/locale/el");
-      setLocale(el);
-      break;
-    }
-    // No default
+    break;
   }
+  case "en": {
+    if (subtag.startsWith("GB")) {
+      const { enGB } = await import("date-fns/locale/en-GB");
+      setLocale(enGB);
+    }
+    break;
+  }
+  case "el": {
+    const { el } = await import("date-fns/locale/el");
+    setLocale(el);
+    break;
+  }
+  // No default
 }
-
-loadLocale();
